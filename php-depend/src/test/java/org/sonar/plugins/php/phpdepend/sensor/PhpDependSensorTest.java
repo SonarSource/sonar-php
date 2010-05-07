ba4bc4d5ccf1b7a21af88dd3d9e74e1145b7771a
Akram Ben Aissi <akram.benaissi@gmail.com>
2010-05-07 12:40:12 +0000

    Correcting unit tests to manage /logs directory creation in case it does not exist.

--- php-depend/src/test/java/org/sonar/plugins/php/phpdepend/sensor/PhpDependSensorTest.java
+++ php-depend/src/test/java/org/sonar/plugins/php/phpdepend/sensor/PhpDependSensorTest.java
@@ -24,68 +24,75 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.io.File;
+
 import org.apache.commons.configuration.Configuration;
 import org.junit.Test;
 import org.sonar.api.resources.Java;
 import org.sonar.api.resources.Project;
+import org.sonar.api.resources.ProjectFileSystem;
 import org.sonar.plugins.php.core.Php;
 import org.sonar.plugins.php.phpdepend.configuration.PhpDependConfiguration;
 
 public class PhpDependSensorTest {
 
-	@Test
-	public void shouldNotLaunchOnNonPhpProject() {
-		Project project = mock(Project.class);
-		when(project.getLanguage()).thenReturn(Java.INSTANCE);
-		Configuration configuration = mock(Configuration.class);
-		PhpDependSensor sensor = new PhpDependSensor();
-		when(
-				configuration.getString(PhpDependConfiguration.REPORT_FILE_NAME_PROPERTY_KEY,
-						PhpDependConfiguration.DEFAULT_REPORT_FILE_NAME)).thenReturn("pdepend.xml");
-		when(
-				configuration.getString(PhpDependConfiguration.REPORT_FILE_RELATIVE_PATH_PROPERTY_KEY,
-						PhpDependConfiguration.DEFAULT_REPORT_FILE_PATH)).thenReturn("reports");
-		when(project.getConfiguration()).thenReturn(configuration);
-		PhpDependConfiguration config = mock(PhpDependConfiguration.class);
-		when(config.isShouldRun()).thenReturn(true);
-		assertEquals(false, sensor.shouldExecuteOnProject(project));
-		when(config.isShouldRun()).thenReturn(false);
-		assertEquals(false, sensor.shouldExecuteOnProject(project));
-	}
+  @Test
+  public void shouldNotLaunchOnNonPhpProject() {
+    Project project = mock(Project.class);
+    when(project.getLanguage()).thenReturn(Java.INSTANCE);
+    ProjectFileSystem fs = mock(ProjectFileSystem.class);
+    when(project.getFileSystem()).thenReturn(fs);
+    when(fs.getBuildDir()).thenReturn(new File(PhpDependConfiguration.DEFAULT_REPORT_FILE_PATH));
+    Configuration configuration = mock(Configuration.class);
+    PhpDependSensor sensor = new PhpDependSensor();
+    when(configuration.getString(PhpDependConfiguration.REPORT_FILE_NAME_PROPERTY_KEY, PhpDependConfiguration.DEFAULT_REPORT_FILE_NAME))
+        .thenReturn("pdepend.xml");
+    when(
+        configuration.getString(PhpDependConfiguration.REPORT_FILE_RELATIVE_PATH_PROPERTY_KEY,
+            PhpDependConfiguration.DEFAULT_REPORT_FILE_PATH)).thenReturn("reports");
+    when(project.getConfiguration()).thenReturn(configuration);
+    PhpDependConfiguration config = mock(PhpDependConfiguration.class);
+    when(config.isShouldRun()).thenReturn(true);
+    assertEquals(false, sensor.shouldExecuteOnProject(project));
+    when(config.isShouldRun()).thenReturn(false);
+    assertEquals(false, sensor.shouldExecuteOnProject(project));
+  }
 
-	@Test
-	public void shouldLaunchOnPhpProjectIfConfiguredSo() {
-		Project project = mock(Project.class);
-		when(project.getLanguage()).thenReturn(Php.INSTANCE);
-		Configuration configuration = mock(Configuration.class);
-		PhpDependSensor sensor = new PhpDependSensor();
-		when(
-				configuration.getString(PhpDependConfiguration.REPORT_FILE_NAME_PROPERTY_KEY,
-						PhpDependConfiguration.DEFAULT_REPORT_FILE_NAME)).thenReturn("pdepend.xml");
-		when(
-				configuration.getString(PhpDependConfiguration.REPORT_FILE_NAME_PROPERTY_KEY,
-						PhpDependConfiguration.DEFAULT_REPORT_FILE_PATH)).thenReturn("reports");
-		when(
-				configuration.getBoolean(PhpDependConfiguration.SHOULD_RUN_PROPERTY_KEY, Boolean
-						.parseBoolean(PhpDependConfiguration.DEFAULT_SHOULD_RUN))).thenReturn(Boolean.TRUE);
-		when(project.getConfiguration()).thenReturn(configuration);
-		assertEquals(true, sensor.shouldExecuteOnProject(project));
-	}
+  @Test
+  public void shouldLaunchOnPhpProjectIfConfiguredSo() {
+    Project project = mock(Project.class);
+    when(project.getLanguage()).thenReturn(Php.INSTANCE);
+    ProjectFileSystem fs = mock(ProjectFileSystem.class);
+    when(project.getFileSystem()).thenReturn(fs);
+    when(fs.getBuildDir()).thenReturn(new File(PhpDependConfiguration.DEFAULT_REPORT_FILE_NAME));
+    Configuration configuration = mock(Configuration.class);
+    PhpDependSensor sensor = new PhpDependSensor();
+    when(configuration.getString(PhpDependConfiguration.REPORT_FILE_NAME_PROPERTY_KEY, PhpDependConfiguration.DEFAULT_REPORT_FILE_NAME))
+        .thenReturn("pdepend.xml");
+    when(configuration.getString(PhpDependConfiguration.REPORT_FILE_RELATIVE_PATH_PROPERTY_KEY, PhpDependConfiguration.DEFAULT_REPORT_FILE_PATH))
+        .thenReturn("reports");
+    when(
+        configuration.getBoolean(PhpDependConfiguration.SHOULD_RUN_PROPERTY_KEY, Boolean
+            .parseBoolean(PhpDependConfiguration.DEFAULT_SHOULD_RUN))).thenReturn(Boolean.TRUE);
+    when(project.getConfiguration()).thenReturn(configuration);
+    assertEquals(true, sensor.shouldExecuteOnProject(project));
+  }
 
-	@Test
-	public void shouldNotLaunchOnPhpProjectIfConfiguredSo() {
-		Project project = mock(Project.class);
-		when(project.getLanguage()).thenReturn(Php.INSTANCE);
-		Configuration configuration = mock(Configuration.class);
-		PhpDependSensor sensor = new PhpDependSensor();
-		when(
-				configuration.getString(PhpDependConfiguration.REPORT_FILE_NAME_PROPERTY_KEY,
-						PhpDependConfiguration.DEFAULT_REPORT_FILE_NAME)).thenReturn("pdepend.xml");
-		when(
-				configuration.getString(PhpDependConfiguration.REPORT_FILE_NAME_PROPERTY_KEY,
-						PhpDependConfiguration.DEFAULT_REPORT_FILE_PATH)).thenReturn("reports");
-		when(configuration.getBoolean(PhpDependConfiguration.SHOULD_RUN_PROPERTY_KEY, Boolean.FALSE)).thenReturn(Boolean.FALSE);
-		when(project.getConfiguration()).thenReturn(configuration);
-		assertEquals(false, sensor.shouldExecuteOnProject(project));
-	}
+  @Test
+  public void shouldNotLaunchOnPhpProjectIfConfiguredSo() {
+    Project project = mock(Project.class);
+    when(project.getLanguage()).thenReturn(Php.INSTANCE);
+    ProjectFileSystem fs = mock(ProjectFileSystem.class);
+    when(project.getFileSystem()).thenReturn(fs);
+    when(fs.getBuildDir()).thenReturn(new File(PhpDependConfiguration.DEFAULT_REPORT_FILE_PATH));
+    Configuration configuration = mock(Configuration.class);
+    PhpDependSensor sensor = new PhpDependSensor();
+    when(configuration.getString(PhpDependConfiguration.REPORT_FILE_NAME_PROPERTY_KEY, PhpDependConfiguration.DEFAULT_REPORT_FILE_NAME))
+        .thenReturn("pdepend.xml");
+    when(configuration.getString(PhpDependConfiguration.REPORT_FILE_RELATIVE_PATH_PROPERTY_KEY, PhpDependConfiguration.DEFAULT_REPORT_FILE_PATH))
+        .thenReturn("reports");
+    when(configuration.getBoolean(PhpDependConfiguration.SHOULD_RUN_PROPERTY_KEY, Boolean.FALSE)).thenReturn(Boolean.FALSE);
+    when(project.getConfiguration()).thenReturn(configuration);
+    assertEquals(false, sensor.shouldExecuteOnProject(project));
+  }
 }
--- php-unit/src/test/java/org/sonar/plugins/php/phpunit/sensor/PhpUnitSensorTest.java
+++ php-unit/src/test/java/org/sonar/plugins/php/phpunit/sensor/PhpUnitSensorTest.java
@@ -42,85 +42,90 @@ import org.sonar.plugins.php.phpunit.configuration.PhpUnitConfiguration;
  */
 public class PhpUnitSensorTest {
 
-	private Project project;
+  private Project project;
 
-	private Configuration configuration;
+  private Configuration configuration;
 
-	private PhpUnitSensor sensor;
+  private PhpUnitSensor sensor;
 
-	/**
-	 * Sould not launch on non php project.
-	 */
-	@Test
-	public void shouldNotLaunchOnNonPhpProject() {
-		Project project = mock(Project.class);
-		when(project.getLanguage()).thenReturn(Java.INSTANCE);
-		Configuration configuration = mock(Configuration.class);
-		PhpUnitSensor sensor = new PhpUnitSensor();
-		when(
-		    configuration.getString(PhpUnitConfiguration.REPORT_FILE_NAME_PROPERTY_KEY,
-		        PhpUnitConfiguration.DEFAULT_REPORT_FILE_NAME)).thenReturn("punit.summary.xml");
-		when(
-		    configuration.getString(PhpUnitConfiguration.COVERAGE_REPORT_FILE_PROPERTY_KEY,
-		        PhpUnitConfiguration.DEFAULT_REPORT_FILE_NAME)).thenReturn("punit.summary.xml");
-		when(
-		    configuration.getString(PhpUnitConfiguration.REPORT_FILE_RELATIVE_PATH_PROPERTY_KEY,
-		        PhpUnitConfiguration.DEFAULT_REPORT_FILE_PATH)).thenReturn("reports");
-		when(project.getConfiguration()).thenReturn(configuration);
-		PhpUnitConfiguration config = mock(PhpUnitConfiguration.class);
-		assertEquals(false, sensor.shouldExecuteOnProject(project));
-		when(project.getLanguage()).thenReturn(Php.INSTANCE);
-		when(config.isShouldRun()).thenReturn(false);
-		assertEquals(false, sensor.shouldExecuteOnProject(project));
-	}
+  /**
+   * Sould not launch on non php project.
+   */
+  @Test
+  public void shouldNotLaunchOnNonPhpProject() {
+    Project project = mock(Project.class);
+    when(project.getLanguage()).thenReturn(Java.INSTANCE);
+    Configuration configuration = mock(Configuration.class);
+    ProjectFileSystem fs = mock(ProjectFileSystem.class);
+    when(project.getFileSystem()).thenReturn(fs);
+    when(fs.getBuildDir()).thenReturn(new File(PhpUnitConfiguration.DEFAULT_REPORT_FILE_PATH));
 
-	/**
-	 * Sould not launch on non php project.
-	 */
-	@Test
-	public void shouldNotLaunchWhenConfiguredSoOnPhpProject() {
-		init();
-		when(project.getLanguage()).thenReturn(Php.INSTANCE);
-		when(
-		    configuration.getBoolean(PhpUnitConfiguration.SHOULD_RUN_PROPERTY_KEY,
-		        Boolean.getBoolean(PhpUnitConfiguration.DEFAULT_SHOULD_RUN))).thenReturn(false);
-		assertEquals(false, sensor.shouldExecuteOnProject(project));
-	}
+    PhpUnitSensor sensor = new PhpUnitSensor();
+    when(configuration.getString(PhpUnitConfiguration.REPORT_FILE_NAME_PROPERTY_KEY, PhpUnitConfiguration.DEFAULT_REPORT_FILE_NAME))
+        .thenReturn("punit.summary.xml");
+    when(configuration.getString(PhpUnitConfiguration.COVERAGE_REPORT_FILE_PROPERTY_KEY, PhpUnitConfiguration.DEFAULT_REPORT_FILE_NAME))
+        .thenReturn("punit.summary.xml");
+    when(
+        configuration.getString(PhpUnitConfiguration.REPORT_FILE_RELATIVE_PATH_PROPERTY_KEY, PhpUnitConfiguration.DEFAULT_REPORT_FILE_PATH))
+        .thenReturn("reports");
+    when(project.getConfiguration()).thenReturn(configuration);
+    PhpUnitConfiguration config = mock(PhpUnitConfiguration.class);
+    assertEquals(false, sensor.shouldExecuteOnProject(project));
+    when(project.getLanguage()).thenReturn(Php.INSTANCE);
+    when(config.isShouldRun()).thenReturn(false);
+    assertEquals(false, sensor.shouldExecuteOnProject(project));
+  }
 
-	@Test(expected = SonarException.class)
-	public void shouldThrowSonarExceptionWhenReportFileIsInvalid() {
-		init();
-		when(
-		    configuration.getString(PhpUnitConfiguration.REPORT_FILE_NAME_PROPERTY_KEY,
-		        PhpUnitConfiguration.DEFAULT_REPORT_FILE_NAME)).thenReturn("punit-invalid.summary.xml");
-		when(
-		    configuration.getBoolean(PhpUnitConfiguration.ANALYZE_ONLY_PROPERTY_KEY,
-		        Boolean.getBoolean(PhpUnitConfiguration.DEFAULT_ANALYZE_ONLY))).thenReturn(true);
-		sensor.analyse(project, null);
-	}
+  /**
+   * Sould not launch on non php project.
+   */
+  @Test
+  public void shouldNotLaunchWhenConfiguredSoOnPhpProject() {
+    init();
+    when(project.getLanguage()).thenReturn(Php.INSTANCE);
+    when(
+        configuration.getBoolean(PhpUnitConfiguration.SHOULD_RUN_PROPERTY_KEY, Boolean.getBoolean(PhpUnitConfiguration.DEFAULT_SHOULD_RUN)))
+        .thenReturn(false);
+    ProjectFileSystem fs = mock(ProjectFileSystem.class);
+    when(project.getFileSystem()).thenReturn(fs);
+    when(fs.getBuildDir()).thenReturn(new File(PhpUnitConfiguration.DEFAULT_REPORT_FILE_PATH));
+    assertEquals(false, sensor.shouldExecuteOnProject(project));
+  }
 
-	private void init() {
-		project = mock(Project.class);
-		configuration = mock(Configuration.class);
-		sensor = new PhpUnitSensor();
-		when(
-		    configuration.getString(PhpUnitConfiguration.REPORT_FILE_NAME_PROPERTY_KEY,
-		        PhpUnitConfiguration.DEFAULT_REPORT_FILE_NAME)).thenReturn("punit.summary.xml");
-		when(
-		    configuration.getString(PhpUnitConfiguration.REPORT_FILE_RELATIVE_PATH_PROPERTY_KEY,
-		        PhpUnitConfiguration.DEFAULT_REPORT_FILE_PATH)).thenReturn("/log");
-		when(
-		    configuration.getString(PhpUnitConfiguration.COVERAGE_REPORT_FILE_PROPERTY_KEY,
-		        PhpUnitConfiguration.DEFAULT_COVERAGE_REPORT_FILE)).thenReturn("phpunit.coverage.xml");
-		when(
-		    configuration.getBoolean(PhpUnitConfiguration.SHOULD_RUN_PROPERTY_KEY,
-		        Boolean.getBoolean(PhpUnitConfiguration.DEFAULT_SHOULD_RUN))).thenReturn(true);
-		when(project.getConfiguration()).thenReturn(configuration);
-		MavenProject mavenProject = mock(MavenProject.class);
-		ProjectFileSystem fs = mock(ProjectFileSystem.class);
-		when(project.getPom()).thenReturn(mavenProject);
-		when(project.getFileSystem()).thenReturn(fs);
-		when(fs.getSourceDirs()).thenReturn(Arrays.asList(new File("C:\\projets\\PHP\\Monkey\\sources\\main")));
-		when(fs.getTestDirs()).thenReturn(Arrays.asList(new File("C:\\projets\\PHP\\Monkey\\sources\\test")));
-	}
+  @Test(expected = SonarException.class)
+  public void shouldThrowSonarExceptionWhenReportFileIsInvalid() {
+    init();
+    when(configuration.getString(PhpUnitConfiguration.REPORT_FILE_NAME_PROPERTY_KEY, PhpUnitConfiguration.DEFAULT_REPORT_FILE_NAME))
+        .thenReturn("punit-invalid.summary.xml");
+    when(
+        configuration.getBoolean(PhpUnitConfiguration.ANALYZE_ONLY_PROPERTY_KEY, Boolean
+            .getBoolean(PhpUnitConfiguration.DEFAULT_ANALYZE_ONLY))).thenReturn(true);
+    sensor.analyse(project, null);
+  }
+
+  private void init() {
+    project = mock(Project.class);
+    configuration = mock(Configuration.class);
+
+    sensor = new PhpUnitSensor();
+    when(configuration.getString(PhpUnitConfiguration.REPORT_FILE_NAME_PROPERTY_KEY, PhpUnitConfiguration.DEFAULT_REPORT_FILE_NAME))
+        .thenReturn("punit.summary.xml");
+    when(
+        configuration.getString(PhpUnitConfiguration.REPORT_FILE_RELATIVE_PATH_PROPERTY_KEY, PhpUnitConfiguration.DEFAULT_REPORT_FILE_PATH))
+        .thenReturn("/log");
+    when(configuration.getString(PhpUnitConfiguration.COVERAGE_REPORT_FILE_PROPERTY_KEY, PhpUnitConfiguration.DEFAULT_COVERAGE_REPORT_FILE))
+        .thenReturn("phpunit.coverage.xml");
+    when(
+        configuration.getBoolean(PhpUnitConfiguration.SHOULD_RUN_PROPERTY_KEY, Boolean.getBoolean(PhpUnitConfiguration.DEFAULT_SHOULD_RUN)))
+        .thenReturn(true);
+    when(project.getConfiguration()).thenReturn(configuration);
+    MavenProject mavenProject = mock(MavenProject.class);
+    ProjectFileSystem fs = mock(ProjectFileSystem.class);
+    when(project.getFileSystem()).thenReturn(fs);
+    when(fs.getBuildDir()).thenReturn(new File(PhpUnitConfiguration.DEFAULT_REPORT_FILE_PATH));
+    when(project.getPom()).thenReturn(mavenProject);
+    when(project.getFileSystem()).thenReturn(fs);
+    when(fs.getSourceDirs()).thenReturn(Arrays.asList(new File("C:\\projets\\PHP\\Monkey\\sources\\main")));
+    when(fs.getTestDirs()).thenReturn(Arrays.asList(new File("C:\\projets\\PHP\\Monkey\\sources\\test")));
+  }
 }
