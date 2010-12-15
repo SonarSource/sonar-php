91e86540f92d05cc5561268b3087f4a3a6589cbd
Akram Ben Aissi <akram.benaissi@gmail.com>
2010-12-15 14:45:52 +0000

    code cleanup

--- sonar-php-plugin/src/main/java/org/sonar/plugins/php/core/PhpProfileImporter.java
+++ sonar-php-plugin/src/main/java/org/sonar/plugins/php/core/PhpProfileImporter.java
@@ -1,6 +1,6 @@
 /*
  * Sonar, open source software quality management tool.
- * Copyright (C) 2010 EchoSource
+ * Copyright (C) 2010 Akram Ben Aissi
  * mailto:contact AT sonarsource DOT com
  *
  * Sonar is free software; you can redistribute it and/or
