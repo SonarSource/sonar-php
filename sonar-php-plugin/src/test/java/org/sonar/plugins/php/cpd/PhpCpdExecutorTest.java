fb1db1bb32857dce7861fc20fbff24a65ea0ce46
Akram Ben Aissi <akram.benaissi@gmail.com>
2010-12-15 22:58:28 +0000

    unit test fix for unix filesystems

--- sonar-php-plugin/src/test/java/org/sonar/plugins/php/cpd/PhpCpdExecutorTest.java
+++ sonar-php-plugin/src/test/java/org/sonar/plugins/php/cpd/PhpCpdExecutorTest.java
@@ -42,37 +42,44 @@ import org.sonar.plugins.php.core.PhpPlugin;
  */
 public class PhpCpdExecutorTest {
 
-  /**
-   * Test method for {@link org.sonar.plugins.php.codesniffer.PhpCpdExecutor#getPhpcpdCommandLine()}.
-   */
-  @Test
-  public void testGetCommandLine() {
+	/**
+	 * Test method for
+	 * {@link org.sonar.plugins.php.codesniffer.PhpCpdExecutor#getPhpcpdCommandLine()}
+	 * .
+	 */
+	@Test
+	public void testGetCommandLine() {
 
-    Configuration configuration = mock(Configuration.class);
-    when(configuration.getStringArray(PhpPlugin.FILE_SUFFIXES_KEY)).thenReturn(null);
-    PhpCpdConfiguration c = mock(PhpCpdConfiguration.class);
-    when(c.getMinimunNumberOfIdenticalLines()).thenReturn(PHPCPD_DEFAULT_MINIMUM_NUMBER_OF_IDENTICAL_LINES);
-    when(c.getMinimunNumberOfIdenticalTokens()).thenReturn(PHPCPD_DEFAULT_MINIMUM_NUMBER_OF_IDENTICAL_TOKENS);
+		Configuration configuration = mock(Configuration.class);
+		when(configuration.getStringArray(PhpPlugin.FILE_SUFFIXES_KEY))
+				.thenReturn(null);
+		PhpCpdConfiguration c = mock(PhpCpdConfiguration.class);
+		when(c.getMinimunNumberOfIdenticalLines()).thenReturn(
+				PHPCPD_DEFAULT_MINIMUM_NUMBER_OF_IDENTICAL_LINES);
+		when(c.getMinimunNumberOfIdenticalTokens()).thenReturn(
+				PHPCPD_DEFAULT_MINIMUM_NUMBER_OF_IDENTICAL_TOKENS);
 
-    String reportFile = "C:\\projets\\PHP\\Monkey\\target\\logs\\php-cpd.xml";
-    when(c.getReportFile()).thenReturn(new File(reportFile));
-    when(c.isOsWindows()).thenReturn(false);
-    String phpcpdScriptName = "phpcpd";
-    when(c.getOsDependentToolScriptName()).thenReturn(phpcpdScriptName);
+		String reportFile = "C:\\projets\\PHP\\Monkey\\target\\logs\\php-cpd.xml";
+		when(c.getReportFile()).thenReturn(new File(reportFile));
+		when(c.isOsWindows()).thenReturn(false);
+		String phpcpdScriptName = "phpcpd";
+		when(c.getOsDependentToolScriptName()).thenReturn(phpcpdScriptName);
 
-    PhpCpdExecutor executor = new PhpCpdExecutor(c);
-    List<String> commandLine = executor.getCommandLine();
-    List<String> expected = new ArrayList<String>();
-    expected.add(phpcpdScriptName);
+		PhpCpdExecutor executor = new PhpCpdExecutor(c);
+		List<String> commandLine = executor.getCommandLine();
+		List<String> expected = new ArrayList<String>();
+		expected.add(phpcpdScriptName);
 
-    expected.add(PHPCPD_MINIMUM_NUMBER_OF_IDENTICAL_LINES_MODIFIER);
-    expected.add(PHPCPD_DEFAULT_MINIMUM_NUMBER_OF_IDENTICAL_LINES.toString());
+		expected.add(PHPCPD_MINIMUM_NUMBER_OF_IDENTICAL_LINES_MODIFIER);
+		expected.add(PHPCPD_DEFAULT_MINIMUM_NUMBER_OF_IDENTICAL_LINES
+				.toString());
 
-    expected.add(PHPCPD_MINIMUM_NUMBER_OF_IDENTICAL_TOKENS_MODIFIER);
-    expected.add(PHPCPD_DEFAULT_MINIMUM_NUMBER_OF_IDENTICAL_TOKENS.toString());
-    expected.add("--log-pmd");
-    expected.add(reportFile);
+		expected.add(PHPCPD_MINIMUM_NUMBER_OF_IDENTICAL_TOKENS_MODIFIER);
+		expected.add(PHPCPD_DEFAULT_MINIMUM_NUMBER_OF_IDENTICAL_TOKENS
+				.toString());
+		expected.add("--log-pmd");
+		expected.add(new File(reportFile).getAbsolutePath());
 
-    assertEquals(expected, commandLine);
-  }
+		assertEquals(expected, commandLine);
+	}
 }
