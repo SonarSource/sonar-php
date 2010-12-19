1476320befa24bd0e8ecc5838639388996f5d2f9
Akram Ben Aissi <akram.benaissi@gmail.com>
2010-12-19 22:49:19 +0000

    adding unit test for PhpRuleRepository

---
+++ sonar-php-plugin/src/test/java/org/sonar/plugins/php/pmd/PhpmdRulesRepositoryTest.java
@@ -0,0 +1,58 @@
+/*
+ * Sonar Flex Plugin
+ * Copyright (C) 2010 SonarSource
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+
+package org.sonar.plugins.php.pmd;
+
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.sonar.api.platform.ServerFileSystem;
+import org.sonar.api.rules.Rule;
+import org.sonar.api.rules.XMLRuleParser;
+
+public class PhpmdRulesRepositoryTest {
+
+  private PhpmdRuleRepository repository;
+
+  @Before
+  public void setup() {
+    ServerFileSystem fs = mock(ServerFileSystem.class);
+    XMLRuleParser parser = new XMLRuleParser();
+    repository = new PhpmdRuleRepository(fs, parser);
+  }
+
+  @Test
+  public void testCreateRules() {
+    List<Rule> rules = repository.createRules();
+    for (Rule rule : rules) {
+      assertNotNull(rule.getKey());
+      assertNotNull(rule.getDescription());
+      assertNotNull(rule.getName());
+      assertNotNull(rule.getRulesCategory());
+      assertNotNull(rule.getPriority());
+      // assertNotNull(rule.getPluginName());
+    }
+  }
+
+}
