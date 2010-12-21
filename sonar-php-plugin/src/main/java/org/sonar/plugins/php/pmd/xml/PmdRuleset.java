1955bccd177b1e99f88bb61c5204a3d18a1e1059
Akram Ben Aissi <akram.benaissi@gmail.com>
2010-12-21 21:10:37 +0000

    code coverage

--- sonar-php-plugin/src/main/java/org/sonar/plugins/php/pmd/xml/PmdRuleset.java
+++ sonar-php-plugin/src/main/java/org/sonar/plugins/php/pmd/xml/PmdRuleset.java
@@ -42,10 +42,6 @@ public class PmdRuleset {
     return rules;
   }
 
-  public void setRules(List<PmdRule> rules) {
-    this.rules = rules;
-  }
-
   /**
    * @return the name
    */
@@ -53,20 +49,8 @@ public class PmdRuleset {
     return name;
   }
 
-  /**
-   * @param name
-   *          the name to set
-   */
-  public void setName(String name) {
-    this.name = name;
-  }
-
   public String getDescription() {
     return description;
-  }
-
-  public void setDescription(String description) {
-    this.description = description;
   }
 
   public void addRule(PmdRule rule) {
