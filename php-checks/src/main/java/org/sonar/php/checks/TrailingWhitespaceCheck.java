3b1c3b667f0776c8405ed4555fbe6df1ee28ce0b
Linda Martin <linda.martin@sonarsource.com>
2015-04-22 12:06:15 +0200

    SONARPHP-435 [S1131]
    Lines should not end with trailing whitespaces

--- php-checks/src/main/java/org/sonar/php/checks/TrailingWhitespaceCheck.java
+++ php-checks/src/main/java/org/sonar/php/checks/TrailingWhitespaceCheck.java
@@ -40,7 +40,7 @@ import java.util.regex.Pattern;
 @Rule(
   key = "S1131",
   name = "Lines should not end with trailing whitespaces",
-  priority = Priority.MAJOR,
+  priority = Priority.MINOR,
   tags = {Tags.CONVENTION, Tags.PSR2})
 @SqaleSubCharacteristic(RulesDefinition.SubCharacteristics.READABILITY)
 @SqaleConstantRemediation("1min")
