d1dbd7e8a5ffc8fd2901e0b574a3f5ef7b1aac19
Linda Martin <linda.martin@sonarsource.com>
2015-04-24 14:28:40 +0200

    Remove useless import.

--- php-checks/src/main/java/org/sonar/php/checks/ExitOrDieUsageCheck.java
+++ php-checks/src/main/java/org/sonar/php/checks/ExitOrDieUsageCheck.java
@@ -19,9 +19,7 @@
  */
 package org.sonar.php.checks;
 
-import com.sonar.sslr.api.AstNode;
 import org.sonar.api.server.rule.RulesDefinition;
-import org.sonar.check.BelongsToProfile;
 import org.sonar.check.Priority;
 import org.sonar.check.Rule;
 import org.sonar.php.parser.PHPGrammar;
@@ -29,6 +27,8 @@ import org.sonar.squidbridge.annotations.SqaleConstantRemediation;
 import org.sonar.squidbridge.annotations.SqaleSubCharacteristic;
 import org.sonar.squidbridge.checks.SquidCheck;
 import org.sonar.sslr.parser.LexerlessGrammar;
+
+import com.sonar.sslr.api.AstNode;
 
 @Rule(
   key = "S1799",
