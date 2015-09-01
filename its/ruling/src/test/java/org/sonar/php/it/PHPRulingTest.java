9a77e79327b81850ee3852f3a5f9a8df8d5a972d
Linda Martin <linda.martin@sonarsource.com>
2015-09-01 12:59:27 +0200

    Integration Tests ruling build on travis.

---
+++ its/ruling/src/test/java/org/sonar/php/it/PHPRulingTest.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2014 SonarSource SA
+ * All rights reserved
+ * mailto:contact AT sonarsource DOT com
+ */
+package org.sonar.php.it;
+
+import com.google.common.io.Files;
+import com.sonar.orchestrator.Orchestrator;
+import com.sonar.orchestrator.build.SonarRunner;
+import com.sonar.orchestrator.locator.FileLocation;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+
+import static org.fest.assertions.Assertions.assertThat;
+import static org.junit.Assert.assertTrue;
+
+public class PHPRulingTest {
+
+  private static final String PLUGIN_KEY = "php";
+  private static final File LITS_DIFFERENCES_FILE = FileLocation.of("target/differences").getFile();
+
+  @ClassRule
+  public static Orchestrator ORCHESTRATOR = Orchestrator.builderEnv()
+    .addPlugin(PLUGIN_KEY)
+    .setMainPluginKey(PLUGIN_KEY)
+    .setOrchestratorProperty("litsVersion", "0.5")
+    .addPlugin("lits")
+    .restoreProfileAtStartup(FileLocation.of("src/test/resources/profile.xml"))
+    .build();
+
+  @Test
+  public void test() throws Exception {
+    assertTrue(
+      "SonarQube 5.1 is the minimum version to generate the issues report, change your orchestrator.properties",
+      ORCHESTRATOR.getConfiguration().getSonarVersion().isGreaterThanOrEquals("5.1"));
+    SonarRunner build = SonarRunner.create(FileLocation.of("../sources/src").getFile())
+      .setProjectKey("project")
+      .setProjectName("project")
+      .setProjectVersion("1")
+      .setSourceDirs(".")
+      .setSourceEncoding("UTF-8")
+      .setProfile("rules")
+      .setProperty("sonar.analysis.mode", "preview")
+      .setProperty("sonar.issuesReport.html.enable", "true")
+      .setProperty("dump.old", FileLocation.of("src/test/resources/expected").getFile().getAbsolutePath())
+      .setProperty("dump.new", FileLocation.of("target/actual").getFile().getAbsolutePath())
+      .setProperty("sonar.cpd.skip", "true")
+      .setProperty("lits.differences", LITS_DIFFERENCES_FILE.getAbsolutePath())
+      .setEnvironmentVariable("SONAR_RUNNER_OPTS", "-Xmx1000m");
+    ORCHESTRATOR.executeBuild(build);
+
+    assertThat(Files.toString(LITS_DIFFERENCES_FILE, StandardCharsets.UTF_8)).isEmpty();
+  }
+
+}
--- travis.sh
+++ travis.sh
@@ -8,30 +8,28 @@ function installTravisTools {
   source ~/.local/bin/install
 }
 
-case "$TESTS" in
+case "$TEST" in
 
-CI)
+ci)
   mvn verify -B -e -V
   ;;
 
-IT-DEV)
+plugin|ruling)
   installTravisTools
 
-  mvn install -T2 -Dsource.skip=true -Denforcer.skip=true -Danimal.sniffer.skip=true -Dmaven.test.skip=true
+  mvn package -Dsource.skip=true -Denforcer.skip=true -Danimal.sniffer.skip=true -Dmaven.test.skip=true
 
-  build_snapshot "SonarSource/sonarqube"
+  if [ "$SQ_VERSION" = "DEV" ] ; then
+    build_snapshot "SonarSource/sonarqube"
+  fi
 
-  cd its/plugin
-  mvn -DphpVersion="DEV" -Dsonar.runtimeVersion="DEV" -Dmaven.test.redirectTestOutputToFile=false install
+  cd its/$TEST
+  mvn -DphpVersion="DEV" -Dsonar.runtimeVersion="$SQ_VERSION" -Dmaven.test.redirectTestOutputToFile=false install
   ;;
 
-IT-LTS)
-  installTravisTools
-
-  mvn install -T2 -Dsource.skip=true -Denforcer.skip=true -Danimal.sniffer.skip=true -Dmaven.test.skip=true
-
-  cd its/plugin
-  mvn -DphpVersion="DEV" -Dsonar.runtimeVersion="LTS_OR_OLDEST_COMPATIBLE" -Dmaven.test.redirectTestOutputToFile=false install
+*)
+  echo "Unexpected TEST mode: $TEST"
+  exit 1
   ;;
 
 esac
