35a489cbd1160a04d3a4311c71f2ccaa7a866acf
Elena Vilchik <elena.vilchik@sonarsource.com>
2015-09-22 14:01:40 +0200

    Migrate YIELD_STATEMENT

---
+++ php-frontend/src/main/java/org/sonar/php/tree/impl/statement/YieldStatementTreeImpl.java
@@ -0,0 +1,69 @@
+/*
+ * SonarQube PHP Plugin
+ * Copyright (C) 2010 SonarSource and Akram Ben Aissi
+ * sonarqube@googlegroups.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonar.php.tree.impl.statement;
+
+import com.google.common.collect.Iterators;
+import org.sonar.php.tree.impl.PHPTree;
+import org.sonar.php.tree.impl.lexical.InternalSyntaxToken;
+import org.sonar.plugins.php.api.tree.Tree;
+import org.sonar.plugins.php.api.tree.expression.YieldExpressionTree;
+import org.sonar.plugins.php.api.tree.lexical.SyntaxToken;
+import org.sonar.plugins.php.api.tree.statement.YieldStatementTree;
+import org.sonar.plugins.php.api.visitors.TreeVisitor;
+
+import java.util.Iterator;
+
+public class YieldStatementTreeImpl extends PHPTree implements YieldStatementTree {
+
+  private static final Kind KIND = Kind.YIELD_STATEMENT;
+
+  private final YieldExpressionTree yieldExpression;
+  private final InternalSyntaxToken eosToken;
+
+  public YieldStatementTreeImpl(YieldExpressionTree yieldExpression, InternalSyntaxToken eosToken) {
+    this.yieldExpression = yieldExpression;
+    this.eosToken = eosToken;
+  }
+
+  @Override
+  public Kind getKind() {
+    return KIND;
+  }
+
+  @Override
+  public Iterator<Tree> childrenIterator() {
+    return Iterators.forArray(yieldExpression, eosToken);
+  }
+
+  @Override
+  public YieldExpressionTree yieldExpression() {
+    return yieldExpression;
+  }
+
+  @Override
+  public SyntaxToken eosToken() {
+    return eosToken;
+  }
+
+  @Override
+  public void accept(TreeVisitor visitor) {
+    visitor.visitYieldStatement(this);
+  }
+}
