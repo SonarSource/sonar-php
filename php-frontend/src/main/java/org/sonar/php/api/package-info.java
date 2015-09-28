7ae38f1ed5afca8acdfc392a693185f1454c3ed7
Elena Vilchik <elena.vilchik@sonarsource.com>
2015-09-28 14:49:30 +0200

    Add `package-info.java` files

--- php-frontend/src/main/java/org/sonar/php/api/package-info.java
+++ php-frontend/src/main/java/org/sonar/php/api/package-info.java
@@ -18,4 +18,4 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
  */
 @javax.annotation.ParametersAreNonnullByDefault
-package org.sonar.php.parser;
+package org.sonar.php.api;
