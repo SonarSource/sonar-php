5587acfa9b86c68be19372ec3fc3f503d52fd5a0
Elena Vilchik <elena.vilchik@sonarsource.com>
2015-10-02 17:17:11 +0200

    SONARPHP-507 Migrate RSPEC-121

--- php-checks/src/test/java/org/sonar/php/checks/AlwaysUseCurlyBracesCheckTest.java
+++ php-checks/src/test/java/org/sonar/php/checks/AlwaysUseCurlyBracesCheckTest.java
@@ -20,25 +20,13 @@
 package org.sonar.php.checks;
 
 import org.junit.Test;
-import org.sonar.php.PHPAstScanner;
-import org.sonar.plugins.php.CheckTest;
 import org.sonar.plugins.php.TestUtils;
-import org.sonar.squidbridge.api.SourceFile;
+import org.sonar.plugins.php.api.tests.PHPCheckTest;
 
-public class AlwaysUseCurlyBracesCheckTest extends CheckTest {
+public class AlwaysUseCurlyBracesCheckTest {
 
   @Test
   public void test() throws Exception {
-    SourceFile file = PHPAstScanner.scanSingleFile(TestUtils.getCheckFile("AlwaysUseCurlyBracesCheck.php"), new AlwaysUseCurlyBracesCheck());
-
-    checkMessagesVerifier.verify(file.getCheckMessages())
-      .next().atLine(5).withMessage("Add curly braces around the nested statement(s).")
-      .next().atLine(7)
-      .next().atLine(9)
-      .next().atLine(11)
-      .next().atLine(13)
-      .next().atLine(18)
-      .next().atLine(22)
-      .noMore();
+    PHPCheckTest.check(new AlwaysUseCurlyBracesCheck(), TestUtils.getCheckFile("AlwaysUseCurlyBracesCheck.php"));
   }
 }
