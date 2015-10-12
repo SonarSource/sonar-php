0663f503a8ea8c0b8fbf308e5ae8a212c584885c
Pierre-Yves Nicolas <pierre-yves.nicolas@sonarsource.com>
2015-10-12 12:32:37 +0200

    Syntactic equivalence

--- php-checks/src/main/java/org/sonar/php/checks/utils/CheckUtils.java
+++ php-checks/src/main/java/org/sonar/php/checks/utils/CheckUtils.java
@@ -107,6 +107,39 @@ public class CheckUtils {
     }
     return true;
   }
+  
+  public static boolean areSyntacticallyEquivalent(@Nullable Tree tree1, @Nullable Tree tree2) {
+    if (tree1 == tree2) {
+      return true;
+    }
+
+    if (tree1 == null || tree2 == null) {
+      return false;
+    }
+
+    PHPTree phpTree1 = (PHPTree) tree1;
+    PHPTree phpTree2 = (PHPTree) tree2;
+
+    if (phpTree1.getKind() != phpTree2.getKind()) {
+      return false;
+    } else if (phpTree1.isLeaf()) {
+      return phpTree1.getFirstToken().text().equals(phpTree2.getFirstToken().text());
+    }
+
+    Iterator<Tree> iterator1 = phpTree1.childrenIterator();
+    Iterator<Tree> iterator2 = phpTree2.childrenIterator();
+    return areSyntacticallyEquivalent(iterator1, iterator2);
+  }
+
+  private static boolean areSyntacticallyEquivalent(Iterator<Tree> iterator1, Iterator<Tree> iterator2) {
+    while (iterator1.hasNext() && iterator2.hasNext()) {
+      if (!areSyntacticallyEquivalent(iterator1.next(), iterator2.next())) {
+        return false;
+      }
+    }
+
+    return !iterator1.hasNext() && !iterator2.hasNext();
+  }
 
   public static String asString(Tree tree) {
     if (tree.is(Tree.Kind.TOKEN)) {
@@ -142,4 +175,5 @@ public class CheckUtils {
   private static boolean isSpaceRequired(SyntaxToken prevToken, SyntaxToken token) {
     return (token.line() > prevToken.line()) || (prevToken.column() + prevToken.text().length() < token.column());
   }
+
 }
---
+++ php-checks/src/test/java/org/sonar/php/checks/utils/CheckUtilsTest.java
@@ -0,0 +1,56 @@
+/*
+ * SonarQube PHP Plugin
+ * Copyright (C) 2010 SonarSource and Akram Ben Aissi
+ * sonarqube@googlegroups.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonar.php.checks.utils;
+
+import com.google.common.base.Charsets;
+import com.sonar.sslr.api.typed.ActionParser;
+import org.junit.Test;
+import org.sonar.php.parser.PHPLexicalGrammar;
+import org.sonar.php.parser.PHPParserBuilder;
+import org.sonar.plugins.php.api.tree.Tree;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+public class CheckUtilsTest {
+
+  private ActionParser<Tree> parser = PHPParserBuilder.createParser(PHPLexicalGrammar.TOP_STATEMENT, Charsets.UTF_8);
+
+  @Test
+  public void areSyntacticallyEquivalent() throws Exception {
+    assertThat(areSyntacticallyEquivalent(null, null)).isTrue();
+    assertThat(areSyntacticallyEquivalent(null, "1;")).isFalse();
+    assertThat(areSyntacticallyEquivalent("$x;", null)).isFalse();
+    assertThat(areSyntacticallyEquivalent("$x;", "1;")).isFalse();
+    assertThat(areSyntacticallyEquivalent("$x;", "$x;")).isTrue();
+    assertThat(areSyntacticallyEquivalent("$x;", "$y;")).isFalse();
+    assertThat(areSyntacticallyEquivalent("switch ($a) {case 1:}", "switch ($a) {case 1:}")).isTrue();
+    assertThat(areSyntacticallyEquivalent("switch ($a) {case 1:}", "switch ($a) {case 1: case2:}")).isFalse();
+    assertThat(areSyntacticallyEquivalent("switch ($a) {case 1: case2:}", "switch ($a) {case 1:}")).isFalse();
+  }
+
+  private boolean areSyntacticallyEquivalent(String toParse1, String toParse2) throws Exception {
+    return CheckUtils.areSyntacticallyEquivalent(parse(toParse1), parse(toParse2));
+  }
+
+  private Tree parse(String toParse) {
+    return toParse == null ? null : parser.parse(toParse);
+  }
+
+}
