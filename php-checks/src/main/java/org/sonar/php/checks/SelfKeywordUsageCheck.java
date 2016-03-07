d899070b16f752e2932ae5dc08b462cecd97d78d
Elena Vilchik <elena.vilchik@sonarsource.com>
2016-03-07 15:03:59 +0100

    SONARPHP-598 False Positive SelfKeywordUsage (S2037): methods which cannot be overridden

--- php-checks/src/main/java/org/sonar/php/checks/SelfKeywordUsageCheck.java
+++ php-checks/src/main/java/org/sonar/php/checks/SelfKeywordUsageCheck.java
@@ -19,12 +19,22 @@
  */
 package org.sonar.php.checks;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.Set;
 import org.sonar.api.server.rule.RulesDefinition;
 import org.sonar.check.Priority;
 import org.sonar.check.Rule;
+import org.sonar.php.checks.utils.CheckUtils;
 import org.sonar.plugins.php.api.tree.Tree;
+import org.sonar.plugins.php.api.tree.Tree.Kind;
+import org.sonar.plugins.php.api.tree.declaration.ClassDeclarationTree;
+import org.sonar.plugins.php.api.tree.declaration.ClassMemberTree;
 import org.sonar.plugins.php.api.tree.declaration.ClassPropertyDeclarationTree;
+import org.sonar.plugins.php.api.tree.declaration.MethodDeclarationTree;
 import org.sonar.plugins.php.api.tree.expression.MemberAccessTree;
+import org.sonar.plugins.php.api.tree.expression.NameIdentifierTree;
 import org.sonar.plugins.php.api.visitors.PHPVisitorCheck;
 import org.sonar.squidbridge.annotations.ActivatedByDefault;
 import org.sonar.squidbridge.annotations.SqaleConstantRemediation;
@@ -43,6 +53,38 @@ public class SelfKeywordUsageCheck extends PHPVisitorCheck {
   public static final String KEY = "S2037";
   private static final String MESSAGE = "Use \"static\" keyword instead of \"self\".";
 
+  private Deque<Boolean> isFinalClassStack = new ArrayDeque<>();
+  private Deque<Set<String>> finalMethodsStack = new ArrayDeque<>();
+
+  @Override
+  public void visitClassDeclaration(ClassDeclarationTree tree) {
+    isFinalClassStack.addLast(isFinalClass(tree));
+    finalMethodsStack.addLast(getFinalMethods(tree));
+
+    super.visitClassDeclaration(tree);
+
+    isFinalClassStack.removeLast();
+    finalMethodsStack.removeLast();
+  }
+
+  private static Set<String> getFinalMethods(ClassDeclarationTree tree) {
+    Set<String> finalMethods = new HashSet<>();
+
+    for (ClassMemberTree classMemberTree : tree.members()) {
+      if (classMemberTree.is(Kind.METHOD_DECLARATION)) {
+        MethodDeclarationTree methodDeclaration = (MethodDeclarationTree) classMemberTree;
+        if (CheckUtils.hasModifier(methodDeclaration.modifiers(), "final")) {
+          finalMethods.add(methodDeclaration.name().text());
+        }
+      }
+    }
+    return finalMethods;
+  }
+
+  private static boolean isFinalClass(ClassDeclarationTree tree) {
+    return tree.modifierToken() != null && "final".equals(tree.modifierToken().text());
+  }
+
   @Override
   public void visitClassPropertyDeclaration(ClassPropertyDeclarationTree tree) {
     // don't enter inside class property declarations
@@ -50,11 +92,22 @@ public class SelfKeywordUsageCheck extends PHPVisitorCheck {
 
   @Override
   public void visitMemberAccess(MemberAccessTree tree) {
-    if (tree.is(Tree.Kind.CLASS_MEMBER_ACCESS) && "self".equals(tree.object().toString())) {
+    if (tree.is(Tree.Kind.CLASS_MEMBER_ACCESS) && "self".equals(tree.object().toString()) && !isException(tree)) {
       context().newIssue(this, MESSAGE).tree(tree);
     }
 
     super.visitMemberAccess(tree);
+  }
+
+  /**
+   * Return true if member can't be overridden
+   */
+  private boolean isException(MemberAccessTree tree) {
+    return !isFinalClassStack.isEmpty() && (isFinalClassStack.getLast() || isFinalMethod(tree.member()));
+  }
+
+  private boolean isFinalMethod(Tree member) {
+    return member.is(Kind.NAME_IDENTIFIER) && finalMethodsStack.getLast().contains(((NameIdentifierTree) member).text());
   }
 
 }
--- php-checks/src/test/resources/checks/SelfKeywordUsageCheck.php
+++ php-checks/src/test/resources/checks/SelfKeywordUsageCheck.php
@@ -16,6 +16,8 @@ class A {
   );
 
   public static function f1() {
+    self::f2();                // NOK
+    self::f3();                // OK, "f3" can't be overridden
     return self::$field;       // NOK {{Use "static" keyword instead of "self".}}
   }
 
@@ -23,4 +25,35 @@ class A {
     return static::$field;     // OK
   }
 
+  public static final function f3() {
+  }
+
+}
+
+final class B {
+  public static $field;
+
+  public static function f1() {
+    self::f2();                // OK, class is final
+    return self::$field;       // OK, class is final
+  }
+
+  public static function f2() {}
+}
+
+class D {
+  public static function f1() {
+    self::f2();      // NOK
+    class C {
+      static $field;
+      public static function f1() {
+        self::f2();                // OK
+        return self::$field;       // NOK
+      }
+
+      public static final function f2() {}
+    }
+  }
+
+  public static function f2() {}
 }
