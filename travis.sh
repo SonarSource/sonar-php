3e9226354159abd0e79debd6ac91189ac97adf31
Tom <thomas.verin@sonarsource.com>
2016-03-17 11:10:19 +0100

    setup QA

--- travis.sh
+++ travis.sh
@@ -6,32 +6,20 @@ function cleanPhpInMavenRepository {
   rm -rf ~/.m2/repository/org/sonarsource/php
 }
 
-function installTravisTools {
+function configureTravis {
   mkdir ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v21 | tar zx --strip-components 1 -C ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v27 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
 }
+
+configureTravis
 
 case "$TEST" in
 
 ci)
-  mvn verify -B -e -V
+  regular_mvn_build_deploy_analyze  
   ;;
 
-plugin|ruling)
-  installTravisTools
-
-  cleanPhpInMavenRepository # make sure we don't use an old version from the travis cache
-  mvn install -Dsource.skip=true -Denforcer.skip=true -Danimal.sniffer.skip=true -Dmaven.test.skip=true
-
-  if [ "$SQ_VERSION" = "DEV" ] ; then
-    build_snapshot "SonarSource/sonarqube"
-  fi
-
-  cd its/$TEST
-  mvn -Dsonar.runtimeVersion="$SQ_VERSION" -Dmaven.test.redirectTestOutputToFile=false package
-  cleanPhpInMavenRepository # avoid adding snapshot to the travis cache
-  ;;
 
 *)
   echo "Unexpected TEST mode: $TEST"
