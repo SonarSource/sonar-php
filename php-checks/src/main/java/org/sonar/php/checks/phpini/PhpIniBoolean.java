0a6d21deccf1bbede4f442479b214dcdb506be4c
Pierre-Yves Nicolas <pierre-yves.nicolas@sonarsource.com>
2016-10-13 17:44:24 +0200

    SONARPHP-640 Rule: "enable_dl" should be set to 0

---
+++ php-checks/src/main/java/org/sonar/php/checks/phpini/PhpIniBoolean.java
@@ -0,0 +1,53 @@
+/*
+ * SonarQube PHP Plugin
+ * Copyright (C) 2010-2016 SonarSource SA
+ * mailto:contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.php.checks.phpini;
+
+import com.google.common.collect.ImmutableSet;
+import java.util.Locale;
+import java.util.Set;
+import org.sonar.php.ini.tree.Directive;
+
+public enum PhpIniBoolean {
+
+  ON(ImmutableSet.of("1", "ON", "TRUE", "YES")),
+  OFF(ImmutableSet.of("0", "OFF", "FALSE", "NO"));
+
+  private final Set<String> variants;
+
+  PhpIniBoolean(Set<String> variants) {
+    this.variants = variants;
+  }
+
+  public boolean matchesValue(Directive directive) {
+    return matchesValue(directive.value().text());
+  }
+
+  public boolean matchesValue(String rawValue) {
+    String value = rawValue;
+    if (isQuotedValue(value, "\"") || isQuotedValue(value, "'")) {
+      value = value.substring(1, value.length() - 1);
+    }
+    return variants.contains(value.toUpperCase(Locale.ENGLISH));
+  }
+
+  private static boolean isQuotedValue(String value, String quote) {
+    return value.startsWith(quote) && value.endsWith(quote);
+  }
+}
---
+++ php-checks/src/test/java/org/sonar/php/checks/phpini/PhpIniBooleanTest.java
@@ -0,0 +1,62 @@
+/*
+ * SonarQube PHP Plugin
+ * Copyright (C) 2010-2016 SonarSource SA
+ * mailto:contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.php.checks.phpini;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.sonar.php.checks.phpini.PhpIniBoolean.OFF;
+import static org.sonar.php.checks.phpini.PhpIniBoolean.ON;
+
+public class PhpIniBooleanTest {
+
+  @Test
+  public void variants() throws Exception {
+    assertThat(ON.matchesValue("1")).isTrue();
+    assertThat(ON.matchesValue("on")).isTrue();
+    assertThat(ON.matchesValue("yes")).isTrue();
+    assertThat(ON.matchesValue("true")).isTrue();
+    assertThat(ON.matchesValue("x")).isFalse();
+
+    assertThat(OFF.matchesValue("0")).isTrue();
+    assertThat(OFF.matchesValue("off")).isTrue();
+    assertThat(OFF.matchesValue("no")).isTrue();
+    assertThat(OFF.matchesValue("false")).isTrue();
+    assertThat(OFF.matchesValue("x")).isFalse();
+  }
+
+  @Test
+  public void case_insensitive() throws Exception {
+    assertThat(ON.matchesValue("On")).isTrue();
+    assertThat(ON.matchesValue("ON")).isTrue();
+    assertThat(ON.matchesValue("yeS")).isTrue();
+  }
+
+  @Test
+  public void quoted_value() throws Exception {
+    assertThat(ON.matchesValue("'on'")).isTrue();
+    assertThat(ON.matchesValue("'1'")).isTrue();
+    assertThat(ON.matchesValue("\"1\"")).isTrue();
+    assertThat(ON.matchesValue("'1")).isFalse();
+    assertThat(ON.matchesValue("1'")).isFalse();
+    assertThat(ON.matchesValue("'x'")).isFalse();
+  }
+
+}
