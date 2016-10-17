896f7ccf2b4f34976ff941acaf27ea770b92ad96
Pierre-Yves Nicolas <pierre-yves.nicolas@sonarsource.com>
2016-10-17 14:27:48 +0200

    SONARPHP-641 Rule: "file_uploads" should be disabled

---
+++ php-checks/src/main/java/org/sonar/php/checks/phpini/FileUploadsCheck.java
@@ -0,0 +1,41 @@
+/*
+ * SonarQube PHP Plugin
+ * Copyright (C) 2010-2016 SonarSource SA
+ * mailto:contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.php.checks.phpini;
+
+import java.util.List;
+import org.sonar.check.Rule;
+import org.sonar.php.ini.PhpIniCheck;
+import org.sonar.php.ini.PhpIniIssue;
+import org.sonar.php.ini.tree.PhpIniFile;
+
+import static org.sonar.php.checks.phpini.PhpIniFiles.checkRequiredBoolean;
+
+@Rule(key = "S3338")
+public class FileUploadsCheck implements PhpIniCheck {
+
+  private static final String DIRECTIVE_MESSAGE = "Update this configuration to disable \"file_uploads\".";
+  private static final String FILE_MESSAGE = "Explicitly disable \"file_uploads\".";
+
+  @Override
+  public List<PhpIniIssue> analyze(PhpIniFile phpIniFile) {
+    return checkRequiredBoolean(phpIniFile, "file_uploads", PhpIniBoolean.OFF, DIRECTIVE_MESSAGE, FILE_MESSAGE);
+  }
+
+}
