2fb929e744ba78904589de964ca242e87e487e00
Julien HENRY <henryju@yahoo.fr>
2016-12-14 15:10:04 +0100

    SONARPHP-658 Add IT

--- its/plugin/tests/src/test/java/com/sonar/it/php/PHPIntegrationTest.java
+++ its/plugin/tests/src/test/java/com/sonar/it/php/PHPIntegrationTest.java
@@ -94,6 +94,14 @@ public class PHPIntegrationTest {
     assertThat(getFileMeasure("files").getIntValue()).isEqualTo(1);
     assertThat(getFileMeasure("classes").getIntValue()).isEqualTo(1);
     assertThat(getFileMeasure("functions").getIntValue()).isEqualTo(3);
+    assertThat(getFileMeasure("ncloc_data").getData()).isEqualTo(
+      "12=1;14=1;21=1;22=1;32=1;33=1;34=1;35=1;37=1;39=1;41=1;42=1;44=1;46=1;47=1;48=1;50=1;52=1;53=1;54=1;56=1;58=1;59=1;60=1;62=1;64=1;66=1;67=1;68=1;70=1;72=1;73=1;75=1;76=1;77=1;78=1;85=1;86=1;87=1;88=1;89=1");
+    if (is_after_sonar_6_2()) {
+      assertThat(getFileMeasure("executable_lines_data").getData()).isEqualTo(
+        "12=1;14=1;21=1;22=1;32=1;33=1;34=1;35=1;37=1;39=1;41=1;42=1;44=1;46=1;47=1;48=1;50=1;52=1;53=1;54=1;56=1;58=1;59=1;60=1;62=1;64=1;66=1;67=1;68=1;70=1;72=1;73=1;75=1;76=1;77=1;78=1;85=1;86=1;87=1;88=1;89=1");
+      assertThat(getFileMeasure("lines_to_cover").getIntValue()).isEqualTo(41);
+      assertThat(getFileMeasure("uncovered_lines").getIntValue()).isEqualTo(41);
+    }
 
     // Comments
     assertThat(getFileMeasure("comment_lines_density").getValue()).isEqualTo(26.8);
@@ -138,6 +146,10 @@ public class PHPIntegrationTest {
   private Measure getFileMeasure(String metricKey) {
     Resource resource = sonar.find(ResourceQuery.createForMetrics(FILE_TOKEN_PARSER, metricKey.trim()));
     return resource == null ? null : resource.getMeasure(metricKey.trim());
+  }
+
+  public static boolean is_after_sonar_6_2() {
+    return orchestrator.getConfiguration().getSonarVersion().isGreaterThanOrEquals("6.2");
   }
 
 }
