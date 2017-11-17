cc41e6b3ad27d0aee2895fed681ee2a8c6204e92
Nicolas PERU <nicolas.peru@sonarsource.com>
2017-11-17 15:33:16 +0100

    SONARPHP-746 fix after review

--- php-checks/src/test/resources/checks/AssignmentInSubExpressionCheck.php
+++ php-checks/src/test/resources/checks/AssignmentInSubExpressionCheck.php
@@ -1,11 +1,16 @@
 <?php
 $str = "";
-if(empty((($str = "foo")))) { // Noncompliant
+if(($str = "foo")) {
+}elseif(($str = "foo")) {}
+if($toto && $str = "foo") {} // Noncompliant
+
+isNull($str = "foo");
+if(empty(((toto && $str = "foo")))) { // Noncompliant
 }
-echo $plop = "plop"; // Noncompliant {{Extract the assignment of "$plop" from this expression.}}
-         //^
-echo $plop =& $str; // Noncompliant
-         //^
+echo "toto".$plop = "plop"; // Noncompliant {{Extract the assignment of "$plop" from this expression.}}
+                //^
+echo "toto".$plop =& $str; // Noncompliant
+                //^
 $plop = "plopure";
 for($i = 0; $i < $seriesCount; ++$i) {
 }
@@ -14,4 +19,17 @@ do{
 while ($c = $c->parent) {
 }
 for ($pos = 0; ; $pos += 128) {
+}
+foreach ($i = array(1, 2, 3, 4) as &$value) { // Noncompliant
+}
+switch ($i = $toto) { // Noncompliant
+    case "apple":
+        echo "i est une pomme";
+        break;
+    case "bar":
+        echo "i est une barre";
+        break;
+    case "cake":
+        echo "i est un gateau";
+        break;
 }
