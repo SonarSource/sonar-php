59fdcc15660f3c3aaa15c1455fd03b94dc3b1b63
Christophe Zürn <36889251+christophe-zurn-sonarsource@users.noreply.github.com>
2018-03-22 13:29:40 +0100

    SONARPHP-778  should not be checked by "Superglobals should not be accessed directly" (#286)
    * SONARPHP-778  should not be checked by "Superglobals should not be accessed directly"
    
    * SONARPHP-778 Update unit test

--- php-checks/src/test/resources/checks/DirectlyAccessingSuperGlobalCheck.php
+++ php-checks/src/test/resources/checks/DirectlyAccessingSuperGlobalCheck.php
@@ -4,3 +4,5 @@ $a = $_POST['name'];      // Noncompliant {{Do not access "$_POST" directly.}}
 //   ^^^^^^
 
 $b = $_MY_ARRAY['name'];  // OK
+
+$c = $_SESSION['name']; // OK $_SESSION is a special case and can be accessed directly
