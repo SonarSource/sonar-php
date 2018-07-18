31ff76a9720f9a755663e1131bcb49d5882e49b8
Elena <elena.vilchik@sonarsource.com>
2018-07-18 17:33:49 +0200

    SONARPHP-635 Rule S2092: Creating cookies without the "secure" flag is security-sensitive

---
+++ php-checks/src/test/resources/checks/CookiesSecureCheck.php
@@ -0,0 +1,12 @@
+<?php
+  setcookie($name, $value, $expire, $path, $domain, false); // Noncompliant {{Set the 6th argument of "setcookie()" function to "true".}}
+//^^^^^^^^^                                         ^^^^^ <
+
+  setcookie($name, $value, $expire, $path, $domain, false, true); // Noncompliant {{Set the 6th argument of "setcookie()" function to "true".}}
+//^^^^^^^^^                                         ^^^^^ <
+
+setcookie($name, $value, $expire, $path, $domain, $secure, false);
+setcookie($name, $value, $expire, $path, $domain, true);
+setcookie($name, $value, $expire, $path, $domain, foo(false));
+setcookie($name, $value, $expire, $path, $domain, $secure);
+setcookie($name, $value, $expire, $path);
