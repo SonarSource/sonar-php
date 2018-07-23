d30eefe7c6d0c9377c3e4d8767131b983007fb97
Andrei Epure <andrei.epure@sonarsource.com>
2018-07-23 17:00:53 +0200

    SONARPHP-813 Rules S2092, S3330 should handle setrawcookie

--- php-checks/src/test/resources/checks/CookiesSecureCheck.php
+++ php-checks/src/test/resources/checks/CookiesSecureCheck.php
@@ -5,6 +5,12 @@
   setcookie($name, $value, $expire, $path, $domain, false, true); // Noncompliant {{Make sure creating this cookie without the "secure" flag is safe here.}}
 //^^^^^^^^^
 
+  setrawcookie($name, $value, $expire, $path, $domain, false); // Noncompliant {{Make sure creating this cookie without the "secure" flag is safe here.}}
+//^^^^^^^^^^^^
+
+  setrawcookie($name, $value, $expire, $path, $domain, false, true); // Noncompliant {{Make sure creating this cookie without the "secure" flag is safe here.}}
+//^^^^^^^^^^^^
+
 setcookie($name, $value, $expire, $path, $domain, FALSE); // Noncompliant
 setcookie($name, $value, $expire, $path, $domain, False); // Noncompliant
 setcookie($name, $value, $expire, $path, $domain, 0); // Noncompliant
@@ -15,6 +21,17 @@ setcookie($name, $value, $expire, $path, $domain, true);
 setcookie($name, $value, $expire, $path, $domain, foo(false));
 setcookie($name, $value, $expire, $path, $domain, $secure);
 setcookie($name, $value, $expire, $path); // Noncompliant
+
+setrawcookie($name, $value, $expire, $path, $domain, FALSE); // Noncompliant
+setrawcookie($name, $value, $expire, $path, $domain, False); // Noncompliant
+setrawcookie($name, $value, $expire, $path, $domain, 0); // Noncompliant
+setrawcookie($name, $value, $expire, $path, $domain, ""); // Noncompliant
+
+setrawcookie($name, $value, $expire, $path, $domain, $secure, false);
+setrawcookie($name, $value, $expire, $path, $domain, true);
+setrawcookie($name, $value, $expire, $path, $domain, foo(false));
+setrawcookie($name, $value, $expire, $path, $domain, $secure);
+setrawcookie($name, $value, $expire, $path); // Noncompliant
 
 session_set_cookie_params($lifetime, $path, $domain, false); // Noncompliant
 session_set_cookie_params($lifetime, $path, $domain); // Noncompliant
