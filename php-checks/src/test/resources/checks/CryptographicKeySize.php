c5290b6274e6a15c12dfabbbeb1c49d18e70212a
Andrea Guarino <andrea.guarino@sonarsource.com>
2018-07-23 16:03:08 +0200

    SONARPHP-796 Rule S4426: Cryptographic keys should not be too short

---
+++ php-checks/src/test/resources/checks/CryptographicKeySize.php
@@ -0,0 +1,50 @@
+<?php
+function nok() {
+    $config = array(
+            "private_key_bits" => 1024, // Noncompliant {{Use a key length of at least 2048 bits}}
+//                                ^^^^
+            "private_key_type" => OPENSSL_KEYTYPE_RSA,
+        );
+    $res = openssl_pkey_new($config);
+}
+
+function nok_var() {
+    $key_length = 1024;
+    $config = array(
+            "private_key_bits" => $key_length, // Noncompliant {{Use a key length of at least 2048 bits}}
+//                                ^^^^^^^^^^^
+            "private_key_type" => OPENSSL_KEYTYPE_RSA,
+        );
+    $res = openssl_pkey_new($config);
+}
+
+
+function ok() {
+    $config = array(
+            "private_key_bits" => 2048,
+            "private_key_type" => OPENSSL_KEYTYPE_RSA,
+        );
+    $res = openssl_pkey_new($config);
+}
+
+function multipleValues() {
+    $maybeConfig = array(
+            "private_key_bits" => 1024,
+            "private_key_type" => OPENSSL_KEYTYPE_RSA,
+        );
+    if (cond()) {
+      $maybeConfig =  array(
+            "private_key_bits" => 2048,
+            "private_key_type" => OPENSSL_KEYTYPE_RSA,
+      );
+    }
+    $res = openssl_pkey_new($config);
+}
+
+function noRsa() {
+  $config = array(
+          "private_key_bits" => 1024,
+          "private_key_type" => OPENSSL_KEYTYPE_EC,
+      );
+  $res = openssl_pkey_new($config);
+}
