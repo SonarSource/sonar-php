da4d28645c986c0173f7c6c05624733566cfd6e9
Tibor Blenessy <tibor.blenessy@sonarsource.com>
2018-07-24 14:38:30 +0200

    review feedback

--- php-checks/src/main/java/org/sonar/php/checks/CakePhpDebugModeCheck.java
+++ php-checks/src/main/java/org/sonar/php/checks/CakePhpDebugModeCheck.java
@@ -20,16 +20,19 @@
 package org.sonar.php.checks;
 
 import com.google.common.collect.ImmutableSet;
+import java.util.Optional;
 import java.util.Set;
 import org.sonar.check.Rule;
 import org.sonar.php.checks.utils.CheckUtils;
+import org.sonar.php.tree.visitors.AssignmentExpressionVisitor;
+import org.sonar.plugins.php.api.symbols.Symbol;
+import org.sonar.plugins.php.api.tree.CompilationUnitTree;
 import org.sonar.plugins.php.api.tree.Tree;
 import org.sonar.plugins.php.api.tree.expression.ExpressionTree;
 import org.sonar.plugins.php.api.tree.expression.FunctionCallTree;
 import org.sonar.plugins.php.api.tree.expression.LiteralTree;
 import org.sonar.plugins.php.api.visitors.PHPVisitorCheck;
 
-import static org.sonar.php.checks.utils.CheckUtils.isTrueValue;
 import static org.sonar.php.checks.utils.CheckUtils.trimQuotes;
 
 @Rule(key = "S4507")
@@ -38,6 +41,7 @@ public class CakePhpDebugModeCheck extends PHPVisitorCheck {
   private static final String MESSAGE = "Make sure CakePHP's debug mode is not activated on production code.";
 
   private static final Set<String> CAKE_DEBUG_FUNCTIONS = ImmutableSet.of("Configure::write", "Configure::config");
+  private AssignmentExpressionVisitor assignmentExpressionVisitor;
 
   @Override
   public void visitFunctionCall(FunctionCallTree tree) {
@@ -47,10 +51,33 @@ public class CakePhpDebugModeCheck extends PHPVisitorCheck {
       ExpressionTree secondArg = tree.arguments().get(1);
       if (firstArg.is(Tree.Kind.REGULAR_STRING_LITERAL)
         && trimQuotes((LiteralTree) firstArg).equals("debug")
-        && isTrueValue(secondArg)) {
+        && isTrue(secondArg)) {
         context().newIssue(this, tree, MESSAGE);
       }
     }
     super.visitFunctionCall(tree);
+  }
+
+  private boolean isTrue(ExpressionTree tree) {
+    if (tree.is(Tree.Kind.BOOLEAN_LITERAL, Tree.Kind.NUMERIC_LITERAL, Tree.Kind.REGULAR_STRING_LITERAL) &&
+      CheckUtils.isTrueValue(tree)) {
+      return true;
+    }
+    if (tree.is(Tree.Kind.VARIABLE_IDENTIFIER)) {
+      Symbol symbol = context().symbolTable().getSymbol(tree);
+      Optional<ExpressionTree> uniqueAssignedValue = assignmentExpressionVisitor.getUniqueAssignedValue(symbol);
+      if (uniqueAssignedValue.isPresent()) {
+        ExpressionTree expressionTree = uniqueAssignedValue.get();
+        return CheckUtils.isTrueValue(expressionTree);
+      }
+    }
+    return false;
+  }
+
+  @Override
+  public void visitCompilationUnit(CompilationUnitTree tree) {
+    this.assignmentExpressionVisitor = new AssignmentExpressionVisitor(context().symbolTable());
+    tree.accept(assignmentExpressionVisitor);
+    super.visitCompilationUnit(tree);
   }
 }
--- php-checks/src/test/resources/checks/CakePhpDebugModeCheck.php
+++ php-checks/src/test/resources/checks/CakePhpDebugModeCheck.php
@@ -10,7 +10,18 @@ Configure::write('debug', 3); // Noncompliant
 
 Configure::config('debug', true); // Noncompliant
 
+$debug = true;
+Configure::config('debug', $debug); // Noncompliant
+
 
 Configure::write('debug', 0);
 Configure::config('debug', false);
 Configure::config('App.baseUrl', "http://example.com/");
+$debug2 = false;
+Configure::config('debug', $debug2);
+
+Configure::write('xxx', 1);
+Configure::config('xxx', true);
+Configure::write($var, 1);
+Configure::write('debug', $var);
+Configure::xxx('debug', 1);
