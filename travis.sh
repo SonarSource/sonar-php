8b3ffa36323e25fe27b8cb3e96862871e2c1ad2e
pynicolas <pierre-yves.nicolas@sonarsource.com>
2018-07-24 13:40:48 +0200

    Upgrade travis-utils to v49

--- travis.sh
+++ travis.sh
@@ -8,7 +8,7 @@ function cleanPhpInMavenRepository {
 
 function configureTravis {
   mkdir -p ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v43 | tar zx --strip-components 1 -C ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v49 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
 }
 
