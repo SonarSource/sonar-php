7cd4c21d6a1c63a4aa464c004d50601e7860a329
pynicolas <pierre-yves.nicolas@sonarsource.com>
2018-07-27 15:24:23 +0200

    SONARPHP-806 Raise issues on other functions and on backticks (#324)

--- php-checks/src/test/resources/checks/ExecCallCheck.php
+++ php-checks/src/test/resources/checks/ExecCallCheck.php
@@ -4,3 +4,15 @@
   exec($command); // Noncompliant {{Make sure that executing this OS command is safe here.}}
 //^^^^
   foo->exec($command);
+
+  passthru($str); // Noncompliant
+  proc_open($str); // Noncompliant
+  popen($str, $mode); // Noncompliant
+  shell_exec($str); // Noncompliant
+  system($str); // Noncompliant
+  pcntl_exec($str); // Noncompliant
+  closedir($str);
+
+  `cd {$var}`; // Noncompliant
+  'cd {$var}';
+  "cd {$var}";
