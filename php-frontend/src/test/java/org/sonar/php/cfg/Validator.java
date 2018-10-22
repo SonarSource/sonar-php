b88e2ad966c959a5ad8d514e89eebd0d74cca936
Tibor Blenessy <tibor.blenessy@sonarsource.com>
2018-10-22 17:53:01 +0200

    Improve CFG test framework to handle empty blocks

--- php-frontend/src/test/java/org/sonar/php/cfg/Validator.java
+++ php-frontend/src/test/java/org/sonar/php/cfg/Validator.java
@@ -19,15 +19,14 @@
  */
 package org.sonar.php.cfg;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 import java.util.StringJoiner;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import org.apache.commons.lang.StringUtils;
 import org.sonar.plugins.php.api.symbols.Symbol;
 
+import static java.lang.String.format;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class Validator {
@@ -44,13 +43,19 @@ class Validator {
     DEBUG_MESSAGE_TEMPLATE = stringJoiner.toString();
   }
 
-  private Validator() {
-    // this is an utility class and should not be instantiated
+  private final ExpectedCfgStructure expectedCfg;
+  private String debugDotNotation;
+
+  Validator(ExpectedCfgStructure expectedCfg) {
+    this.expectedCfg = expectedCfg;
   }
 
   static void assertCfgStructure(ControlFlowGraph actualCfg) {
-    ExpectedCfgStructure expectedCfg = ExpectedCfgStructure.parse(actualCfg.blocks());
-    String debugDotNotation = CfgPrinter.toDot(actualCfg);
+    new Validator(ExpectedCfgStructure.parse(actualCfg.blocks())).assertCfg(actualCfg);
+  }
+
+  void assertCfg(ControlFlowGraph actualCfg) {
+    debugDotNotation = CfgPrinter.toDot(actualCfg);
 
     assertThat(actualCfg.end().successors())
       .withFailMessage("END block should not have successors")
@@ -59,42 +64,71 @@ class Validator {
       .withFailMessage("END block should not have elements")
       .isEmpty();
     assertThat(actualCfg.blocks())
-      .withFailMessage(buildDebugMessage("size", "CFG", debugDotNotation))
+      .withFailMessage(buildDebugMessage(format("size expected: %d actual: %d", expectedCfg.size(), actualCfg.blocks().size()), "CFG"))
       .hasSize(expectedCfg.size());
 
     for (CfgBlock actualBlock : actualCfg.blocks()) {
       if (actualBlock.equals(actualCfg.end())) {
         continue;
       }
+      if (actualBlock.elements().isEmpty()) {
+        assertEmptyBlock(actualBlock);
+        continue;
+      }
 
       String blockTestId = expectedCfg.testId(actualBlock);
-      assertSuccessors(actualBlock, expectedCfg, debugDotNotation);
-
-      if (expectedCfg.hasNonEmptyPredecessors()) {
-        Set<CfgBlock> expectedPred = getCfgBlocksSet(expectedCfg.expectedPred(actualBlock), expectedCfg);
-        assertThat(actualBlock.predecessors())
-          .withFailMessage(buildDebugMessage("predecessors", blockTestId, debugDotNotation))
-          .containsOnlyElementsOf(expectedPred);
-      }
-
-      if (expectedCfg.hasNonEmptyElementNumbers()) {
-        int actualElementNumber = actualBlock.elements().size();
-        int expectedElementNumber = expectedCfg.expectedNumberOfElements(actualBlock);
-        String message = String.format("Expecting %d elements instead of %d for '%s'",
-          expectedElementNumber, actualElementNumber, blockTestId);
-        assertThat(actualBlock.elements().size())
-          .withFailMessage(message)
-          .isEqualTo(expectedCfg.expectedNumberOfElements(actualBlock));
-      }
+      assertSuccessors(actualBlock);
+      assertPredecessors(actualBlock, blockTestId);
+      assertElements(actualBlock, blockTestId);
+      assertSyntacticSuccessor(actualBlock);
     }
+  }
+
+  private void assertPredecessors(CfgBlock actualBlock, String blockTestId) {
+    if (expectedCfg.hasNonEmptyPredecessors()) {
+      List<String> expectedPred = expectedCfg.expectedPred(actualBlock);
+      assertThat(expectedCfg.blockIds(actualBlock.predecessors()))
+        .withFailMessage(buildDebugMessage("predecessors", blockTestId))
+        .containsOnlyElementsOf(expectedPred);
+    }
+  }
+
+  private void assertElements(CfgBlock actualBlock, String blockTestId) {
+    if (expectedCfg.hasNonEmptyElementNumbers()) {
+      int actualElementNumber = actualBlock.elements().size();
+      int expectedElementNumber = expectedCfg.expectedNumberOfElements(actualBlock);
+      String message = format("Expecting %d elements instead of %d for '%s'",
+        expectedElementNumber, actualElementNumber, blockTestId);
+      assertThat(actualBlock.elements().size())
+        .withFailMessage(message)
+        .isEqualTo(expectedCfg.expectedNumberOfElements(actualBlock));
+    }
+  }
+
+  private void assertEmptyBlock(CfgBlock emptyBlock) {
+    List<ExpectedCfgStructure.BlockExpectation> matchedExpectations = expectedCfg.emptyBlockExpectations.stream()
+      .filter(blockExpectation -> blockExpectation.matchesBlock(emptyBlock))
+      .collect(Collectors.toList());
+    assertThat(matchedExpectations)
+      .withFailMessage(format("Failed to assert empty block succ=%s, pred=%s. Matched expectations: %s",
+        expectedCfg.blockIds(emptyBlock.successors()),
+        expectedCfg.blockIds(emptyBlock.predecessors()),
+        matchedExpectations))
+      .hasSize(1);
+    // remove the expectation we've just asserted so it is not used for another empty block
+    expectedCfg.emptyBlockExpectations.removeAll(matchedExpectations);
   }
 
   static void assertLiveVariables(ControlFlowGraph actualCfg, LiveVariablesAnalysis actualLva) {
     ExpectedCfgStructure expectedCfg = ExpectedCfgStructure.parse(actualCfg.blocks());
-    String debugDotNotation = CfgPrinter.toDot(actualCfg);
+    new Validator(expectedCfg).assertLva(actualCfg, actualLva);
+  }
+
+  private void assertLva(ControlFlowGraph actualCfg, LiveVariablesAnalysis actualLva) {
+    debugDotNotation = CfgPrinter.toDot(actualCfg);
 
     assertThat(actualCfg.blocks())
-      .withFailMessage(buildDebugMessage("size", "CFG", debugDotNotation))
+      .withFailMessage(buildDebugMessage("size", "CFG"))
       .hasSize(expectedCfg.size());
 
     for (CfgBlock actualBlock : actualCfg.blocks()) {
@@ -104,76 +138,74 @@ class Validator {
 
       String blockTestId = expectedCfg.testId(actualBlock);
       LiveVariablesAnalysis.LiveVariables actualLiveVariables = actualLva.getLiveVariables(actualBlock);
-      assertVariablesAreEqual("Gen Variables", actualLiveVariables.getGen(), expectedCfg.expectedGenVariables(actualBlock), blockTestId, debugDotNotation);
-      assertVariablesAreEqual("Killed Variables", actualLiveVariables.getKill(), expectedCfg.expectedKilledVariables(actualBlock), blockTestId, debugDotNotation);
-      assertVariablesAreEqual("Live In Variables", actualLiveVariables.getIn(), expectedCfg.expectedLiveInVariables(actualBlock), blockTestId, debugDotNotation);
-      assertVariablesAreEqual("Live Out Variables", actualLiveVariables.getOut(), expectedCfg.expectedLiveOutVariables(actualBlock), blockTestId, debugDotNotation);
+      assertVariablesAreEqual("Gen Variables", actualLiveVariables.getGen(), expectedCfg.expectedGenVariables(actualBlock), blockTestId);
+      assertVariablesAreEqual("Killed Variables", actualLiveVariables.getKill(), expectedCfg.expectedKilledVariables(actualBlock), blockTestId);
+      assertVariablesAreEqual("Live In Variables", actualLiveVariables.getIn(), expectedCfg.expectedLiveInVariables(actualBlock), blockTestId);
+      assertVariablesAreEqual("Live Out Variables", actualLiveVariables.getOut(), expectedCfg.expectedLiveOutVariables(actualBlock), blockTestId);
     }
   }
 
-  private static void assertVariablesAreEqual(String variableType, Set<Symbol> actualVariables, Set<String> expectedVariables, String blockTestId, String debugDotNotation) {
+  private void assertVariablesAreEqual(String variableType, Set<Symbol> actualVariables, Set<String> expectedVariables, String blockTestId) {
     int actualSize = actualVariables.size();
     int expectedSize = expectedVariables.size();
     assertThat(actualSize)
-      .withFailMessage(buildDebugMessage(variableType + " size expected " + expectedSize + " and is " + actualSize, blockTestId, debugDotNotation))
+      .withFailMessage(buildDebugMessage(variableType + " size expected " + expectedSize + " and is " + actualSize, blockTestId))
       .isEqualTo(expectedSize);
     Set<String> actualVariableNames = actualVariables.stream().map(Symbol::name).collect(Collectors.toSet());
     assertThat(actualVariableNames)
-      .withFailMessage(buildDebugMessage(variableType + " elements differ. Actual: " + StringUtils.join(actualVariableNames, " ; "), blockTestId, debugDotNotation))
+      .withFailMessage(buildDebugMessage(variableType + " elements differ. Actual: " + StringUtils.join(actualVariableNames, " ; "), blockTestId))
       .containsOnlyElementsOf(expectedVariables);
   }
 
-  private static void assertSuccessors(CfgBlock actualBlock, ExpectedCfgStructure expectedCfg, String debugDotNotation) {
+  private void assertSuccessors(CfgBlock actualBlock) {
     String blockTestId = expectedCfg.testId(actualBlock);
 
     if (actualBlock instanceof PhpCfgBranchingBlock) {
 
       PhpCfgBranchingBlock actualIfBlock = (PhpCfgBranchingBlock) actualBlock;
-      List<CfgBlock> expectedSucc = getCfgBlocksList(expectedCfg.expectedSucc(actualBlock), expectedCfg);
+      List<String> expectedSucc = expectedCfg.expectedSucc(actualBlock);
       assertThat(expectedSucc)
-        .withFailMessage(buildDebugMessage("branching block must have 2 elements", blockTestId, debugDotNotation))
+        .withFailMessage(buildDebugMessage("branching block must have 2 elements", blockTestId))
         .hasSize(2);
-      assertThat(actualIfBlock.trueSuccessor())
-        .withFailMessage(buildDebugMessage("'true' branch successor", blockTestId, debugDotNotation))
+      assertThat(expectedCfg.testId(actualIfBlock.trueSuccessor()))
+        .withFailMessage(buildDebugMessage("'true' branch successor", blockTestId))
         .isEqualTo(expectedSucc.get(0));
-      assertThat(actualIfBlock.falseSuccessor())
-        .withFailMessage(buildDebugMessage("'false' branch successor", blockTestId, debugDotNotation))
+      assertThat(expectedCfg.testId(actualIfBlock.falseSuccessor()))
+        .withFailMessage(buildDebugMessage("'false' branch successor", blockTestId))
         .isEqualTo(expectedSucc.get(1));
 
     } else {
 
-      Set<CfgBlock> expectedSucc = getCfgBlocksSet(expectedCfg.expectedSucc(actualBlock), expectedCfg);
-      assertThat(actualBlock.successors())
-        .withFailMessage(buildDebugMessage("successors", blockTestId, debugDotNotation))
+      List<String> expectedSucc = expectedCfg.expectedSucc(actualBlock);
+      List<String> actual = expectedCfg.blockIds(actualBlock.successors());
+      assertThat(actual)
+        .withFailMessage(buildDebugMessage(format("successors actual: %s expected %s", actual, expectedSucc), blockTestId))
         .containsOnlyElementsOf(expectedSucc);
     }
 
+  }
+
+  private void assertSyntacticSuccessor(CfgBlock actualBlock) {
+    String blockTestId = expectedCfg.testId(actualBlock);
     String expectedSyntSucc = expectedCfg.expectedSyntSucc(actualBlock);
 
     if (expectedSyntSucc != null) {
-      assertThat(actualBlock.syntacticSuccessor())
-        .withFailMessage(buildDebugMessage("syntactic successor", blockTestId, debugDotNotation))
-        .isEqualTo(expectedCfg.cfgBlock(expectedSyntSucc));
-
+      if (expectedSyntSucc.equals(ExpectedCfgStructure.EMPTY)) {
+        assertThat(actualBlock.syntacticSuccessor().elements())
+          .withFailMessage("syntactic successor should be _empty", blockTestId)
+          .isEmpty();
+      } else {
+        assertThat(actualBlock.syntacticSuccessor())
+          .withFailMessage(buildDebugMessage("syntactic successor", blockTestId))
+          .isEqualTo(expectedCfg.cfgBlock(expectedSyntSucc));
+      }
     } else {
-      assertThat(actualBlock.syntacticSuccessor()).withFailMessage(buildDebugMessage("syntactic successor", blockTestId, debugDotNotation)).isNull();
+      assertThat(actualBlock.syntacticSuccessor()).withFailMessage(buildDebugMessage("syntactic successor", blockTestId)).isNull();
     }
   }
 
-  private static Set<CfgBlock> getCfgBlocksSet(Collection<String> testIds, ExpectedCfgStructure expectedCfg) {
-    return getCfgBlocksStream(testIds, expectedCfg).collect(Collectors.toSet());
-  }
-
-  private static List<CfgBlock> getCfgBlocksList(Collection<String> testIds, ExpectedCfgStructure expectedCfg) {
-    return getCfgBlocksStream(testIds, expectedCfg).collect(Collectors.toList());
-  }
-
-  private static Stream<CfgBlock> getCfgBlocksStream(Collection<String> testIds, ExpectedCfgStructure expectedCfg) {
-    return testIds.stream().map(expectedCfg::cfgBlock);
-  }
-
-  private static String buildDebugMessage(String hint, String blockId, String debugDotNotation) {
-    return String.format(DEBUG_MESSAGE_TEMPLATE, hint, blockId, debugDotNotation);
+  private String buildDebugMessage(String hint, String blockId) {
+    return format(DEBUG_MESSAGE_TEMPLATE, hint, blockId, this.debugDotNotation);
   }
 
 }
