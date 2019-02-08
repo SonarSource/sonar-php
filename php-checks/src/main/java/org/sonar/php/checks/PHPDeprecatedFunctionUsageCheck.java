21c397848067d533310a62d9cd72bcb197f2d8fe
Alban Auzeill <alban.auzeill@sonarsource.com>
2019-02-08 11:26:59 +0100

    SONARPHP-895 SONARPHP-887 Rule S2001: Add deprecated functions and constants (#434)

--- php-checks/src/main/java/org/sonar/php/checks/PHPDeprecatedFunctionUsageCheck.java
+++ php-checks/src/main/java/org/sonar/php/checks/PHPDeprecatedFunctionUsageCheck.java
@@ -21,14 +21,23 @@ package org.sonar.php.checks;
 
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
+import java.util.Locale;
+import java.util.function.Predicate;
 import org.sonar.check.Rule;
+import org.sonar.php.checks.utils.CheckUtils;
 import org.sonar.php.checks.utils.FunctionUsageCheck;
+import org.sonar.php.checks.utils.type.NewObjectCall;
+import org.sonar.php.checks.utils.type.ObjectMemberFunctionCall;
+import org.sonar.php.checks.utils.type.TreeValues;
 import org.sonar.plugins.php.api.tree.SeparatedList;
 import org.sonar.plugins.php.api.tree.Tree.Kind;
+import org.sonar.plugins.php.api.tree.declaration.FunctionDeclarationTree;
 import org.sonar.plugins.php.api.tree.declaration.NamespaceNameTree;
 import org.sonar.plugins.php.api.tree.expression.ExpressionTree;
 import org.sonar.plugins.php.api.tree.expression.FunctionCallTree;
 import org.sonar.plugins.php.api.tree.expression.LiteralTree;
+import org.sonar.plugins.php.api.tree.expression.MemberAccessTree;
+import org.sonar.plugins.php.api.tree.expression.NameIdentifierTree;
 
 @Rule(key = PHPDeprecatedFunctionUsageCheck.KEY)
 public class PHPDeprecatedFunctionUsageCheck extends FunctionUsageCheck {
@@ -36,8 +45,10 @@ public class PHPDeprecatedFunctionUsageCheck extends FunctionUsageCheck {
   public static final String KEY = "S2001";
   private static final String MESSAGE_SET_LOCAL_ARG = "Use the \"%s\" constant instead of a string literal.";
   private static final String MESSAGE_WITH_REPLACEMENT = "Replace this \"%s()\" call with a call to \"%s\".";
-  private static final String MESSAGE_WITHOUT_REPLACEMENT = "Remove this \"%s()\" call.";
+  private static final String MESSAGE_WITHOUT_REPLACEMENT = "Remove this call to deprecated \"%s()\".";
   private static final String SESSION = "$_SESSION";
+  private static final String FGETSS_FUNCTION = "fgetss";
+  private static final String GZGETSS_FUNCTION = "gzgetss";
 
   private static final ImmutableMap<String, String> NEW_BY_DEPRECATED_FUNCTIONS = ImmutableMap.<String, String>builder()
     .put("call_user_method", "call_user_func()")
@@ -53,16 +64,33 @@ public class PHPDeprecatedFunctionUsageCheck extends FunctionUsageCheck {
     .put("session_register", SESSION)
     .put("session_unregister", SESSION)
     .put("session_is_registered", SESSION)
-    .put("set_socket_blocking", "stream_set_blocking")
-    .put("split", "preg_split")
-    .put("spliti", "preg_split")
+    .put("set_socket_blocking", "stream_set_blocking()")
+    .put("split", "preg_split()")
+    .put("spliti", "preg_split()")
     .put("sql_regcase", "")
     .put("mysql_db_query", "mysql_select_db() and mysql_query()")
-    .put("mysql_escape_string", "mysql_real_escape_string")
-    .put("__autoload", "spl_autoload_register")
+    .put("mysql_escape_string", "mysql_real_escape_string()")
+    .put("__autoload", "spl_autoload_register()")
     .put("create_function", "")
-    .put("gmp_random", "gmp_random_bits")
+    .put("gmp_random", "gmp_random_bits()")
     .put("each", "")
+    .put("mbregex_encoding", "mb_regex_encoding()")
+    .put("mbereg", "mb_ereg()")
+    .put("mberegi", "mb_eregi()")
+    .put("mbereg_replace", "mb_ereg_replace()")
+    .put("mberegi_replace", "mb_eregi_replace()")
+    .put("mbsplit", "mb_split()")
+    .put("mbereg_match", "mb_ereg_match()")
+    .put("mbereg_search", "mb_ereg_search()")
+    .put("mbereg_search_pos", "mb_ereg_search_pos()")
+    .put("mbereg_search_regs", "mb_ereg_search_regs()")
+    .put("mbereg_search_init", "mb_ereg_search_init()")
+    .put("mbereg_search_getregs", "mb_ereg_search_getregs()")
+    .put("mbereg_search_getpos", "mb_ereg_search_getpos()")
+    .put("mbereg_search_setpos", "mb_ereg_search_setpos()")
+    .put(FGETSS_FUNCTION, "")
+    .put(GZGETSS_FUNCTION, "")
+    .put("image2wbmp", "imagewbmp()")
     .build();
 
   private static final String SET_LOCALE_FUNCTION = "setlocale";
@@ -72,15 +100,69 @@ public class PHPDeprecatedFunctionUsageCheck extends FunctionUsageCheck {
   private static final ImmutableSet<String> LOCALE_CATEGORY_CONSTANTS = ImmutableSet.of(
     "LC_ALL", "LC_COLLATE", "LC_CTYPE", "LC_MONETARY", "LC_NUMERIC", "LC_TIME", "LC_MESSAGES");
 
+  private static final ImmutableSet<String> DEPRECATED_CASE_SENSITIVE_CONSTANTS = ImmutableSet.of(
+    "FILTER_FLAG_SCHEME_REQUIRED", "FILTER_FLAG_HOST_REQUIRED");
+
+  private static final ImmutableSet<String> SEARCHING_STRING_FUNCTIONS = ImmutableSet.of(
+    "stristr", "strrchr", "strstr", "strripos", "stripos", "strrpos", "strpos", "strchr");
+
+  private static final Predicate<TreeValues> SPLFILEOBJECT_FGETSS = new ObjectMemberFunctionCall(FGETSS_FUNCTION, new NewObjectCall("SplFileObject"));
+
   @Override
   protected ImmutableSet<String> functionNames() {
     return ImmutableSet.<String>builder()
       .addAll(NEW_BY_DEPRECATED_FUNCTIONS.keySet())
+      .addAll(SEARCHING_STRING_FUNCTIONS)
       .add(SET_LOCALE_FUNCTION)
       .add(PARSE_STR_FUNCTION)
       .add(ASSERT_FUNCTION)
       .add(DEFINE_FUNCTION)
       .build();
+  }
+  @Override
+  public void visitFunctionCall(FunctionCallTree tree) {
+    TreeValues possibleValues = TreeValues.of(tree, context().symbolTable());
+    if (SPLFILEOBJECT_FGETSS.test(possibleValues)) {
+      context().newIssue(this, tree, String.format(MESSAGE_WITHOUT_REPLACEMENT, FGETSS_FUNCTION));
+    }
+    super.visitFunctionCall(tree);
+  }
+
+  @Override
+  public void visitNameIdentifier(NameIdentifierTree tree) {
+    if (DEPRECATED_CASE_SENSITIVE_CONSTANTS.contains(tree.text())) {
+      context().newIssue(this, tree, "Do not use this deprecated \"" + tree.text() + "\" constant.");
+    }
+    super.visitNameIdentifier(tree);
+  }
+
+  @Override
+  public void visitLiteral(LiteralTree tree) {
+    if (tree.is(Kind.REGULAR_STRING_LITERAL) && CheckUtils.trimQuotes(tree).equals("string.strip_tags")) {
+      context().newIssue(this, tree, "Remove this deprecated \"string.strip_tags\" filter usage.");
+    }
+    super.visitLiteral(tree);
+  }
+
+  @Override
+  public void visitMemberAccess(MemberAccessTree tree) {
+    if (tree.isStatic() && tree.object().is(Kind.NAMESPACE_NAME) && tree.member().is(Kind.NAME_IDENTIFIER)) {
+      String constantType = ((NamespaceNameTree) tree.object()).unqualifiedName();
+      String constantName = ((NameIdentifierTree) tree.member()).text();
+      if (constantType.equalsIgnoreCase("Normalizer") && constantName.equals("NONE")) {
+        context().newIssue(this, tree, "Do not use this deprecated \"Normalizer::NONE\" constant.");
+      }
+    }
+    super.visitMemberAccess(tree);
+  }
+
+  @Override
+  public void visitFunctionDeclaration(FunctionDeclarationTree tree) {
+    NameIdentifierTree name = tree.name();
+    if (name.text().equalsIgnoreCase(ASSERT_FUNCTION)) {
+      context().newIssue(this, name, "Use the standard \"assert\" function instead of declaring a new assert function.");
+    }
+    super.visitFunctionDeclaration(tree);
   }
 
   @Override
@@ -98,6 +180,9 @@ public class PHPDeprecatedFunctionUsageCheck extends FunctionUsageCheck {
 
     } else if (DEFINE_FUNCTION.equalsIgnoreCase(functionName)) {
       checkDefineArguments(tree);
+
+    } else if (SEARCHING_STRING_FUNCTIONS.contains(functionName.toLowerCase(Locale.ROOT))) {
+      checkSearchingStringArguments(tree);
 
     } else {
       context().newIssue(this, tree.callee(), buildMessage(functionName));
@@ -153,4 +238,13 @@ public class PHPDeprecatedFunctionUsageCheck extends FunctionUsageCheck {
     }
   }
 
+  private void checkSearchingStringArguments(FunctionCallTree tree) {
+    SeparatedList<ExpressionTree> arguments = tree.arguments();
+    if (arguments.size() >= 2) {
+      ExpressionTree needleArgument = arguments.get(1);
+      if (needleArgument.is(Kind.NUMERIC_LITERAL, Kind.UNARY_MINUS)) {
+        context().newIssue(this, needleArgument, "Convert this integer needle into a string.");
+      }
+    }
+  }
 }
--- php-checks/src/test/resources/checks/PHPDeprecatedFunctionUsageCheck.php
+++ php-checks/src/test/resources/checks/PHPDeprecatedFunctionUsageCheck.php
@@ -5,7 +5,7 @@ namespace {
 
   call_user_method();          // Noncompliant {{Replace this "call_user_method()" call with a call to "call_user_func()".}}
 //^^^^^^^^^^^^^^^^
-  define_syslog_variables();   // Noncompliant {{Remove this "define_syslog_variables()" call.}}
+  define_syslog_variables();   // Noncompliant {{Remove this call to deprecated "define_syslog_variables()".}}
 
   if (sql_regcase());          // Noncompliant
 
@@ -38,6 +38,15 @@ namespace A {
   function call_user_method() {
   }
 
+  function assert() { // Noncompliant {{Use the standard "assert" function instead of declaring a new assert function.}}
+  //       ^^^^^^
+  }
+
+  class Foo {
+    function assert() {
+    }
+  }
+
   call_user_method();             /* Noncompliant */ // FIXME (SONARPHP-552) False-Positive
   \call_user_method();            // Noncompliant
 }
@@ -47,3 +56,93 @@ namespace A {
 define('BAR', 21);        // OK
 define('BAR', 21, false); // OK
 define('BAR', 21, null);
+
+$a = FILTER_FLAG_SCHEME_REQUIRED; // Noncompliant {{Do not use this deprecated "FILTER_FLAG_SCHEME_REQUIRED" constant.}}
+//   ^^^^^^^^^^^^^^^^^^^^^^^^^^^
+$a = FILTER_FLAG_HOST_REQUIRED; // Noncompliant
+//   ^^^^^^^^^^^^^^^^^^^^^^^^^
+$a = ANY_CONSTANT;
+
+mbregex_encoding($test_enc);                // Noncompliant {{Replace this "mbregex_encoding()" call with a call to "mb_regex_encoding()".}}
+mbereg_search_init($str, $look_for, $opt);  // Noncompliant {{Replace this "mbereg_search_init()" call with a call to "mb_ereg_search_init()".}}
+while (mbereg_search_pos()) {               // Noncompliant {{Replace this "mbereg_search_pos()" call with a call to "mb_ereg_search_pos()".}}
+    $regs = mbereg_search_getregs();        // Noncompliant {{Replace this "mbereg_search_getregs()" call with a call to "mb_ereg_search_getregs()".}}
+    printf("%d\n", mbereg_search_getpos()); // Noncompliant {{Replace this "mbereg_search_getpos()" call with a call to "mb_ereg_search_getpos()".}}
+}
+mbereg('[0-9]', $text);                      // Noncompliant {{Replace this "mbereg()" call with a call to "mb_ereg()".}}
+mberegi('äpfel', $text);                     // Noncompliant {{Replace this "mberegi()" call with a call to "mb_eregi()".}}
+$data = mbereg_replace("/[0-9]/","",$data);  // Noncompliant {{Replace this "mbereg_replace()" call with a call to "mb_ereg_replace()".}}
+$data = mberegi_replace("/[0-9]/","",$data); // Noncompliant {{Replace this "mberegi_replace()" call with a call to "mb_eregi_replace()".}}
+$data = mbsplit("/\s/", "hello world");      // Noncompliant {{Replace this "mbsplit()" call with a call to "mb_split()".}}
+$test = mbereg_match("a", "some apples");    // Noncompliant {{Replace this "mbereg_match()" call with a call to "mb_ereg_match()".}}
+$r = mbereg_search();                        // Noncompliant {{Replace this "mbereg_search()" call with a call to "mb_ereg_search()".}}
+$r = mbereg_search_regs();                   // Noncompliant {{Replace this "mbereg_search_regs()" call with a call to "mb_ereg_search_regs()".}}
+mbereg_search_setpos($i);                    // Noncompliant {{Replace this "mbereg_search_setpos()" call with a call to "mb_ereg_search_setpos()".}}
+
+mb_regex_encoding($test_enc);
+mb_ereg_search_init($str, $look_for, $opt);
+while (mb_ereg_search_pos()) {
+    $regs = mb_ereg_search_getregs();
+    printf("%d\n", mb_ereg_search_getpos());
+}
+mb_ereg('[0-9]', $text);
+mb_eregi('äpfel', $text);
+$data = mb_ereg_replace("/[0-9]/","",$data);
+$data = mb_eregi_replace("/[0-9]/","",$data);
+$data = mb_split("/\s/", "hello world");
+$test = mb_ereg_match("a", "some apples");
+$r = mb_ereg_search();
+$r = mb_ereg_search_regs();
+mb_ereg_search_setpos($i);
+
+$buffer = fgetss($handle, 4096);  // Noncompliant {{Remove this call to deprecated "fgetss()".}}
+$buffer = gzgetss($handle, 4096); // Noncompliant {{Remove this call to deprecated "gzgetss()".}}
+
+stream_filter_append($fp); // coverage
+stream_filter_append($fp, $filterName);
+stream_filter_append($fp, 'string.tolower');
+stream_filter_append($fp, 'string.strip_tags', STREAM_FILTER_WRITE, "<strong><em><span>"); // Noncompliant {{Remove this deprecated "string.strip_tags" filter usage.}}
+//                        ^^^^^^^^^^^^^^^^^^^
+
+function f() {
+  $file = new SplFileObject("sample.php");
+  $obj = new Unknown();
+  while (!$file->eof()) {
+    echo $file->fgetss();  // Noncompliant {{Remove this call to deprecated "fgetss()".}}
+//       ^^^^^^^^^^^^^^^
+    echo $obj->fgetss();
+  }
+}
+
+image2wbmp($image);  // Noncompliant {{Replace this "image2wbmp()" call with a call to "imagewbmp()".}}
+imagewbmp($image);
+
+$char = Normalizer::normalize($char_A_ring, Normalizer::NONE);  // Noncompliant {{Do not use this deprecated "Normalizer::NONE" constant.}}
+//                                          ^^^^^^^^^^^^^^^^
+$x = Other::Normalizer::NONE;
+$x = Normalizer::$NONE;
+
+$a = stristr($email, 'e');
+$a = stristr($email, 42);  // Noncompliant {{Convert this integer needle into a string.}}
+//                   ^^
+$a = strrchr($PATH, ":");
+$a = strrchr($PATH, -1);  // Noncompliant {{Convert this integer needle into a string.}}
+
+$user = strstr($email, '@', true);
+$user = strstr($email, 0x41, true);  // Noncompliant {{Convert this integer needle into a string.}}
+
+$pos = strripos($haystack, $needle);
+$pos = strripos($haystack, 127);  // Noncompliant {{Convert this integer needle into a string.}}
+
+$pos1 = stripos($mystring1, null);
+$pos1 = stripos($mystring1, 1);  // Noncompliant {{Convert this integer needle into a string.}}
+
+$pos = strrpos($mystring, "b");
+$pos = strrpos($mystring, 49);  // Noncompliant {{Convert this integer needle into a string.}}
+
+$pos = strpos($mystring, f());
+$pos = strpos($mystring, 32);  // Noncompliant {{Convert this integer needle into a string.}}
+
+$result = strchr($stringA); // coverage
+$result = strchr($stringA,$toFind);
+$result = strchr($stringA, 0); // Noncompliant {{Convert this integer needle into a string.}}
