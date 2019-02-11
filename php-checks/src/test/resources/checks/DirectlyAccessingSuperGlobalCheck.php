4994b2e599a23ea143d1d8daec071478ad1c8c82
Alban Auzeill <alban.auzeill@sonarsource.com>
2019-02-11 11:47:59 +0100

    SONARPHP-883 S2043 should raise an issue when using $_REQUEST

--- php-checks/src/test/resources/checks/DirectlyAccessingSuperGlobalCheck.php
+++ php-checks/src/test/resources/checks/DirectlyAccessingSuperGlobalCheck.php
@@ -2,7 +2,15 @@
 
 $a = $_POST['name'];      // Noncompliant {{Do not access "$_POST" directly.}}
 //   ^^^^^^
+$a = $_COOKIE['name'];    // Noncompliant
+$a = $_ENV['name'];       // Noncompliant
+$a = $_FILES['name'];     // Noncompliant
+$a = $_GET['name'];       // Noncompliant
+$a = $_REQUEST['name'];   // Noncompliant
+$a = $_SERVER['name'];    // Noncompliant
+
 
 $b = $_MY_ARRAY['name'];  // OK
 
 $c = $_SESSION['name']; // OK $_SESSION is a special case and can be accessed directly
+$c = $GLOBALS['name'];  // OK $GLOBALS does not require sanitization
