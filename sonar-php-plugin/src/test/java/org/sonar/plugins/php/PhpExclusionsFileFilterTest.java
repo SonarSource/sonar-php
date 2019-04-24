30c7fced0f401f5ab7514e6ec6a89539e340c6b0
benzonico <nicolas.peru@sonarsource.com>
2019-04-24 17:40:07 +0200

    SONARPHP-909 exclude files with average line length greater than 200 (#463)
    SONARPHP-909 exclude files with average line length greater than 220

--- sonar-php-plugin/src/test/java/org/sonar/plugins/php/PhpExclusionsFileFilterTest.java
+++ sonar-php-plugin/src/test/java/org/sonar/plugins/php/PhpExclusionsFileFilterTest.java
@@ -19,11 +19,17 @@
  */
 package org.sonar.plugins.php;
 
+import com.google.common.io.Files;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import org.junit.Test;
 import org.sonar.api.batch.fs.internal.DefaultInputFile;
 import org.sonar.api.batch.fs.internal.TestInputFileBuilder;
+import org.sonar.api.batch.sensor.internal.SensorContextTester;
 import org.sonar.api.config.internal.MapSettings;
 
+import static org.assertj.core.api.Assertions.fail;
 import static org.fest.assertions.Assertions.assertThat;
 
 public class PhpExclusionsFileFilterTest {
@@ -83,8 +89,57 @@ public class PhpExclusionsFileFilterTest {
     assertThat(filter.accept(inputFile("vendor/some_lib.php"))).isFalse();
   }
 
+  @Test
+  public void should_ignore_php_files_with_average_line_length_over_threshold() throws IOException {
+    PhpExclusionsFileFilter filter = new PhpExclusionsFileFilter(new MapSettings().asConfig());
+    File baseDir = new File("src/test/resources/exclusions");
+    SensorContextTester context = SensorContextTester.create(baseDir);
+    DefaultInputFile testFile = setupSingleFile(baseDir, context, "excluded.php");
+    assertThat(filter.accept(testFile)).isFalse();
+
+    // ignore header comment
+    testFile = setupSingleFile(baseDir, context, "included.php");
+    assertThat(filter.accept(testFile)).isTrue();
+
+    // first line is not a comment nor a single php tag
+    testFile = setupSingleFile(baseDir, context, "excluded2.php");
+    assertThat(filter.accept(testFile)).isFalse();
+
+    testFile = setupSingleFile(baseDir, context, "excluded3.php");
+    assertThat(filter.accept(testFile)).isFalse();
+
+    //
+
+    testFile = setupSingleFile(baseDir, context, "unknown.php", "");
+    try {
+      filter.accept(testFile);
+      fail("unknown file should have failed");
+    } catch (AnalysisException ae) {
+      assertThat(ae).hasMessage("Unable to read file 'unknown.php'");
+    }
+
+    testFile = setupSingleFile(baseDir, context, "empty.php");
+    assertThat(filter.accept(testFile)).isTrue();
+
+  }
+
+  private static DefaultInputFile setupSingleFile(File baseDir, SensorContextTester context, String fileName) throws IOException {
+    return setupSingleFile(baseDir, context, fileName, Files.asCharSource(new File(baseDir, fileName), StandardCharsets.UTF_8).read());
+  }
+
+  private static DefaultInputFile setupSingleFile(File baseDir, SensorContextTester context, String fileName, String content) {
+    DefaultInputFile file1 = TestInputFileBuilder.create("moduleKey", baseDir, new File(baseDir, fileName))
+      .setCharset(StandardCharsets.UTF_8)
+      .initMetadata(content)
+      .setLanguage("php")
+      .build();
+    context.fileSystem().add(file1);
+    return file1;
+  }
+
   private DefaultInputFile inputFile(String file) {
     return new TestInputFileBuilder("test", "test_vendor/" + file)
+      .setCharset(StandardCharsets.UTF_8)
       .setLanguage(language(file))
       .setContents("<?php foo();")
       .build();
