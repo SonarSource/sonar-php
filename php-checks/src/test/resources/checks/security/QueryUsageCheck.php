526bde429c9123ae7df63c5ba96120bbd611fbd1
Elena Vilchik <elena.vilchik@sonarsource.com>
2019-04-24 17:02:33 +0200

    SONARPHP-920 Rule S2077: Update rule to raise only on concatenated and formatted SQL strings (#467)

--- php-checks/src/test/resources/checks/security/QueryUsageCheck.php
+++ php-checks/src/test/resources/checks/security/QueryUsageCheck.php
@@ -2,14 +2,16 @@
 
 function executeMysqlQuery($query, $database)
 {
-    mysql_query($query); // Noncompliant {{Make sure that executing SQL queries is safe here.}}
+    mysql_query("select * from A where " . $query); // Noncompliant {{Make sure that formatting this SQL query is safe here.}}
 //  ^^^^^^^^^^^
-    mysql_db_query($database, $query); // Noncompliant
-    mysql_unbuffered_query($query); // Noncompliant
+
+    mysql_query($query); // OK
+    mysql_db_query($database, $query); // OK
+    mysql_unbuffered_query($query); // OK
 
     mysql_query("select * from db"); // OK
     mysql_query('select * from db'); // OK
-    mysql_query("select * from $db"); // Noncompliant
+    mysql_query("select * from $db"); // Noncompliant {{Make sure that formatting this SQL query is safe here.}}
     mysql_query('select * from $db'); // OK
 
     mysql_db_query($database, "select * from db".$query); // Noncompliant
@@ -17,37 +19,37 @@ function executeMysqlQuery($query, $database)
 }
 
 function executeQuery($query) {
-    mssql_query($query); // Noncompliant
+    mssql_query("select * from db".$query); // Noncompliant
     MSSQL_query("SELECT * FROM mytable"); // OK
 }
 
-function executeMysqliQuery($statement, $column) {
+function executeMysqliQuery($query) {
     // Object oriented style
     $mysqli = new mysqli("localhost", "myUser", "myPassword", "myDatabase");
-    $mysqli->query($statement); // Noncompliant
-    $mysqli->real_query($statement); // Noncompliant
-    $mysqli->multi_query($statement); // Noncompliant
-    $mysqli->send_query($statement); // Noncompliant
+    $mysqli->query("select * from db".$query); // Noncompliant
+    $mysqli->real_query("select * from db".$query); // Noncompliant
+    $mysqli->multi_query("select * from db".$query); // Noncompliant
+    $mysqli->send_query("select * from db".$query); // Noncompliant
 
     // Procedural style
     $link = mysqli_connect("localhost", "myUser", "myPassword", "myDatabase");
-    mysqli_query($link, $statement); // Noncompliant
-    mysqli_real_query($link, $statement); // Noncompliant
-    mysqli_multi_query($link, $statement); // Noncompliant
-    mysqli_send_query($link, $statement); // Noncompliant
+    mysqli_query($link, "select * from db".$query); // Noncompliant
+    mysqli_real_query($link, "select * from db".$query); // Noncompliant
+    mysqli_multi_query($link, "select * from db".$query); // Noncompliant
+    mysqli_send_query($link, "select * from db".$query); // Noncompliant
 
     $mysqli->query("SELECT * from $mytable"); // Noncompliant
     $mysqli->real_query("SELECT * from $mytable"); // Noncompliant
 }
 
-function executePDOQuery($dsn, $user, $password, $statement, $type, $obj) {
+function executePDOQuery($dsn, $user, $password, $query, $type, $obj) {
     $conn = new PDO($dsn, $user, $password);
-    $conn->query($statement); // Noncompliant
+    $conn->query("select * from db".$query); // Noncompliant
     $conn->query('SELECT * from test'); // OK
-    $conn->query($statement, PDO::FETCH_COLUMN, 42); // Noncompliant
-    $conn->query($statement, PDO::FETCH_CLASS, 'MyClass'); // Noncompliant
-    $conn->query($statement, PDO::FETCH_INTO, $obj); // Noncompliant
-    $conn->exec($statement); // Noncompliant
+    $conn->query("select * from db".$query, PDO::FETCH_COLUMN, 42); // Noncompliant
+    $conn->query("select * from db".$query, PDO::FETCH_CLASS, 'MyClass'); // Noncompliant
+    $conn->query("select * from db".$query, PDO::FETCH_INTO, $obj); // Noncompliant
+    $conn->exec("select * from db".$query); // Noncompliant
 
     $hardcoded_statement = 'SELECT * from test';
     $conn->prepare($hardcoded_statement); // OK
@@ -58,9 +60,9 @@ function executePDOQuery($dsn, $user, $password, $statement, $type, $obj) {
 }
 
 function executePostgresQuery($conn, $query, $tableName) {
-    pg_query($query); // Noncompliant
-    PG_Query($conn, $query); // Noncompliant
-    pg_send_query ($conn, $query); // Noncompliant
+    pg_query("select * from db".$query); // Noncompliant
+    PG_Query($conn, "select * from db".$query); // Noncompliant
+    pg_send_query ($conn, "select * from db".$query); // Noncompliant
 
     pg_send_query($conn, 'INSERT INTO test (ID) VALUES (1)'); // OK
     pg_query('INSERT INTO test (ID) VALUES (1)'); // OK
@@ -70,9 +72,9 @@ function executePostgresQuery($conn, $query, $tableName) {
     pg_query("INSERT INTO test (ID) VALUES ($query)"); // Noncompliant
     pg_query($conn, "INSERT INTO test (ID) VALUES ($query)"); // Noncompliant
 
-    $cond = array('id'=>'1');
+    $cond = array('id'=>('1'.$query));
     $data = array('id'=>'2');
-    pg_update($conn, $tableName, $data, $cond); // Noncompliant
+    pg_update($conn, $tableName, $data, $cond); // Ok, "pg_update" is ignored by this rule
 }
 
 // coverage
