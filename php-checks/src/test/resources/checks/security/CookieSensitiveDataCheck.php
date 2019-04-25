300bf50556cdd8e874097558aeb5b8e178537247
Nicolas PERU <nicolas.peru@sonarsource.com>
2019-04-25 14:58:02 +0200

    Revert "SONARPHP-823 Rule S2255: update implementation to include Cookie reading (#402)"
    This reverts commit 6b08ee9807f6a93b69b58ec3a08eb91f19a43a7c.

--- php-checks/src/test/resources/checks/security/CookieSensitiveDataCheck.php
+++ php-checks/src/test/resources/checks/security/CookieSensitiveDataCheck.php
@@ -1,22 +1,22 @@
 <?php
 
-  setcookie($name, $value, $expire, $path, $domain, $secure, false); // Noncompliant {{Make sure that this cookie is used safely.}}
+  setcookie($name, $value, $expire, $path, $domain, $secure, false); // Noncompliant {{Make sure storing this data in this cookie is safe here.}}
 //^^^^^^^^^
 
-  setrawcookie($name, $value, $expire, $path, $domain, true, true); // Noncompliant {{Make sure that this cookie is used safely.}}
+  setrawcookie($name, $value, $expire, $path, $domain, true, true); // Noncompliant {{Make sure storing this data in this cookie is safe here.}}
 //^^^^^^^^^^^^
 
-setcookie($name, $value); // Noncompliant {{Make sure that this cookie is used safely.}}
-setrawcookie($name, $value); // Noncompliant {{Make sure that this cookie is used safely.}}
+setcookie($name, $value); // Noncompliant {{Make sure storing this data in this cookie is safe here.}}
+setrawcookie($name, $value); // Noncompliant {{Make sure storing this data in this cookie is safe here.}}
 
 SetCookie($name, false); // Noncompliant {{Make sure that this cookie is used safely.}}
 SetRawCookie($name, false); // Noncompliant {{Make sure that this cookie is used safely.}}
 
-setcookie($name, 1); // Noncompliant {{Make sure that this cookie is used safely.}}
-setrawcookie($name, 1); // Noncompliant {{Make sure that this cookie is used safely.}}
+setcookie($name, 1); // Noncompliant {{Make sure storing this data in this cookie is safe here.}}
+setrawcookie($name, 1); // Noncompliant {{Make sure storing this data in this cookie is safe here.}}
 
-setcookie($name, "x"); // Noncompliant {{Make sure that this cookie is used safely.}}
-setrawcookie($name, "x"); // Noncompliant {{Make sure that this cookie is used safely.}}
+setcookie($name, "x"); // Noncompliant {{Make sure storing this data in this cookie is safe here.}}
+setrawcookie($name, "x"); // Noncompliant {{Make sure storing this data in this cookie is safe here.}}
 
 setcookie($name); // Ok
 setcookie($name, NULL); // Ok
@@ -26,16 +26,3 @@ setrawcookie($name); // Ok
 setrawcookie($name, NULL); // Ok
 setrawcookie($name, ""); // Ok
 setrawcookie($name, "   "); // Ok
-
-  $_COOKIE["name"]; // Noncompliant
-//^^^^^^^^
-  $HTTP_COOKIE_VARS["name"]; // Noncompliant
-//^^^^^^^^^^^^^^^^^
-$abc = $_COOKIE["name"]; // Noncompliant
-
-unset($_COOKIE["name"]);
-unset($HTTP_COOKIE_VARS["name"]);
-isset($_COOKIE["name"]);
-isset($HTTP_COOKIE_VARS["name"]);
-
-other_function($_COOKIE["name"]); // Noncompliant
