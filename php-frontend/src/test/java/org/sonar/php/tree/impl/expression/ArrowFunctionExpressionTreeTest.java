3b069cb7af7ea57768000fa0ed91fecc1c230408
Alban Auzeill <alban.auzeill@sonarsource.com>
2019-11-28 15:03:04 +0100

    Fix from review, add yield and array index/value precedence tests

--- php-frontend/src/test/java/org/sonar/php/tree/impl/expression/ArrowFunctionExpressionTreeTest.java
+++ php-frontend/src/test/java/org/sonar/php/tree/impl/expression/ArrowFunctionExpressionTreeTest.java
@@ -19,11 +19,20 @@
  */
 package org.sonar.php.tree.impl.expression;
 
+import com.sonar.sslr.api.typed.ActionParser;
 import org.junit.Test;
 import org.sonar.php.PHPTreeModelTest;
+import org.sonar.php.parser.PHPParserBuilder;
+import org.sonar.plugins.php.api.tree.CompilationUnitTree;
 import org.sonar.plugins.php.api.tree.Tree;
 import org.sonar.plugins.php.api.tree.declaration.BuiltInTypeTree;
+import org.sonar.plugins.php.api.tree.declaration.FunctionDeclarationTree;
+import org.sonar.plugins.php.api.tree.expression.ArrayInitializerFunctionTree;
+import org.sonar.plugins.php.api.tree.expression.ArrayPairTree;
 import org.sonar.plugins.php.api.tree.expression.ArrowFunctionExpressionTree;
+import org.sonar.plugins.php.api.tree.expression.AssignmentExpressionTree;
+import org.sonar.plugins.php.api.tree.expression.YieldExpressionTree;
+import org.sonar.plugins.php.api.tree.statement.ExpressionStatementTree;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -53,9 +62,52 @@ public class ArrowFunctionExpressionTreeTest extends PHPTreeModelTest {
     assertThat(tree.referenceToken().text()).isEqualTo("&");
     assertThat(tree.parameters().parameters()).hasSize(1);
     assertThat(tree.parameters().parameters().get(0).variableIdentifier().text()).isEqualTo("$x");
-    assertThat(((BuiltInTypeTree)tree.returnTypeClause().type().typeName()).token().text()).isEqualTo("string");
+    assertThat(((BuiltInTypeTree) tree.returnTypeClause().type().typeName()).token().text()).isEqualTo("string");
     assertThat(tree.doubleArrowToken().text()).isEqualTo("=>");
     assertThat(expressionToString(tree.body())).isEqualTo("$x . '_'");
+  }
+
+  @Test
+  public void arrow_function_and_yield_precedence() throws Exception {
+    ActionParser<Tree> parser = PHPParserBuilder.createParser();
+    CompilationUnitTree root = (CompilationUnitTree) parser.parse("<?php\n" +
+      "function f() {\n" +
+      " yield foo($a) => $a;\n" +
+      " yield fn($a)  => $a;\n" +
+      "}\n");
+    FunctionDeclarationTree function = (FunctionDeclarationTree) root.script().statements().get(0);
+
+    YieldExpressionTree yield0 = (YieldExpressionTree) ((ExpressionStatementTree) function.body().statements().get(0)).expression();
+    assertThat(yield0.key().getKind()).isEqualTo(Tree.Kind.FUNCTION_CALL);
+    assertThat(yield0.doubleArrowToken()).isNotNull();
+    assertThat(yield0.value().getKind()).isEqualTo(Tree.Kind.VARIABLE_IDENTIFIER);
+
+    YieldExpressionTree yield1 = (YieldExpressionTree) ((ExpressionStatementTree) function.body().statements().get(1)).expression();
+    assertThat(yield1.key()).isNull();
+    assertThat(yield1.doubleArrowToken()).isNull();
+    assertThat(yield1.value().getKind()).isEqualTo(Tree.Kind.ARROW_FUNCTION_EXPRESSION);
+  }
+
+  @Test
+  public void arrow_function_and_array_index_value_precedence() throws Exception {
+    ActionParser<Tree> parser = PHPParserBuilder.createParser();
+    CompilationUnitTree root = (CompilationUnitTree) parser.parse("<?php\n" +
+      "$x = array(\n" +
+      "    foo($a) => $a,\n" +
+      "    fn($a)  => $a\n" +
+      ");\n");
+    ExpressionStatementTree statement = (ExpressionStatementTree) root.script().statements().get(0);
+    ArrayInitializerFunctionTree arrayInitializer = (ArrayInitializerFunctionTree) ((AssignmentExpressionTree) statement.expression()).value();
+
+    ArrayPairTree arrayPair0 = arrayInitializer.arrayPairs().get(0);
+    assertThat(arrayPair0.key().getKind()).isEqualTo(Tree.Kind.FUNCTION_CALL);
+    assertThat(arrayPair0.doubleArrowToken()).isNotNull();
+    assertThat(arrayPair0.value().getKind()).isEqualTo(Tree.Kind.VARIABLE_IDENTIFIER);
+
+    ArrayPairTree arrayPair1 = arrayInitializer.arrayPairs().get(1);
+    assertThat(arrayPair1.key()).isNull();
+    assertThat(arrayPair1.doubleArrowToken()).isNull();
+    assertThat(arrayPair1.value().getKind()).isEqualTo(Tree.Kind.ARROW_FUNCTION_EXPRESSION);
   }
 
 }
