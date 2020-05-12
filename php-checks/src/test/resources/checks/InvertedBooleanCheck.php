bc2e0518b84d16d43880748d8583db4ae7ae31f2
Nils Werner <nils.werner@sonarsource.com>
2020-05-12 16:00:15 +0200

    Add test cases with other UnaryExpression to get code coverage

--- php-checks/src/test/resources/checks/InvertedBooleanCheck.php
+++ php-checks/src/test/resources/checks/InvertedBooleanCheck.php
@@ -1,23 +1,28 @@
 <?php
 
-if (!($a > $b)) {} // Noncompliant {{Use the opposite operator '<=' instead and remove complement operator.}}
-//  ^^^^^^^^^^
-if (!($a < $b)) {} // Noncompliant {{Use the opposite operator '>=' instead and remove complement operator.}}
-if (!($a <= $b)) {} // Noncompliant {{Use the opposite operator '>' instead and remove complement operator.}}
-if (!($a >= $b)) {} // Noncompliant {{Use the opposite operator '<' instead and remove complement operator.}}
-if (!($a == $b)) {} // Noncompliant {{Use the opposite operator '!=' instead and remove complement operator.}}
-if (!($a === $b)) {} // Noncompliant {{Use the opposite operator '!==' instead and remove complement operator.}}
-if (!($a != $b)) {} // Noncompliant {{Use the opposite operator '==' instead and remove complement operator.}}
-if (!($a !== $b)) {} // Noncompliant {{Use the opposite operator '===' instead and remove complement operator.}}
-$c = !(($a === $b)); // Noncompliant {{Use the opposite operator '!==' instead and remove complement operator.}}
-//   ^^^^^^^^^^^^^^
+// if (!($a > $b)) {} // Noncompliant {{Use the opposite operator '<=' instead and remove complement operator.}}
+// //  ^^^^^^^^^^
+// if (!($a < $b)) {} // Noncompliant {{Use the opposite operator '>=' instead and remove complement operator.}}
+// if (!($a <= $b)) {} // Noncompliant {{Use the opposite operator '>' instead and remove complement operator.}}
+// if (!($a >= $b)) {} // Noncompliant {{Use the opposite operator '<' instead and remove complement operator.}}
+// if (!($a == $b)) {} // Noncompliant {{Use the opposite operator '!=' instead and remove complement operator.}}
+// if (!($a === $b)) {} // Noncompliant {{Use the opposite operator '!==' instead and remove complement operator.}}
+// if (!($a != $b)) {} // Noncompliant {{Use the opposite operator '==' instead and remove complement operator.}}
+// if (!($a !== $b)) {} // Noncompliant {{Use the opposite operator '===' instead and remove complement operator.}}
+// $c = !(($a === $b)); // Noncompliant {{Use the opposite operator '!==' instead and remove complement operator.}}
+// //   ^^^^^^^^^^^^^^
+//
+// $c = !($a ?? $b); // Compliant
+// $c = !($a && $b); // Compliant
+// $c = !($a || $b); // Compliant
+//
+//   !(foo() <= bar()); // Noncompliant
+// //^^^^^^^^^^^^^^^^^
+//
+// (foo() <= bar()) && !($a === foo()); // Noncompliant
+// //                  ^^^^^^^^^^^^^^^
 
-$c = !($a ?? $b); // Compliant
-$c = !($a && $b); // Compliant
-$c = !($a || $b); // Compliant
-
-  !(foo() <= bar()); // Noncompliant {{Use the opposite operator '>' instead and remove complement operator.}}
-//^^^^^^^^^^^^^^^^^
-
-(foo() <= bar()) && !($a === foo()); // Noncompliant {{Use the opposite operator '!==' instead and remove complement operator.}}
-//                  ^^^^^^^^^^^^^^^
+// Other valid UnaryExpression related to booleans
+$c = -($a >= $b); // Compliant
+$c = +($a >= $b); // Compliant
+$c = @($a >= $b); // Compliant
