0fea4c338188e86700dd383c6f32db0696546adf
Karim El Ouerghemmi <karim.ouerghemmi@sonarsource.com>
2020-05-15 11:53:28 +0200

    implement preliminary version

--- php-checks/src/main/java/org/sonar/php/checks/OverwrittenArrayElementCheck.java
+++ php-checks/src/main/java/org/sonar/php/checks/OverwrittenArrayElementCheck.java
@@ -1,9 +1,73 @@
 package org.sonar.php.checks;
 
+import com.google.common.collect.ImmutableList;
 import org.sonar.check.Rule;
-import org.sonar.plugins.php.api.visitors.PHPVisitorCheck;
+import org.sonar.plugins.php.api.tree.ScriptTree;
+import org.sonar.plugins.php.api.tree.Tree;
+import org.sonar.plugins.php.api.tree.expression.ArrayAccessTree;
+import org.sonar.plugins.php.api.tree.expression.AssignmentExpressionTree;
+import org.sonar.plugins.php.api.tree.expression.ExpressionTree;
+import org.sonar.plugins.php.api.tree.expression.LiteralTree;
+import org.sonar.plugins.php.api.tree.expression.VariableIdentifierTree;
+import org.sonar.plugins.php.api.tree.statement.BlockTree;
+import org.sonar.plugins.php.api.tree.statement.ExpressionStatementTree;
+import org.sonar.plugins.php.api.tree.statement.StatementTree;
+import org.sonar.plugins.php.api.visitors.PHPSubscriptionCheck;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Rule(key = "S4143")
-public class OverwrittenArrayElementCheck extends PHPVisitorCheck {
+public class OverwrittenArrayElementCheck extends PHPSubscriptionCheck {
 
+  @Override
+  public List<Tree.Kind> nodesToVisit() {
+    return ImmutableList.of(Tree.Kind.BLOCK, Tree.Kind.SCRIPT);
+  }
+
+  @Override
+  public void visitNode(Tree tree) {
+    List<StatementTree> statementTrees;
+    if (tree.is(Tree.Kind.BLOCK)) {
+      statementTrees = ((BlockTree) tree).statements();
+    } else {
+      statementTrees = ((ScriptTree) tree).statements();
+    }
+
+    Map<String, Tree> writtenAndUnread = new HashMap<>();
+    for (StatementTree statementTree : statementTrees) {
+      if (!isAssignmentStatement(statementTree)) {
+        writtenAndUnread.clear();
+        continue;
+      }
+
+      ExpressionTree assignVariableTree = ((AssignmentExpressionTree) ((ExpressionStatementTree) statementTree).expression()).variable();
+
+      if (!assignVariableTree.is(Tree.Kind.ARRAY_ACCESS) ||
+        !((ArrayAccessTree) assignVariableTree).object().is(Tree.Kind.VARIABLE_IDENTIFIER) ||
+        !((ArrayAccessTree) assignVariableTree).offset().is(Tree.Kind.NUMERIC_LITERAL, Tree.Kind.REGULAR_STRING_LITERAL)) {
+        continue;
+      }
+
+      ArrayAccessTree arrayAccessTree = (ArrayAccessTree) assignVariableTree;
+      String key = ((VariableIdentifierTree)arrayAccessTree.object()).text() + ((LiteralTree)arrayAccessTree.offset()).value();
+
+      if (writtenAndUnread.containsKey(key)) {
+        context().newIssue(this, statementTree, "Verify this is the key that was intended; it was already set before");
+      }
+
+      writtenAndUnread.put(key, statementTree);
+    }
+
+    super.visitNode(tree);
+  }
+
+  private static boolean isAssignmentStatement(StatementTree tree) {
+    if (!tree.is(Tree.Kind.EXPRESSION_STATEMENT)) {
+      return false;
+    }
+
+    return ((ExpressionStatementTree) tree).expression().is(Tree.Kind.ASSIGNMENT); // TODO: Check different kinds?
+  }
 }
--- php-checks/src/test/resources/checks/OverwrittenArrayElementCheck.php
+++ php-checks/src/test/resources/checks/OverwrittenArrayElementCheck.php
@@ -1,8 +1,11 @@
 <?php
 
-$a[1] = "foo";
+$a[2] = "foo";
 $a[2] = "bar"; // Noncompliant
 
 $b[1] = "foo";
-echo $b[1];
 $b[2] = "bar"; // Compliant
+
+$c[1] = "foo";
+doSomething($c[1]);
+$c[1] = "bar";
