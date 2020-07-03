a0ff48858ad2f31b538939a4c31620646e61f040
karim-ouerghemmi-sonarsource <64004037+karim-ouerghemmi-sonarsource@users.noreply.github.com>
2020-07-03 16:03:45 +0200

    SONARPHP-972: Rule S4790 should raise when insecure algos are passed to hash(), hash_init(), hash_pbkdf2(), mhash() (#576)
    * extend S4970 to verify other hash functions
    
    * add compliant code tests

--- php-checks/src/test/resources/checks/security/CryptographicHashCheck.php
+++ php-checks/src/test/resources/checks/security/CryptographicHashCheck.php
@@ -11,3 +11,59 @@ sha1_file($str);
 md5($str, true); // Noncompliant {{Make sure this weak hash algorithm is not used in a sensitive context here.}}
 sha1($str);       // Noncompliant {{Make sure this weak hash algorithm is not used in a sensitive context here.}}
 SHA1($str, false); // Noncompliant {{Make sure this weak hash algorithm is not used in a sensitive context here.}}
+
+hash_init("md2"); // Noncompliant
+//        ^^^^^
+hash_init("md4"); // Noncompliant
+hash_init("md5"); // Noncompliant
+hash_init("sha1"); // Noncompliant
+hash_init("sha224"); // Noncompliant
+hash_init("ripemd128"); // Noncompliant
+hash_init("ripemd160"); // Noncompliant
+hash_init("haval160,3"); // Noncompliant
+hash_init("haval192,3"); // Noncompliant
+hash_init("haval224,3"); // Noncompliant
+hash_init("sha512"); // Compliant
+hash_init($unknown); // Compliant
+
+
+hash("md2", $data); // Noncompliant
+hash("md4", $data); // Noncompliant
+hash("md5", $data); // Noncompliant
+hash("sha1", $data); // Noncompliant
+hash("sha224", $data); // Noncompliant
+hash("ripemd128", $data); // Noncompliant
+hash("ripemd160", $data); // Noncompliant
+hash("haval160,3", $data); // Noncompliant
+hash("haval192,3", $data); // Noncompliant
+hash("haval224,3", $data); // Noncompliant
+hash("sha512", $data); // Compliant
+hash($unknown, $data); // Compliant
+
+hash_pbkdf2("md2", $password, $salt, $iterations); // Noncompliant
+hash_pbkdf2("md4", $password, $salt, $iterations); // Noncompliant
+hash_pbkdf2("md5", $password, $salt, $iterations); // Noncompliant
+hash_pbkdf2("sha1", $password, $salt, $iterations); // Noncompliant
+hash_pbkdf2("sha224", $password, $salt, $iterations); // Noncompliant
+hash_pbkdf2("ripemd128", $password, $salt, $iterations); // Noncompliant
+hash_pbkdf2("ripemd160", $password, $salt, $iterations); // Noncompliant
+hash_pbkdf2("haval160,3", $password, $salt, $iterations); // Noncompliant
+hash_pbkdf2("haval192,3", $password, $salt, $iterations); // Noncompliant
+hash_pbkdf2("haval224,3", $password, $salt, $iterations); // Noncompliant
+hash_pbkdf2("sha512", $password, $salt, $iterations); // Compliant
+hash_pbkdf2($unknown, $password, $salt, $iterations); // Compliant
+
+mhash(MHASH_MD2, $data); // Noncompliant
+mhash(MHASH_MD4, $data); // Noncompliant
+mhash(MHASH_MD5, $data); // Noncompliant
+mhash(MHASH_RIPEMD128, $data); // Noncompliant
+mhash(MHASH_SHA1, $data); // Noncompliant
+mhash(MHASH_SHA192, $data); // Noncompliant
+mhash(MHASH_SHA224, $data); // Noncompliant
+mhash(MHASH_HAVAL128, $data); // Noncompliant
+mhash(MHASH_HAVAL160, $data); // Noncompliant
+mhash(MHASH_HAVAL192, $data); // Noncompliant
+mhash(MHASH_HAVAL224, $data); // Noncompliant
+mhash(MHASH_HAVAL224, $data); // Noncompliant
+mhash(MHASH_SHA512, $data); // Compliant
+mhash("xyz", $data); // Compliant
