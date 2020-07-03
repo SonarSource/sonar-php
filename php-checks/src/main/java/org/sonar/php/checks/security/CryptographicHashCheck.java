39d981a006cfa9065e8551ccb8de17b14db38d73
Karim El Ouerghemmi <karim.ouerghemmi@sonarsource.com>
2020-07-03 10:56:03 +0200

    extend S4970 to verify other hash functions

--- php-checks/src/main/java/org/sonar/php/checks/security/CryptographicHashCheck.java
+++ php-checks/src/main/java/org/sonar/php/checks/security/CryptographicHashCheck.java
@@ -21,22 +21,70 @@ package org.sonar.php.checks.security;
 
 import com.google.common.collect.ImmutableSet;
 import org.sonar.check.Rule;
-import org.sonar.php.checks.utils.FunctionUsageCheck;
+import org.sonar.php.checks.utils.CheckUtils;
+import org.sonar.php.checks.utils.FunctionArgumentCheck;
+import org.sonar.plugins.php.api.tree.expression.ExpressionTree;
 import org.sonar.plugins.php.api.tree.expression.FunctionCallTree;
 
+import java.util.Set;
+
 @Rule(key = "S4790")
-public class CryptographicHashCheck extends FunctionUsageCheck {
+public class CryptographicHashCheck extends FunctionArgumentCheck {
 
   private static final String MESSAGE = "Make sure this weak hash algorithm is not used in a sensitive context here.";
 
-  @Override
-  protected ImmutableSet<String> functionNames() {
-    return ImmutableSet.of("md5", "sha1");
-  }
+  private static final Set<String> WEAK_HASH_FUNCTIONS = ImmutableSet.of("md5", "sha1");
+  private static final Set<String> WEAK_HASH_ARGUMENTS = ImmutableSet.of(
+    "md2",
+    "md4",
+    "md5",
+    "sha1",
+    "sha224",
+    "ripemd128",
+    "ripemd160",
+    "haval160,3",
+    "haval192,3",
+    "haval224,3"
+  );
+  private static final Set<String> WEAK_MHASH_ARGUMENTS = ImmutableSet.of(
+    "MHASH_MD2",
+    "MHASH_MD4",
+    "MHASH_MD5",
+    "MHASH_RIPEMD128",
+    "MHASH_SHA1",
+    "MHASH_SHA192",
+    "MHASH_SHA224",
+    "MHASH_HAVAL128",
+    "MHASH_HAVAL160",
+    "MHASH_HAVAL192",
+    "MHASH_HAVAL224"
+  );
+
+  private static final ArgumentVerifier hashArgumentVerifier = new ArgumentVerifier(0, WEAK_HASH_ARGUMENTS);
+  private static final ArgumentVerifier mHashArgumentVerifier = new ArgumentVerifier(0, WEAK_MHASH_ARGUMENTS);
 
   @Override
+  public void visitFunctionCall(FunctionCallTree tree) {
+    super.visitFunctionCall(tree);
+
+    String functionName = CheckUtils.getLowerCaseFunctionName(tree);
+    if (WEAK_HASH_FUNCTIONS.contains(functionName)) {
+      createIssue(tree);
+      return;
+    }
+
+    checkArgument(tree, "hash_init", hashArgumentVerifier);
+    checkArgument(tree, "hash", hashArgumentVerifier);
+    checkArgument(tree, "hash_pbkdf2", hashArgumentVerifier);
+    checkArgument(tree, "mhash", mHashArgumentVerifier);
+  }
+
   protected void createIssue(FunctionCallTree tree) {
     context().newIssue(this, tree.callee(), MESSAGE);
   }
 
+  @Override
+  protected void createIssue(ExpressionTree argument) {
+    context().newIssue(this, argument, MESSAGE);
+  }
 }
--- php-checks/src/test/resources/checks/security/CryptographicHashCheck.php
+++ php-checks/src/test/resources/checks/security/CryptographicHashCheck.php
@@ -11,3 +11,50 @@ sha1_file($str);
 md5($str, true); // Noncompliant {{Make sure this weak hash algorithm is not used in a sensitive context here.}}
 sha1($str);       // Noncompliant {{Make sure this weak hash algorithm is not used in a sensitive context here.}}
 SHA1($str, false); // Noncompliant {{Make sure this weak hash algorithm is not used in a sensitive context here.}}
+
+hash_init("md2"); // Noncompliant
+//        ^^^^^
+hash_init("md4"); // Noncompliant
+hash_init("md5"); // Noncompliant
+hash_init("sha1"); // Noncompliant
+hash_init("sha224"); // Noncompliant
+hash_init("ripemd128"); // Noncompliant
+hash_init("ripemd160"); // Noncompliant
+hash_init("haval160,3"); // Noncompliant
+hash_init("haval192,3"); // Noncompliant
+hash_init("haval224,3"); // Noncompliant
+
+
+hash("md2"); // Noncompliant
+hash("md4"); // Noncompliant
+hash("md5"); // Noncompliant
+hash("sha1"); // Noncompliant
+hash("sha224"); // Noncompliant
+hash("ripemd128"); // Noncompliant
+hash("ripemd160"); // Noncompliant
+hash("haval160,3"); // Noncompliant
+hash("haval192,3"); // Noncompliant
+hash("haval224,3"); // Noncompliant
+
+hash_pbkdf2("md2", $password, $salt, $iterations); // Noncompliant
+hash_pbkdf2("md4", $password, $salt, $iterations); // Noncompliant
+hash_pbkdf2("md5", $password, $salt, $iterations); // Noncompliant
+hash_pbkdf2("sha1", $password, $salt, $iterations); // Noncompliant
+hash_pbkdf2("sha224", $password, $salt, $iterations); // Noncompliant
+hash_pbkdf2("ripemd128", $password, $salt, $iterations); // Noncompliant
+hash_pbkdf2("ripemd160", $password, $salt, $iterations); // Noncompliant
+hash_pbkdf2("haval160,3", $password, $salt, $iterations); // Noncompliant
+hash_pbkdf2("haval192,3", $password, $salt, $iterations); // Noncompliant
+hash_pbkdf2("haval224,3", $password, $salt, $iterations); // Noncompliant
+
+mhash(MHASH_MD2, $data); // Noncompliant
+mhash(MHASH_MD4, $data); // Noncompliant
+mhash(MHASH_MD5, $data); // Noncompliant
+mhash(MHASH_RIPEMD128, $data); // Noncompliant
+mhash(MHASH_SHA1, $data); // Noncompliant
+mhash(MHASH_SHA192, $data); // Noncompliant
+mhash(MHASH_SHA224, $data); // Noncompliant
+mhash(MHASH_HAVAL128, $data); // Noncompliant
+mhash(MHASH_HAVAL160, $data); // Noncompliant
+mhash(MHASH_HAVAL192, $data); // Noncompliant
+mhash(MHASH_HAVAL224, $data); // Noncompliant
