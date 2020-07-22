d2400b67fec1b08907dc2d63a5daeb58a4f10924
Nils Werner <nils.werner@sonarsource.com>
2020-07-22 11:12:53 +0200

    Update UnusedExceptionCheck implementation and test cases

--- php-checks/src/test/resources/checks/UnusedExceptionCheck.php
+++ php-checks/src/test/resources/checks/UnusedExceptionCheck.php
@@ -1,24 +1,39 @@
 <?php
 
-/***/
-function f() {
-  new SomeClass();    // OK
-  someFunction();    // OK
-  someException();    // OK
-  someException;    // OK
-  new someException();    // Noncompliant {{Throw this exception or remove this useless statement}}
+class SomeException extends Exception {}
+
+new SomeClass();    // OK
+SomeFunction();    // OK
+SomeException();    // OK
+SomeException;    // OK
+
+  new SomeException();    // Noncompliant {{Throw this exception or remove this useless statement}}
 //^^^^^^^^^^^^^^^^^^^
-  new someException; // Noncompliant {{Throw this exception or remove this useless statement}}
+  new SomeException; // Noncompliant {{Throw this exception or remove this useless statement}}
 //^^^^^^^^^^^^^^^^^
 
-  new foo::someException(); // Noncompliant {{Throw this exception or remove this useless statement}}
-//^^^^^^^^^^^^^^^^^^^^^^^^
+throw new SomeException(); // OK
+
+new foo::SomeException(); // OK - No valid PHP syntax but is parsed
+$exception = new SomeException(); // OK
 
 $util->setLogger(new class {
     public function log($msg)
     {
-      new someException; // Noncompliant {{Throw this exception or remove this useless statement}}
+      new SomeException; // Noncompliant {{Throw this exception or remove this useless statement}}
     //^^^^^^^^^^^^^^^^^
     }
 });
-}
+
+class MyError extends Exception {}
+new MyError(); // Noncompliant
+throw new MyError(); // OK
+
+class MyOtherError extends exception {}
+new MyOtherError(); // Noncompliant
+
+class NotReallyAnException {}
+new NotReallyAnException(); // OK
+
+class MyRuntimeError extends RuntimeException {}
+new MyRuntimeError(); // Noncompliant
