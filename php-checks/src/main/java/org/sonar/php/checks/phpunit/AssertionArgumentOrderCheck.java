3f9a5757c4955a2af3238b3d11203e6fd9120fb1
Nils Werner <nils.werner@sonarsource.com>
2020-07-29 09:42:37 +0200

    Fix index out of bound and move assignmentExpressionVisitor to abstract class

--- php-checks/src/main/java/org/sonar/php/checks/phpunit/AssertionArgumentOrderCheck.java
+++ php-checks/src/main/java/org/sonar/php/checks/phpunit/AssertionArgumentOrderCheck.java
@@ -22,54 +22,31 @@ package org.sonar.php.checks.phpunit;
 import java.util.Optional;
 import org.sonar.check.Rule;
 import org.sonar.php.checks.utils.PhpUnitCheck;
-import org.sonar.php.tree.visitors.AssignmentExpressionVisitor;
-import org.sonar.plugins.php.api.symbols.Symbol;
-import org.sonar.plugins.php.api.tree.CompilationUnitTree;
-import org.sonar.plugins.php.api.tree.Tree;
+import org.sonar.php.checks.utils.RequireAssignmentExpressionVisitor;
 import org.sonar.plugins.php.api.tree.expression.ExpressionTree;
 import org.sonar.plugins.php.api.tree.expression.FunctionCallTree;
 
-import static org.sonar.plugins.php.api.tree.Tree.*;
+import static org.sonar.plugins.php.api.tree.Tree.Kind;
 
 @Rule(key = "S3415")
-public class AssertionArgumentOrderCheck extends PhpUnitCheck {
+public class AssertionArgumentOrderCheck extends PhpUnitCheck implements RequireAssignmentExpressionVisitor {
 
   private static final String MESSAGE = "Swap these 2 arguments so they are in the correct order: expected value, actual value.";
   private static final Kind[] LITERAL = {Kind.BOOLEAN_LITERAL, Kind.NULL_LITERAL, Kind.NUMERIC_LITERAL, Kind.EXPANDABLE_STRING_LITERAL, Kind.REGULAR_STRING_LITERAL};
 
-  private AssignmentExpressionVisitor assignmentExpressionVisitor;
-
   @Override
   public void visitFunctionCall(FunctionCallTree tree) {
-    Optional<Assertion> assertion = getAssertion(tree);
-    if (assertion.isPresent() && assertion.get().hasExpectedValue()) {
-      ExpressionTree expected = tree.arguments().get(0);
-      ExpressionTree actual = tree.arguments().get(1);
-      if (getAssignedValue(actual).is(LITERAL) && !getAssignedValue(expected).is(LITERAL)) {
-        context().newIssue(this, actual, MESSAGE).secondary(expected, null);
+    if (isPhpUnitTestMethod()) {
+      Optional<Assertion> assertion = getAssertion(tree);
+      if (tree.arguments().size() >= 2 && assertion.isPresent() && assertion.get().hasExpectedValue()) {
+        ExpressionTree expected = tree.arguments().get(0);
+        ExpressionTree actual = tree.arguments().get(1);
+        if (getAssignedValue(actual).is(LITERAL) && !getAssignedValue(expected).is(LITERAL)) {
+          context().newIssue(this, actual, MESSAGE).secondary(expected, null);
+        }
       }
     }
 
     super.visitFunctionCall(tree);
-  }
-
-  @Override
-  public void visitCompilationUnit(CompilationUnitTree tree) {
-    assignmentExpressionVisitor = new AssignmentExpressionVisitor(context().symbolTable());
-    tree.accept(assignmentExpressionVisitor);
-    super.visitCompilationUnit(tree);
-  }
-
-  /**
-   * Try to resolve the value of a variable which is passed as argument.
-   */
-  private ExpressionTree getAssignedValue(ExpressionTree value) {
-    if (value.is(Tree.Kind.VARIABLE_IDENTIFIER)) {
-      Symbol valueSymbol = context().symbolTable().getSymbol(value);
-      return assignmentExpressionVisitor
-        .getUniqueAssignedValue(valueSymbol)
-        .orElse(value);
-    }
-    return value;
   }
 }
--- php-checks/src/main/java/org/sonar/php/checks/utils/PhpUnitCheck.java
+++ php-checks/src/main/java/org/sonar/php/checks/utils/PhpUnitCheck.java
@@ -26,8 +26,13 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.sonar.php.symbols.ClassSymbol;
 import org.sonar.php.symbols.Symbols;
+import org.sonar.php.tree.visitors.AssignmentExpressionVisitor;
+import org.sonar.plugins.php.api.symbols.Symbol;
+import org.sonar.plugins.php.api.tree.CompilationUnitTree;
+import org.sonar.plugins.php.api.tree.Tree;
 import org.sonar.plugins.php.api.tree.declaration.ClassDeclarationTree;
 import org.sonar.plugins.php.api.tree.declaration.MethodDeclarationTree;
+import org.sonar.plugins.php.api.tree.expression.ExpressionTree;
 import org.sonar.plugins.php.api.tree.expression.FunctionCallTree;
 import org.sonar.plugins.php.api.visitors.PHPVisitorCheck;
 
@@ -38,6 +43,8 @@ public abstract class PhpUnitCheck extends PHPVisitorCheck {
   private boolean isPhpUnitTestCase = false;
   private boolean isPhpUnitTestMethod = false;
   private static final Map<String, Assertion> ASSERTION = assertions();
+
+  protected AssignmentExpressionVisitor assignmentExpressionVisitor;
 
   private static Map<String, Assertion> assertions() {
     return Stream.of(
@@ -258,6 +265,15 @@ public abstract class PhpUnitCheck extends PHPVisitorCheck {
     isPhpUnitTestMethod = false;
   }
 
+  @Override
+  public void visitCompilationUnit(CompilationUnitTree tree) {
+    if (this instanceof RequireAssignmentExpressionVisitor) {
+      assignmentExpressionVisitor = new AssignmentExpressionVisitor(context().symbolTable());
+      tree.accept(assignmentExpressionVisitor);
+    }
+    super.visitCompilationUnit(tree);
+  }
+
   protected boolean isTestCaseMethod(MethodDeclarationTree tree) {
     return isPhpUnitTestCase && CheckUtils.isPublic(tree)
       && (tree.name().text().startsWith("test") || CheckUtils.hasAnnotation(tree, "test"));
@@ -287,6 +303,19 @@ public abstract class PhpUnitCheck extends PHPVisitorCheck {
 
   public static boolean isAssertion(FunctionCallTree tree) {
     return getAssertion(tree).isPresent();
+  }
+
+  /**
+   * Try to resolve the value of a variable which is passed as argument.
+   */
+  protected ExpressionTree getAssignedValue(ExpressionTree value) {
+    if (value.is(Tree.Kind.VARIABLE_IDENTIFIER) && assignmentExpressionVisitor != null) {
+      Symbol valueSymbol = context().symbolTable().getSymbol(value);
+      return assignmentExpressionVisitor
+        .getUniqueAssignedValue(valueSymbol)
+        .orElse(value);
+    }
+    return value;
   }
 
   public static Optional<Assertion> getAssertion(FunctionCallTree tree) {
