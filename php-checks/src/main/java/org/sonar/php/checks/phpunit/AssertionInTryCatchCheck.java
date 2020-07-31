d152880cfb1af33ca41b7d2c40c880d71ce4239e
Karim El Ouerghemmi <karim.ouerghemmi@sonarsource.com>
2020-07-31 14:38:52 +0200

    Add testcases and finish implementation.

--- php-checks/src/main/java/org/sonar/php/checks/phpunit/AssertionInTryCatchCheck.java
+++ php-checks/src/main/java/org/sonar/php/checks/phpunit/AssertionInTryCatchCheck.java
@@ -1,3 +1,22 @@
+/*
+ * SonarQube PHP Plugin
+ * Copyright (C) 2010-2020 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 package org.sonar.php.checks.phpunit;
 
 import com.google.common.collect.ImmutableSet;
@@ -52,7 +71,7 @@ public class AssertionInTryCatchCheck extends PhpUnitCheck {
   private List<NamespaceNameTree> getCaughtRelevantExceptionTypes(List<CatchBlockTree> catchBlocks) {
     List<NamespaceNameTree> result = new ArrayList<>();
     for(CatchBlockTree catchBlockTree : catchBlocks) {
-      if (exceptionVariableIsUsed(catchBlockTree.variable())) {
+      if (variableIsUsed(catchBlockTree.variable())) {
         continue;
       }
 
@@ -64,7 +83,7 @@ public class AssertionInTryCatchCheck extends PhpUnitCheck {
     return result;
   }
 
-  private boolean exceptionVariableIsUsed(VariableIdentifierTree variable) {
+  private boolean variableIsUsed(VariableIdentifierTree variable) {
     return !context().symbolTable().getSymbol(variable).usages().isEmpty();
   }
 
@@ -77,7 +96,7 @@ public class AssertionInTryCatchCheck extends PhpUnitCheck {
 
     @Override
     public void visitTryStatement(TryStatementTree tree) {
-      // Avoid visiting nested try-catch statement.
+      // Avoid visiting nested try-catch statements.
     }
 
     @Override
--- php-checks/src/test/resources/checks/phpunit/AssertionInTryCatchCheck.php
+++ php-checks/src/test/resources/checks/phpunit/AssertionInTryCatchCheck.php
@@ -12,5 +12,50 @@ class MyTest extends TestCase
     } catch(ExpectationFailedException $e) {
           //^^^^^^^^^^^^^^^^^^^^^^^^^^< {{Exception type that catches assertion exceptions.}}
     }
+
+    try {
+      $this->assertEquals("foo", $bar); // Noncompliant
+    } catch(Exception $e2) {
+      echo "bar";
+    }
+
+    try {
+      $this->assertEquals("foo", $bar); // Noncompliant
+    } catch(ExpectationFailedException|SomeOtherException $e22) {
+      echo "bar";
+    }
+
+    try {
+      $this->assertEquals("foo", $bar); // Noncompliant
+//    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    } catch(ExpectationFailedException|Exception $e22b) {
+          //^^^^^^^^^^^^^^^^^^^^^^^^^^<^^^^^^^^^<
+      echo "bar";
+    }
+
+    try {
+      $this->assertEquals("foo", $bar); // Noncompliant
+    } catch(SomeOtherException $e222) {
+      echo $e222;
+    } catch(ExpectationFailedException $e2222) {}
+
+    try {
+      $this->assertEquals("foo", $bar); // Compliant
+    } catch(ExpectationFailedException $e3) {
+      doSomethingWith($e3);
+    }
+
+    try {
+      $this->notAssertion(); // Compliant
+    } catch(ExpectationFailedException $e4) {
+    }
+
+  }
+}
+
+// For coverage
+class FooClass {
+  public function fooFunction() {
+    try {} catch(\Exception $e) {}
   }
 }
