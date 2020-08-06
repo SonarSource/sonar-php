a629abce324bc07998d308822503149fcbf926b9
Karim El Ouerghemmi <karim.ouerghemmi@sonarsource.com>
2020-08-06 09:27:28 +0200

    Add more tests.

--- php-checks/src/test/resources/checks/phpunit/AssertionsAfterExceptionCheck.php
+++ php-checks/src/test/resources/checks/phpunit/AssertionsAfterExceptionCheck.php
@@ -13,4 +13,37 @@ class ATest extends TestCase {
         $this->expectException(\RuntimeException::class);
         $this->assertTrue(foo()); // Noncompliant {{Refactor this test; if this assertion's argument raises an exception, the assertion will never get executed.}}
     }
+
+    public function testNoExpect() {
+      $this->assertTrue(true); // Compliant
+    }
+
+    public function testNoAssertionAtEnd() {
+      $this->expectException(\RuntimeException::class);
+      $this->assertTrue(true);
+      foo(); // Compliant
+    }
+
+    public function testExpectNotMainStatement() {
+      if(itRains()) {
+        $this->expectException(\SlipperyException::class);
+      }
+      $this->assertTrue(true); // Compliant
+    }
+
+    public function testInnerFunction() {
+      $this->expectException(\RuntimeException::class);
+      $foo = function() {
+        $this->assertTrue(true); // Compliant
+      };
+    }
 }
+
+
+// For coverage
+class Foo {
+  public function a() {
+  }
+}
+
+foo();
