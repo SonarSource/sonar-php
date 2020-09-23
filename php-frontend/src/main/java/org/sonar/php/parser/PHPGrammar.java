27c717e9182dcef61612801bd293829d18a9273a
Nils Werner <nils.werner@sonarsource.com>
2020-09-23 16:50:29 +0200

    Update grammar to only parse valid syntax for NEW_EXPRESSION

--- php-frontend/src/main/java/org/sonar/php/parser/PHPGrammar.java
+++ php-frontend/src/main/java/org/sonar/php/parser/PHPGrammar.java
@@ -1697,7 +1697,18 @@ public class PHPGrammar {
 
   public NewExpressionTree NEW_EXPRESSION() {
     return b.<NewExpressionTree>nonterminal(Kind.NEW_EXPRESSION).is(
-      f.newExpression(b.token(NEW), b.firstOf(MEMBER_EXPRESSION(), ANONYMOUS_CLASS())));
+      f.newExpression(b.token(NEW), b.firstOf(NEW_OBJECT_EXPRESSION(), ANONYMOUS_CLASS())));
+  }
+
+  public ExpressionTree NEW_OBJECT_EXPRESSION() {
+    return b.<ExpressionTree>nonterminal(PHPLexicalGrammar.NEW_OBJECT_EXPRESSION).is(
+      f.newObjectExpression(
+        PRIMARY_EXPRESSION(),
+        b.zeroOrMore(
+          b.firstOf(FUNCTION_CALL_ARGUMENT_LIST(), f.classMemberAccess(b.token(DOUBLECOLON), VARIABLE_WITHOUT_OBJECTS()))
+        )
+      )
+    );
   }
 
   public AnonymousClassTree ANONYMOUS_CLASS() {
