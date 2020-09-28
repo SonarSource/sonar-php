19f8346e25d61f0378a59392aa5782cce0822755
Nils Werner <64034005+nils-werner-sonarsource@users.noreply.github.com>
2020-09-28 15:45:39 +0200

    SONARPHP-885: Resolve variable constant values (#664)

--- php-checks/src/main/java/org/sonar/php/checks/security/QueryUsageCheck.java
+++ php-checks/src/main/java/org/sonar/php/checks/security/QueryUsageCheck.java
@@ -19,6 +19,8 @@
  */
 package org.sonar.php.checks.security;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
@@ -28,12 +30,24 @@ import org.sonar.php.checks.utils.type.NewObjectCall;
 import org.sonar.php.checks.utils.type.ObjectMemberFunctionCall;
 import org.sonar.php.checks.utils.type.TreeValues;
 import org.sonar.php.checks.utils.type.TypePredicateList;
+import org.sonar.php.tree.visitors.AssignmentExpressionVisitor;
+import org.sonar.plugins.php.api.symbols.Symbol;
+import org.sonar.plugins.php.api.tree.CompilationUnitTree;
 import org.sonar.plugins.php.api.tree.Tree;
-import org.sonar.plugins.php.api.tree.Tree.Kind;
 import org.sonar.plugins.php.api.tree.declaration.NamespaceNameTree;
+import org.sonar.plugins.php.api.tree.expression.BinaryExpressionTree;
+import org.sonar.plugins.php.api.tree.expression.ExpandableStringLiteralTree;
 import org.sonar.plugins.php.api.tree.expression.ExpressionTree;
 import org.sonar.plugins.php.api.tree.expression.FunctionCallTree;
+import org.sonar.plugins.php.api.tree.expression.VariableIdentifierTree;
 import org.sonar.plugins.php.api.visitors.PHPVisitorCheck;
+
+import static org.sonar.plugins.php.api.tree.Tree.Kind.BOOLEAN_LITERAL;
+import static org.sonar.plugins.php.api.tree.Tree.Kind.CONCATENATION;
+import static org.sonar.plugins.php.api.tree.Tree.Kind.NULL_LITERAL;
+import static org.sonar.plugins.php.api.tree.Tree.Kind.NUMERIC_LITERAL;
+import static org.sonar.plugins.php.api.tree.Tree.Kind.REGULAR_STRING_LITERAL;
+import static org.sonar.plugins.php.api.tree.Tree.Kind.VARIABLE_IDENTIFIER;
 
 @Rule(key = "S2077")
 public class QueryUsageCheck extends PHPVisitorCheck {
@@ -41,8 +55,16 @@ public class QueryUsageCheck extends PHPVisitorCheck {
   private static final String MESSAGE = "Make sure that formatting this SQL query is safe here.";
   private static final NewObjectCall IS_PDO_OBJECT = new NewObjectCall("PDO");
   private static final NewObjectCall IS_MYSQLI_OBJECT = new NewObjectCall("mysqli");
+  private static final Tree.Kind[] LITERALS = {
+    REGULAR_STRING_LITERAL,
+    BOOLEAN_LITERAL,
+    NULL_LITERAL,
+    NUMERIC_LITERAL
+  };
 
   private static final Map<String, Integer> SUSPICIOUS_GLOBAL_FUNCTIONS = buildSuspiciousGlobalFunctions();
+
+  private AssignmentExpressionVisitor assignmentExpressionVisitor;
 
   private static Map<String, Integer> buildSuspiciousGlobalFunctions() {
     Map<String, Integer> map = new HashMap<>();
@@ -68,6 +90,14 @@ public class QueryUsageCheck extends PHPVisitorCheck {
   private static final Predicate<TreeValues> PDO_PREPARE_PREDICATE = new ObjectMemberFunctionCall("prepare", new NewObjectCall("PDO"));
 
   @Override
+  public void visitCompilationUnit(CompilationUnitTree tree) {
+    assignmentExpressionVisitor = new AssignmentExpressionVisitor(context().symbolTable());
+    tree.accept(assignmentExpressionVisitor);
+
+    super.visitCompilationUnit(tree);
+  }
+
+  @Override
   public void visitFunctionCall(FunctionCallTree tree) {
     TreeValues possibleValues = TreeValues.of(tree, context().symbolTable());
     if (isSuspiciousGlobalFunction(tree) || isSuspiciousMemberFunction(tree, possibleValues) || isSuspiciousPrepareStatement(tree, possibleValues)) {
@@ -77,7 +107,7 @@ public class QueryUsageCheck extends PHPVisitorCheck {
     super.visitFunctionCall(tree);
   }
 
-  private static boolean isSuspiciousGlobalFunction(FunctionCallTree tree) {
+  private boolean isSuspiciousGlobalFunction(FunctionCallTree tree) {
     ExpressionTree callee = tree.callee();
     if (callee.is(Tree.Kind.NAMESPACE_NAME)) {
       String qualifiedNameLowerCase = ((NamespaceNameTree) callee).qualifiedName().toLowerCase(Locale.ENGLISH);
@@ -92,16 +122,63 @@ public class QueryUsageCheck extends PHPVisitorCheck {
     return false;
   }
 
-  private static boolean isSuspiciousMemberFunction(FunctionCallTree tree, TreeValues possibleValues) {
+  private boolean isSuspiciousMemberFunction(FunctionCallTree tree, TreeValues possibleValues) {
     return SUSPICIOUS_QUERY_PREDICATES.test(possibleValues) && !tree.arguments().isEmpty() && isSuspiciousArgument(tree.arguments().get(0));
   }
 
-  private static boolean isSuspiciousPrepareStatement(FunctionCallTree tree, TreeValues possibleValues) {
+  private boolean isSuspiciousPrepareStatement(FunctionCallTree tree, TreeValues possibleValues) {
     return PDO_PREPARE_PREDICATE.test(possibleValues) && !tree.arguments().isEmpty() && isSuspiciousArgument(tree.arguments().get(0));
   }
 
-  private static boolean isSuspiciousArgument(ExpressionTree expression) {
-    return expression.is(Tree.Kind.EXPANDABLE_STRING_LITERAL) || expression.is(Kind.CONCATENATION);
+  private boolean isSuspiciousArgument(ExpressionTree expression) {
+    return (expression.is(Tree.Kind.EXPANDABLE_STRING_LITERAL) && isSuspiciousExpandableString((ExpandableStringLiteralTree) expression))
+      || (expression.is(CONCATENATION) && isSuspiciousConcat((BinaryExpressionTree) expression));
   }
 
+  private boolean isSuspiciousExpandableString(ExpandableStringLiteralTree tree) {
+    for (ExpressionTree element: tree.expressions()) {
+      if (!element.is(VARIABLE_IDENTIFIER) || isSuspiciousVariable((VariableIdentifierTree) element)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  private boolean isSuspiciousConcat(BinaryExpressionTree tree) {
+    boolean isSuspicious = false;
+
+    Deque<ExpressionTree> operands = new ArrayDeque<>();
+    operands.add(tree.leftOperand());
+    operands.add(tree.rightOperand());
+
+    while (!isSuspicious && !operands.isEmpty()) {
+      ExpressionTree operand = operands.pop();
+      switch (operand.getKind()) {
+        case BOOLEAN_LITERAL:
+        case NULL_LITERAL:
+        case REGULAR_STRING_LITERAL:
+        case NUMERIC_LITERAL:
+          break;
+        case CONCATENATION:
+          operands.add(((BinaryExpressionTree) operand).leftOperand());
+          operands.add(((BinaryExpressionTree) operand).rightOperand());
+          break;
+        case VARIABLE_IDENTIFIER:
+          isSuspicious = isSuspiciousVariable((VariableIdentifierTree) operand);
+          break;
+        default:
+          isSuspicious = true;
+      }
+    }
+    return isSuspicious;
+  }
+
+  private boolean isSuspiciousVariable(VariableIdentifierTree variable) {
+    Symbol variableSymbol = context().symbolTable().getSymbol(variable);
+    return!assignmentExpressionVisitor
+      .getUniqueAssignedValue(variableSymbol)
+      .orElse(variable)
+      .is(LITERALS);
+  }
 }
