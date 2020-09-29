c1eb5bb166fd33c8b8faa6cb9e5140746a72f16a
roberto-orlandi-sonarsource <71495874+roberto-orlandi-sonarsource@users.noreply.github.com>
2020-09-29 13:32:24 +0200

    SONARPHP-973 S5527 should not raise on 1 or TRUE (#665)

--- php-checks/src/test/resources/checks/SSLHostVerificationDisabledCheck.php
+++ php-checks/src/test/resources/checks/SSLHostVerificationDisabledCheck.php
@@ -4,12 +4,13 @@ $verify_host_on = '2';
 curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, $verify_host_on);
 curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, '2');
 curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 2);
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, TRUE);
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 1);
 other_func($curl, CURLOPT_SSL_VERIFYHOST, 0);
 curl_setopt($curl, CURLOPT_OTHER_KEY, 0);
 
 $verify_host_off = FALSE;
 curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, $verify_host_off); // Noncompliant {{Enable server hostname verification on this SSL/TLS connection.}}
-curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, TRUE); // Noncompliant
 curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, '0'); // Noncompliant
 curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0); // Noncompliant
 
