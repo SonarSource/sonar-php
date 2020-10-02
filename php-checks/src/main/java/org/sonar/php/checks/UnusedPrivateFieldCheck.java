f4df54b50026cbbc467dfcfe5f21560c908f4769
Nils Werner <nils.werner@sonarsource.com>
2020-10-02 12:47:15 +0200

    Private constant's are reported as unused when referenced in other consts only

--- php-checks/src/main/java/org/sonar/php/checks/UnusedPrivateFieldCheck.java
+++ php-checks/src/main/java/org/sonar/php/checks/UnusedPrivateFieldCheck.java
@@ -19,10 +19,21 @@
  */
 package org.sonar.php.checks;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Set;
 import org.sonar.check.Rule;
+import org.sonar.php.tree.TreeUtils;
 import org.sonar.plugins.php.api.symbols.Symbol;
 import org.sonar.plugins.php.api.symbols.Symbol.Kind;
 import org.sonar.plugins.php.api.tree.CompilationUnitTree;
+import org.sonar.plugins.php.api.tree.Tree;
+import org.sonar.plugins.php.api.tree.declaration.ClassDeclarationTree;
+import org.sonar.plugins.php.api.tree.declaration.NamespaceNameTree;
+import org.sonar.plugins.php.api.tree.expression.ExpressionTree;
+import org.sonar.plugins.php.api.tree.expression.MemberAccessTree;
+import org.sonar.plugins.php.api.tree.expression.NameIdentifierTree;
 import org.sonar.plugins.php.api.visitors.PHPVisitorCheck;
 
 @Rule(key = UnusedPrivateFieldCheck.KEY)
@@ -31,12 +42,40 @@ public class UnusedPrivateFieldCheck extends PHPVisitorCheck {
   public static final String KEY = "S1068";
   private static final String MESSAGE = "Remove this unused \"%s\" private field.";
 
+  private static final Set<String> constantUsedBeforeInit = new HashSet<>();
+
+  @Override
+  public void visitMemberAccess(MemberAccessTree tree) {
+    if (tree.is(Tree.Kind.CLASS_MEMBER_ACCESS) && tree.member().is(Tree.Kind.NAME_IDENTIFIER) && isSelfConstantAccess(tree.object())) {
+      constantUsedBeforeInit.add(((NameIdentifierTree) tree.member()).text().toLowerCase(Locale.ROOT));
+    }
+
+    super.visitMemberAccess(tree);
+  }
+
   @Override
   public void visitCompilationUnit(CompilationUnitTree tree) {
+    super.visitCompilationUnit(tree);
+
     for (Symbol fieldSymbol : context().symbolTable().getSymbols(Kind.FIELD)) {
-      if (fieldSymbol.hasModifier("private") && fieldSymbol.usages().isEmpty()) {
+      if (fieldSymbol.hasModifier("private") && fieldSymbol.usages().isEmpty() && !constantUsedBeforeInit.contains(fieldSymbol.name())) {
         context().newIssue(this, fieldSymbol.declaration(), String.format(MESSAGE, fieldSymbol.name()));
       }
     }
   }
+
+  private boolean isSelfConstantAccess(ExpressionTree tree) {
+    if (tree.is(Tree.Kind.NAMESPACE_NAME)) {
+      String className = ((NamespaceNameTree) tree).fullName();
+      if (TreeUtils.findAncestorWithKind(tree, Collections.singleton(Tree.Kind.ANONYMOUS_CLASS)) == null && className.equals("self")) {
+        return true;
+      }
+
+      ClassDeclarationTree classDeclaration = (ClassDeclarationTree) TreeUtils.findAncestorWithKind(tree, Collections.singleton(Tree.Kind.CLASS_DECLARATION));
+      return classDeclaration != null && classDeclaration.name().text().equals(className);
+    }
+
+    return false;
+  }
+
 }
