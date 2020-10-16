03f4a2d4a27a6df18587894399ad76f5051ec9a4
Pierre-Yves Nicolas <pierre-yves.nicolas@sonarsource.com>
2020-10-16 13:40:51 +0200

    SONARPHP-1057 Named arguments: adapt NoPaddingRsaCheck

--- php-checks/src/test/resources/checks/NoPaddingRsaCheck.php
+++ php-checks/src/test/resources/checks/NoPaddingRsaCheck.php
@@ -7,6 +7,8 @@ function f() {
   openssl_public_encrypt($data, $crypted, $key, OPENSSL_PKCS1_PADDING); // Noncompliant
   openssl_public_encrypt($data, $crypted, $key, OPENSSL_SSLV23_PADDING); // Noncompliant
   openssl_public_encrypt($data, $crypted, $key, OPENSSL_PKCS1_OAEP_PADDING);
+  openssl_public_encrypt($data, $crypted, padding:OPENSSL_PKCS1_OAEP_PADDING, key:$key);
+  openssl_public_encrypt($data, $crypted, padding:OPENSSL_NO_PADDING, key:$key); // Noncompliant
 
   // by default OPENSSL_PKCS1_PADDING is used
   openssl_public_encrypt($data, $crypted, $key); // Noncompliant
