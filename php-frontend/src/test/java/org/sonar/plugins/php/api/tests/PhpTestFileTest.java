3f53c866f0013c129108cad3b411ba82008c0a6c
Nils Werner <nils.werner@sonarsource.com>
2020-10-22 15:03:40 +0200

    Remove expectedException field in PhpTestFileTest

--- php-frontend/src/test/java/org/sonar/plugins/php/api/tests/PhpTestFileTest.java
+++ php-frontend/src/test/java/org/sonar/plugins/php/api/tests/PhpTestFileTest.java
@@ -21,17 +21,12 @@ package org.sonar.plugins.php.api.tests;
 
 import java.io.File;
 import java.nio.file.Paths;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class PhpTestFileTest {
-
-  @Rule
-  public ExpectedException expectedException = ExpectedException.none();
 
   @Test
   public void load_file() throws Exception {
