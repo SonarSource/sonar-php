c5fb443551127127af6ba9c3372cba0c8e9337bd
Nils Werner <64034005+nils-werner-sonarsource@users.noreply.github.com>
2020-11-12 10:49:21 +0100

    Add a message on the secondary location to S3415 (#719)

--- php-checks/src/test/resources/checks/phpunit/AssertionArgumentOrderCheck.php
+++ php-checks/src/test/resources/checks/phpunit/AssertionArgumentOrderCheck.php
@@ -7,7 +7,7 @@ class MyTest extends TestCase
   public function test()
   {
     self::assertEquals($runner.getExitCode(), 0); // Noncompliant {{Swap these 2 arguments so they are in the correct order: expected value, actual value.}}
-    //                 ^^^^^^^^^^^^^^^^^^^^^> ^
+    //                 ^^^^^^^^^^^^^^^^^^^^^> ^ {{Other argument to swap.}}
     self::assertEquals($result->format('d-M-Y'), '31-Jan-2012'); // Noncompliant
     self::assertEquals(0, $runner.getExitCode()); // OK
     self::assertEquals(0, 1); // OK
