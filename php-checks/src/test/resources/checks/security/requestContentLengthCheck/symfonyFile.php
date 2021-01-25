b83f2797f188ad87e71240cac4b931206cdf82de
Karim El Ouerghemmi <karim.ouerghemmi@sonarsource.com>
2021-01-25 16:07:36 +0100

    Add tests.

--- php-checks/src/test/resources/checks/security/requestContentLengthCheck/symfonyFile.php
+++ php-checks/src/test/resources/checks/security/requestContentLengthCheck/symfonyFile.php
@@ -9,34 +9,44 @@ class Test
 {
     public static function loadValidatorMetadata(ClassMetadata $metadata)
     {
-        $metadata->addPropertyConstraint('test1', new Assert\File([
-            'maxSize' => 8000000, // Compliant
+        $metadata->addPropertyConstraint('test', new Assert\File([
+            'maxSize' => '9M', // Noncompliant {{Make sure the content length limit is safe here.}}
+            //           ^^^^
         ]));
 
-        $metadata->addPropertyConstraint('test2', new Assert\File([
-            'maxSize' => '8000k', // Compliant
-        ]));
-
-        $metadata->addPropertyConstraint('test3', new Assert\File([
-            'maxSize' => '8M', // Compliant
-        ]));
-
-        $metadata->addPropertyConstraint('test4', new Assert\File([
-            'maxSize' => '7812Ki', // Compliant
-        ]));
-
-        $metadata->addPropertyConstraint('test5', new Assert\File([
-            'maxSize' => '7Mi', // Compliant
-        ]));
-
-        $metadata->addPropertyConstraint('test6', new Assert\File([
-            'maxSize' => '9M', // Noncompliant
-        ]));
-
-        $metadata->addPropertyConstraint('test7', new Assert\File([
+        $metadata->addPropertyConstraint('test', new Assert\File([
             'maxSize' => '8Mi', // Noncompliant
         ]));
 
-        $metadata->addPropertyConstraint('test8', new Assert\File([])); // Noncompliant
+        $metadata->addPropertyConstraint('test', new Assert\File([])); // Noncompliant
+                                                              // ^^
+
+        $metadata->addPropertyConstraint('test', new Assert\File([
+            'maxSize' => null, // Noncompliant
+        ]));
+
+        $metadata->addPropertyConstraint('test', new Assert\File([
+            'maxSize' => 8000000, // Compliant
+        ]));
+
+        $metadata->addPropertyConstraint('test', new Assert\File([
+            'maxSize' => '8000k', // Compliant
+        ]));
+
+        $metadata->addPropertyConstraint('test', new Assert\File([
+            'maxSize' => '8M', // Compliant
+        ]));
+
+        $metadata->addPropertyConstraint('test', new Assert\File([
+            'maxSize' => '7812Ki', // Compliant
+        ]));
+
+        $metadata->addPropertyConstraint('test', new Assert\File([
+            'maxSize' => '7Mi', // Compliant
+        ]));
+
+        $metadata->addPropertyConstraint('test', new NotFileConstraint([
+            'maxSize' => '7Mi', // Compliant
+        ]));
     }
 }
