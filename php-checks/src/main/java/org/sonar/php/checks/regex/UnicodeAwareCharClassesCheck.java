2fde1b0fe49f4bf6401fd36a6b13cb9adfe78b3b
Pierre-Yves Nicolas <pierre-yves.nicolas@sonarsource.com>
2021-09-14 15:37:27 +0200

    SONARPHP-1192 Rule S5867: Unicode-aware versions of character classes should be preferred

---
+++ php-checks/src/main/java/org/sonar/php/checks/regex/UnicodeAwareCharClassesCheck.java
@@ -0,0 +1,122 @@
+/*
+ * SonarQube PHP Plugin
+ * Copyright (C) 2010-2021 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.php.checks.regex;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import org.sonar.check.Rule;
+import org.sonar.php.utils.collections.MapBuilder;
+import org.sonar.php.utils.collections.SetUtils;
+import org.sonar.plugins.php.api.tree.expression.FunctionCallTree;
+import org.sonarsource.analyzer.commons.regex.RegexParseResult;
+import org.sonarsource.analyzer.commons.regex.ast.CharacterRangeTree;
+import org.sonarsource.analyzer.commons.regex.ast.EscapedCharacterClassTree;
+import org.sonarsource.analyzer.commons.regex.ast.NonCapturingGroupTree;
+import org.sonarsource.analyzer.commons.regex.ast.RegexBaseVisitor;
+import org.sonarsource.analyzer.commons.regex.ast.RegexTree;
+
+@Rule(key = "S5867")
+public class UnicodeAwareCharClassesCheck extends AbstractRegexCheck {
+
+  private static final List<Character> unicodeAwareClassesWithFlag = Arrays.asList('s', 'S', 'w', 'W');
+  private static final Set<String> unicodeAwarePropertiesWithFlag = SetUtils.immutableSetOf(
+    "Lower", "Upper", "Alpha", "Alnum", "Punct", "Graph", "Print", "Blank", "Space");
+
+  private static final Map<Character, Character> unicodeUnawareCharacterRanges = MapBuilder.<Character, Character>builder()
+    .put('a', 'z')
+    .put('A', 'Z')
+    .build();
+
+  @Override
+  public void checkRegex(RegexParseResult regexParseResult, FunctionCallTree regexFunctionCall) {
+    new UnicodeUnawareCharClassFinder(regexFunctionCall).visit(regexParseResult);
+  }
+
+  private class UnicodeUnawareCharClassFinder extends RegexBaseVisitor {
+
+    private final FunctionCallTree methodInvocation;
+    private final List<CharacterRangeTree> unicodeUnawareRanges = new ArrayList<>();
+    private final List<RegexTree> unicodeAwareWithFlag = new ArrayList<>();
+    private boolean containsUnicodeCharacterFlag = false;
+
+    public UnicodeUnawareCharClassFinder(FunctionCallTree methodInvocation) {
+      this.methodInvocation = methodInvocation;
+    }
+
+    @Override
+    protected void before(RegexParseResult regexParseResult) {
+      containsUnicodeCharacterFlag |= regexParseResult.getInitialFlags().contains(Pattern.UNICODE_CHARACTER_CLASS);
+    }
+
+    @Override
+    protected void after(RegexParseResult regexParseResult) {
+      int unicodeUnawareRangeSize = unicodeUnawareRanges.size();
+      if (unicodeUnawareRangeSize == 1) {
+        newIssue(unicodeUnawareRanges.get(0), "Replace this character range with a Unicode-aware character class.");
+      } else if (unicodeUnawareRangeSize > 1) {
+        List<RegexIssueLocation> secondaries = unicodeUnawareRanges.stream()
+          .map(tree -> new RegexIssueLocation(tree, "Character range"))
+          .collect(Collectors.toList());
+        newIssue(regexParseResult.getResult(), "Replace these character ranges with Unicode-aware character classes.", secondaries);
+      }
+
+
+      if (!unicodeAwareWithFlag.isEmpty() && !containsUnicodeCharacterFlag) {
+        List<RegexIssueLocation> secondaries = unicodeAwareWithFlag.stream()
+          .map(tree -> new RegexIssueLocation(tree, "Predefined/POSIX character class"))
+          .collect(Collectors.toList());
+        newIssue(methodInvocation, "Enable the \"u\" flag or use a Unicode-aware alternative.", secondaries);
+      }
+    }
+
+    @Override
+    public void visitCharacterRange(CharacterRangeTree tree) {
+      int lowerBound = tree.getLowerBound().codePointOrUnit();
+      if (lowerBound < 0xFFFF) {
+        Character expectedUpperBoundChar = unicodeUnawareCharacterRanges.get((char) lowerBound);
+        if (expectedUpperBoundChar != null && expectedUpperBoundChar == tree.getUpperBound().codePointOrUnit()) {
+          unicodeUnawareRanges.add(tree);
+        }
+      }
+    }
+
+    @Override
+    public void visitEscapedCharacterClass(EscapedCharacterClassTree tree) {
+      String property = tree.property();
+      if ((property != null && unicodeAwarePropertiesWithFlag.contains(property)) ||
+        unicodeAwareClassesWithFlag.contains(tree.getType())) {
+
+        unicodeAwareWithFlag.add(tree);
+      }
+    }
+
+    @Override
+    public void visitNonCapturingGroup(NonCapturingGroupTree tree) {
+      containsUnicodeCharacterFlag |= tree.activeFlags().contains(Pattern.UNICODE_CHARACTER_CLASS);
+      super.visitNonCapturingGroup(tree);
+    }
+  }
+
+}
---
+++ php-checks/src/test/resources/checks/regex/UnicodeAwareCharClassesCheck.php
@@ -0,0 +1,44 @@
+<?php
+
+function noncompliantCharRanges() {
+  preg_match("/[a-z]/", $input); // Noncompliant {{Replace this character range with a Unicode-aware character class.}}
+  //            ^^^
+  preg_match("/[A-Z]/", $input); // Noncompliant
+  preg_match("/[0-9a-z]/", $input); // Noncompliant
+  preg_match("/[abcA-Zdef]/", $input); // Noncompliant
+  preg_match("/[\\x{61}-\\x{7A}]/", $input); // Noncompliant
+  //            ^^^^^^^^^^^^^^^
+  preg_match("/[a-zA-Z]/", $input); // Noncompliant {{Replace these character ranges with Unicode-aware character classes.}}
+}
+
+function noncompliantPredefinedPosixClasses() {
+  preg_match("/\\p{Lower}/", $input); // Noncompliant {{Enable the "u" flag or use a Unicode-aware alternative.}}
+  preg_match("/\\p{Alnum}/", $input); // Noncompliant
+  preg_match("/\\p{Space}/", $input); // Noncompliant
+  preg_match("/\\s/", $input); // Noncompliant
+  preg_match("/\\S/", $input); // Noncompliant
+  preg_match("/\\w/", $input); // Noncompliant
+  preg_match("/\\W/", $input); // Noncompliant
+  preg_match("/\\s\\w\\p{Lower}/", $input); // Noncompliant
+  preg_match("/\\S\\p{Upper}\\w/", $input); // Noncompliant
+}
+
+function compliantCharRanges() {
+  preg_match("/[0-9]/", $input); // Compliant: we do not consider digits
+  preg_match("/[a-y]/", $input); // Compliant: It appears a more restrictive range than simply 'all letters'
+  preg_match("/[D-Z]/", $input);
+  preg_match("/[\\x{1F600}-\\x{1F637}]/", $input);
+}
+
+function compliantPredefinedPosixClasses() {
+  preg_match("/\\p{ASCII}/", $input);
+  preg_match("/\\p{Cntrl}/", $input);
+  preg_match("/\\p{Lower}/u", $input);
+  preg_match("/(?U)\\p{Lower}/", $input);
+  preg_match("/\\w/u", $input);
+  preg_match("/(?U)\\w/", $input);
+  preg_match("/(?U:\\w)/", $input);
+  preg_match("/\\w/iu",);
+  preg_match("/\\w((?U)\\w)\\w/", $input);
+  preg_match("/\\w(?U:[a-y])\\w/", $input); // Compliant. We assume the developer knows what they are doing if they are using unicode flags somewhere.
+}
