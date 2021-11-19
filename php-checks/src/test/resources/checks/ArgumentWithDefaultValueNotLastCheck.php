9bda4d3f8df2e30534df9202dea8bfc18a5634ec
Nils Werner <64034005+nils-werner-sonarsource@users.noreply.github.com>
2021-11-19 10:10:07 +0100

    Show parser already supports `new` in initializers (#877)

--- php-checks/src/test/resources/checks/ArgumentWithDefaultValueNotLastCheck.php
+++ php-checks/src/test/resources/checks/ArgumentWithDefaultValueNotLastCheck.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Checks\ArgumentWithDefaultValueNotLastCheck;
+
 function f($p1, $p2 = 2, $p3) {           // Noncompliant {{Move arguments "$p2" after arguments without default value}}
 //        ^^^^^^^^^^^^^^^^^^^
   return;
@@ -14,4 +16,7 @@ function g($p1, $p2 = 2) {               // OK
 }
 
 function foo($p = 42, ... $rest) {  // OK, parameter with variable number of arguments is always last
+}
+
+function test($foo = new A, $bar = new B(1), $baz,) { // Noncompliant {{Move arguments "$foo", "$bar" after arguments without default value}}
 }
