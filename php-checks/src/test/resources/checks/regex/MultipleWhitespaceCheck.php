29b697816ba577de78f4a44a88da853747ac1ff2
Nils Werner <nils.werner@sonarsource.com>
2022-01-25 13:32:24 +0100

    SONARPHP-1275 Rule S6326: Regular expressions should not contain multiple spaces

---
+++ php-checks/src/test/resources/checks/regex/MultipleWhitespaceCheck.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace checks\regex;
+
+class MultipleWhitespaceCheck
+{
+  function noncompliant($input) {
+    preg_match('/Hello,   world!/', $input);          // Noncompliant {{Replace spaces with quantifier `{2}`.}}
+    //                  ^^
+    preg_match('/Hello,  world!/', $input);           // Noncompliant {{Replace spaces with quantifier `{1}`.}}
+    //                  ^
+    preg_match('/Hello, world!   /', $input);         // Noncompliant
+  }
+
+  function compliant($input) {
+    preg_match('/Hello , world!/', $input);
+    preg_match('/Hello, {3}world!/', $input);
+    preg_match('/Hello,   world!/x', $input);
+  }
+}
