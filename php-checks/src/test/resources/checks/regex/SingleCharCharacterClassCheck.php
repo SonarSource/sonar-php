6c27a5815786a4411d62bced76db6b0230f5a9fb
Stanislav Jordanov <stanislav.jordanov@sonarsource.com>
2022-01-31 17:27:31 +0200

    address review comments

--- php-checks/src/test/resources/checks/regex/SingleCharCharacterClassCheck.php
+++ php-checks/src/test/resources/checks/regex/SingleCharCharacterClassCheck.php
@@ -7,10 +7,10 @@ class SingleCharCharacterClassCheck
   function noncompliant($input) {
     preg_match('/[0]/', $input);  // Noncompliant {{Replace this character class by the character itself.}}
     //            ^
-
-    // The following couple of testcases would have been Noncompliant *if* PHP regexes supported nested char classes:
-    // preg_match('/[[a]]/', $input);  // Noncompliant {{Replace this character class by the character itself.}}
-    // preg_match('/[1-2[3]4-5]/', $input);  // Noncompliant {{Replace this character class by the character itself.}}
+    preg_match('/[z]/', $input);  // Noncompliant {{Replace this character class by the character itself.}}
+    //            ^
+    preg_match('/[ ]/', $input);  // Noncompliant {{Replace this character class by the character itself.}}
+    //            ^
   }
 
   function compliant($input) {
@@ -28,5 +28,6 @@ class SingleCharCharacterClassCheck
     preg_match('/[0-1]/', $input);
     preg_match('/[^abc]/', $input);
     preg_match('/[^a]/', $input);
+    preg_match('/[]/', $input);
   }
 }
