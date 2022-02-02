666836a6b8dabfacde149a12ef5b22df2a693d16
Stanislav <stanislav.jordanov@sonarsource.com>
2022-02-02 12:02:00 +0200

    improve tests

--- php-checks/src/test/resources/checks/regex/EmptyGroupCheck.php
+++ php-checks/src/test/resources/checks/regex/EmptyGroupCheck.php
@@ -8,23 +8,40 @@ class EmptyGroupCheck
   function noncompliant($input)
   {
     preg_match("/foo()bar/", $input);    // Noncompliant {{Remove this empty group.}}
+    //              ^^
     preg_match("/foo(?-)bar/", $input);  // Noncompliant
+    //              ^^^^
     preg_match("/foo(?-x)bar/", $input); // Noncompliant
+    //              ^^^^^
     preg_match("/foo(?:)bar/", $input);  // Noncompliant
+    //              ^^^^
     preg_match("/foo(?>)bar/", $input);  // Noncompliant
+    //              ^^^^
     preg_match("/foo(?=)bar/", $input);  // Noncompliant
+    //              ^^^^
     preg_match("/foo(?!)bar/", $input);  // Noncompliant
+    //              ^^^^
     preg_match("/foo(?<=)bar/", $input); // Noncompliant
+    //              ^^^^^
     preg_match("/foo(?<!)bar/", $input); // Noncompliant
+    //              ^^^^^
 
     preg_match("/(foo()bar)/", $input);    // Noncompliant
+    //               ^^
     preg_match("/(foo(?-)bar)/", $input);  // Noncompliant
+    //               ^^^^
     preg_match("/(foo(?:)bar)/", $input);  // Noncompliant
+    //               ^^^^
     preg_match("/(foo(?>)bar)/", $input);  // Noncompliant
+    //               ^^^^
     preg_match("/(foo(?=)bar)/", $input);  // Noncompliant
+    //               ^^^^
     preg_match("/(foo(?!)bar)/", $input);  // Noncompliant
+    //               ^^^^
     preg_match("/(foo(?<=)bar)/", $input); // Noncompliant
+    //               ^^^^^
     preg_match("/(foo(?<!)bar)/", $input); // Noncompliant
+    //               ^^^^^
   }
 
   function compliant($input)
