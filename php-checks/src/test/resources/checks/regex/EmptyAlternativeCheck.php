b3c123c57c1a085e7c96a82b3d489e08c3d9a55f
Nils Werner <nils.werner@sonarsource.com>
2022-02-02 13:51:24 +0100

    SONARPHP-1274 Rule S6323: Alternation in regular expressions should not contain empty alternatives

---
+++ php-checks/src/test/resources/checks/regex/EmptyAlternativeCheck.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace checks\regex;
+
+class EmptyAlternativeCheck
+{
+  function noncompliant($input) {
+    preg_match("/(mandatory||optional)/", $input); // Noncompliant {{Remove this empty alternative.}}
+    //                      ^
+    preg_match("/mandatory(|-optional)/", $input); // Noncompliant
+    //                     ^
+  }
+
+  function compliant($input) {
+    preg_match("/(mandatory|optional|)/", $input);
+    preg_match("/mandatory(-optional|)/", $input);
+  }
+}
