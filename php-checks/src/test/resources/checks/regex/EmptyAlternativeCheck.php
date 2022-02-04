3347d8a7edca9b15a2b868107789d316f0d00469
Stanislav <90248579+stanislav-jordanov-sonarsource@users.noreply.github.com>
2022-02-04 13:41:29 +0200

    SONARPHP-1283:  Fix a misinterpretation of SONARPHP-1274 (rule S6323) (#945)
    * fix some test cases
    
    * update ruling
    
    * add a multiline Noncompliant test case

--- php-checks/src/test/resources/checks/regex/EmptyAlternativeCheck.php
+++ php-checks/src/test/resources/checks/regex/EmptyAlternativeCheck.php
@@ -7,12 +7,24 @@ class EmptyAlternativeCheck
   function noncompliant($input) {
     preg_match("/(mandatory||optional)/", $input); // Noncompliant {{Remove this empty alternative.}}
     //                      ^
-    preg_match("/mandatory(|-optional)/", $input); // Noncompliant
-    //                     ^
+
+    preg_match("/|mandatory|-optional/", $input); // Noncompliant
+    //           ^
+
+    preg_match("/mandatory|-optional|/", $input); // Noncompliant
+    //                              ^
+
+    // Noncompliant@+3
+    preg_match("/(
+                \'[^\']*(\'|$)| # - a string that starts with a quote, up until the next quote or the end of the string
+                |               # or
+                \S              # - a non-whitespace character
+                )/x", $input);
   }
 
   function compliant($input) {
     preg_match("/(mandatory|optional|)/", $input);
     preg_match("/mandatory(-optional|)/", $input);
+    preg_match("/mandatory(|-optional)/", $input);
   }
 }
