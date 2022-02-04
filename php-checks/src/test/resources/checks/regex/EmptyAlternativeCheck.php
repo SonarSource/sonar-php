3229c2d0fa0846d4ee280a1ccbb359413c412ec9
Stanislav <stanislav.jordanov@sonarsource.com>
2022-02-04 11:46:49 +0200

    fix some test cases

--- php-checks/src/test/resources/checks/regex/EmptyAlternativeCheck.php
+++ php-checks/src/test/resources/checks/regex/EmptyAlternativeCheck.php
@@ -7,12 +7,15 @@ class EmptyAlternativeCheck
   function noncompliant($input) {
     preg_match("/(mandatory||optional)/", $input); // Noncompliant {{Remove this empty alternative.}}
     //                      ^
-    preg_match("/mandatory(|-optional)/", $input); // Noncompliant
-    //                     ^
+    preg_match("/|mandatory|-optional/", $input); // Noncompliant
+    //           ^
+    preg_match("/mandatory|-optional|/", $input); // Noncompliant
+    //                              ^
   }
 
   function compliant($input) {
     preg_match("/(mandatory|optional|)/", $input);
     preg_match("/mandatory(-optional|)/", $input);
+    preg_match("/mandatory(|-optional)/", $input);
   }
 }
