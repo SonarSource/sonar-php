c28a6c3b5ee1dea91d3d6b4d5a9554a9e95e3ec5
marco-bearzi-sonarsource <100424414+marco-bearzi-sonarsource@users.noreply.github.com>
2022-08-03 11:33:20 +0200

    SONARPHP-1136: S2755 should support cakephp xml utils (#974)

--- php-checks/src/main/java/org/sonar/php/checks/security/XxeCheck.java
+++ php-checks/src/main/java/org/sonar/php/checks/security/XxeCheck.java
@@ -19,20 +19,28 @@
  */
 package org.sonar.php.checks.security;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import org.sonar.check.Rule;
 import org.sonar.php.checks.utils.CheckUtils;
+import org.sonar.php.tree.impl.declaration.ClassNamespaceNameTreeImpl;
+import org.sonar.php.tree.impl.expression.MemberAccessTreeImpl;
 import org.sonar.plugins.php.api.tree.Tree;
 import org.sonar.plugins.php.api.tree.declaration.CallArgumentTree;
 import org.sonar.plugins.php.api.tree.declaration.NamespaceNameTree;
+import org.sonar.plugins.php.api.tree.expression.ArrayInitializerTree;
 import org.sonar.plugins.php.api.tree.expression.BinaryExpressionTree;
 import org.sonar.plugins.php.api.tree.expression.ExpressionTree;
 import org.sonar.plugins.php.api.tree.expression.FunctionCallTree;
 import org.sonar.plugins.php.api.tree.expression.ParenthesisedExpressionTree;
 import org.sonar.plugins.php.api.tree.expression.VariableIdentifierTree;
 import org.sonar.plugins.php.api.visitors.PHPVisitorCheck;
+import org.sonar.plugins.php.api.visitors.PreciseIssue;
 
 import static org.sonar.php.checks.utils.CheckUtils.argument;
+import static org.sonar.php.checks.utils.CheckUtils.arrayValue;
+import static org.sonar.php.checks.utils.CheckUtils.isFalseValue;
 import static org.sonar.php.checks.utils.CheckUtils.isTrueValue;
 import static org.sonar.php.checks.utils.CheckUtils.nameOf;
 import static org.sonar.plugins.php.api.tree.Tree.Kind;
@@ -40,21 +48,32 @@ import static org.sonar.plugins.php.api.tree.Tree.Kind;
 @Rule(key = "S2755")
 public class XxeCheck extends PHPVisitorCheck {
 
+  private static final String MESSAGE = "Disable access to external entities in XML parsing.";
+  private static final String SECONDARY_MESSAGE = "This value enables external entities in XML parsing.";
+  private static final String PROPAGATED_MESSAGE = "Propagated settings.";
+
+  private static final String OPTIONS = "options";
+  private static final Tree.Kind[] ARRAY = {Tree.Kind.ARRAY_INITIALIZER_BRACKET, Tree.Kind.ARRAY_INITIALIZER_FUNCTION};
+
   @Override
   public void visitFunctionCall(FunctionCallTree call) {
     String functionName = CheckUtils.lowerCaseFunctionName(call);
     ExpressionTree callee = call.callee();
     if (callee.is(Kind.NAMESPACE_NAME) && "simplexml_load_string".equals(functionName)) {
-      argument(call, "options", 2).ifPresent(x -> checkSimpleXmlOption(x.value(), x));
+      argument(call, OPTIONS, 2).ifPresent(options -> checkSimpleXmlOption(options.value(), options));
     } else if (callee.is(Kind.OBJECT_MEMBER_ACCESS)) {
       if ("load".equals(functionName) || "loadxml".equals(functionName)) {
-        argument(call, "options", 1).ifPresent(x -> checkSimpleXmlOption(x.value(), x));
+        argument(call, OPTIONS, 1).ifPresent(options -> checkSimpleXmlOption(options.value(), options));
       } else if ("setparserproperty".equals(functionName)) {
         checkSetParserProperty(call);
       }
+    } else if (callee.is(Kind.CLASS_MEMBER_ACCESS) && "build".equals(functionName) &&
+      ("Cake\\Utility\\Xml").equalsIgnoreCase(namespaceMemberFullQualifiedName(callee))) {
+      argument(call, OPTIONS, 1).ifPresent(options -> checkXmlBuildOption(options.value(), options));
     }
     super.visitFunctionCall(call);
   }
+
 
   private void checkSimpleXmlOption(ExpressionTree optionValue, Tree treeToReport) {
     if (optionValue.is(Kind.NAMESPACE_NAME) && "LIBXML_NOENT".equals(((NamespaceNameTree) optionValue).unqualifiedName())) {
@@ -80,8 +99,40 @@ public class XxeCheck extends PHPVisitorCheck {
     }
   }
 
-  private void createIssue(Tree tree) {
-    context().newIssue(this, tree, "Disable access to external entities in XML parsing.");
+  private void checkXmlBuildOption(ExpressionTree optionValue, Tree optionArgument) {
+    if (optionValue.is(Kind.VARIABLE_IDENTIFIER)) {
+      CheckUtils.uniqueAssignedValue((VariableIdentifierTree) optionValue)
+        .filter(uniqueValue -> !uniqueValue.toString().equals(optionArgument.toString()))
+        .ifPresent(assignedValue -> checkXmlBuildOption(assignedValue, optionArgument));
+    } else if (optionValue.is(ARRAY)) {
+      arrayValue((ArrayInitializerTree) optionValue, "loadEntities")
+        .ifPresent(loadEntitiesValue -> raiseIssueIfTrue(loadEntitiesValue, optionArgument));
+    }
+  }
+
+  private void raiseIssueIfTrue(ExpressionTree value, Tree treeToReport) {
+    ExpressionTree assignedValue = CheckUtils.assignedValue(value);
+    List<Tree> secondaryTrees = new ArrayList<>();
+    while (assignedValue.is(Kind.VARIABLE_IDENTIFIER)) {
+      secondaryTrees.add(assignedValue);
+      assignedValue = CheckUtils.assignedValue(assignedValue);
+    }
+    if (!isFalseValue(assignedValue)) {
+      PreciseIssue issue = createIssue(treeToReport);
+      issue.secondary(assignedValue, SECONDARY_MESSAGE);
+      secondaryTrees.forEach(tree -> issue.secondary(tree, PROPAGATED_MESSAGE));
+    }
+  }
+
+  private PreciseIssue createIssue(Tree tree) {
+    return context().newIssue(this, tree, MESSAGE);
+  }
+
+  private static String namespaceMemberFullQualifiedName(ExpressionTree callee) {
+    return ((ClassNamespaceNameTreeImpl) ((MemberAccessTreeImpl) callee).object())
+      .symbol()
+      .qualifiedName()
+      .toString();
   }
 
 }
---
+++ php-checks/src/test/resources/checks/security/XxeCheck_CakeUtils.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace ns1 {
+  use Cake\Utility\Xml;
+  $xml = Xml::build($content, ['loadEntities' => true]);                  // Noncompliant
+}
+
+namespace ns2 {
+  use Cake\Utility;
+  $xml = Utility\Xml::build($content, ['loadEntities' => true]);          // Noncompliant
+}
+
+namespace ns3 {
+  use Cake;
+  $xml = Cake\Utility\Xml::build($content, ['loadEntities' => true]);     // Noncompliant
+}
+
+namespace ns4 {
+  use Cake\Utility\Xml;
+  $xml = Cake\Utility\Xml::build($content, ['loadEntities' => true]);     // Noncompliant
+}
+
+namespace ns5 {
+  use Cake\Utility\Xml as CakeXML;
+  $xml = CakeXML::build($content, ['loadEntities' => true]);              // Noncompliant
+}
+
+namespace ns6 {
+  use Cake\Utility\Xml;
+
+  function trueValue() {
+    return true;
+  }
+
+  function trueEntities() {
+    return array("loadEntities" => true);
+  }
+
+  function xmlBuild($param) {
+    $xml = Xml::build($content, ['loadEntities' => true]);      // Noncompliant {{Disable access to external entities in XML parsing.}}
+//                              ^^^^^^^^^^^^^^^^^^^^^^^^
+
+    $xml = Cake\Utility\Xml::build($content, ['loadEntities' => true]); // Noncompliant
+
+    $val = true;
+//         ^^^^> {{This value enables external entities in XML parsing.}}
+    $xml = Xml::build($content, ['loadEntities' => $val]);      // Noncompliant
+//                              ^^^^^^^^^^^^^^^^^^^^^^^^
+    $options = array('loadEntities' => true);
+//                                     ^^^^>  {{This value enables external entities in XML parsing.}}
+    $xml = Xml::build($content, $options);                      // Noncompliant
+//                              ^^^^^^^^
+    $var = true;
+//         ^^^^> {{This value enables external entities in XML parsing.}}
+    $secondValue = $var;
+//                 ^^^^> {{Propagated settings.}}
+    $xml = Xml::build($content, ['loadEntities' => $secondValue]); // Noncompliant {{Disable access to external entities in XML parsing.}}
+//                              ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+    $xml = Xml::build($content, ['loadEntities' => trueValue()]);// Noncompliant
+
+    $xml = Xml::build($content, trueEntities());                // FN should raise issue
+
+    $xml = Xml::build($content);                                // Compliant by default
+    $xml = Xml::build($content, ['loadEntities' => false]);     // Compliant
+    $options2 = array('loadEntities' => false);
+    $xml = Xml::build($content, $options2);                     // Compliant
+    $xml = Xml::build($content, $valueOutOfScope);              // Compliant if value is unknown
+    $xml = Xml::build($content, ['loadEntities' => NULL]);     // Compliant
+
+    $options3 = $options4;
+    $options4 = $options3;
+
+    $xml = Xml::build($content, $options3);
+
+    $option5 = $options3;
+    $xml = Xml::build($content, $options5);
+
+    $valueForCoverage = Xml::otherFunction();
+  }
+
+}
+
+namespace ns7 {
+  use Xml;
+  $xml = Xml::build($content, ['loadEntities' => true]);                        // Compliant as not using Cake\Utility\Xml
+  $notCakeCoverage = XmlNotCAKE::build($content, ['loadEntities' => true]);     // Compliant as not using Cake\Utility\Xml
+}
