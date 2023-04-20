f90ced7424de23346267e4f3b4db79d5111b6ab2
Rudy Regazzoni <rudy.regazzoni@sonarsource.com>
2023-04-20 12:23:17 +0200

    SONARPHP-1347 S6600 Parentheses should not be used for calls of language constructs

---
+++ php-checks/src/test/resources/checks/ConstructWithParenthesesCheck.php
@@ -0,0 +1,66 @@
+<?php
+// basic cases with each construct
+echo "string";
+  echo("string");               // Noncompliant {{Remove the parentheses from this "echo" call.}}
+//^^^^^^^^^^^^^^
+break 1;
+break(1);                       // Noncompliant {{Remove the parentheses from this "break" call.}}
+$b = clone $a;
+$b = clone($a);                 // Noncompliant {{Remove the parentheses from this "clone" call.}}
+switch(1) {
+    case 1: break;
+    case(2): break;             // Noncompliant {{Remove the parentheses from this "case" call.}}
+//  ^^^^^^^^
+}
+continue 1;
+continue(1);                    // Noncompliant {{Remove the parentheses from this "continue" call.}}
+include "file.php";
+include("file.php");            // Noncompliant {{Remove the parentheses from this "include" call.}}
+include_once "file.php";
+include_once("file.php");       // Noncompliant {{Remove the parentheses from this "include_once" call.}}
+require "file.php";
+require("file.php");            // Noncompliant {{Remove the parentheses from this "require" call.}}
+require_once "file.php";
+require_once("file.php");       // Noncompliant {{Remove the parentheses from this "require_once" call.}}
+print "string";
+print("string");                // Noncompliant {{Remove the parentheses from this "print" call.}}
+return 1;
+return(1);                      // Noncompliant {{Remove the parentheses from this "return" call.}}
+return(1+1)*2;
+throw new Exception("string");
+throw(new Exception("string")); // Noncompliant {{Remove the parentheses from this "throw" call.}}
+yield 1;
+yield(1);                       // Noncompliant {{Remove the parentheses from this "yield" call.}}
+yield from [1, 2, 3];
+yield from([1, 2, 3]);          // Noncompliant {{Remove the parentheses from this "yield from" call.}}
+
+// more advanced cases
+yield 1 => 2;
+  yield(1) => 2;   // Noncompliant
+//^^^^^^^^^^^^^
+yield 1 => (2);
+yield(1) => (2); // Noncompliant
+echo ("string"), "string";
+
+// case with binary expression
+print("string") && false; // Noncompliant
+
+if ( print  "string"            ) {}
+if ( print ("string")           ) {} // Noncompliant
+if ( print  "string"  && false  ) {}
+if ( print ("string") && false  ) {} // Noncompliant
+//   ^^^^^^^^^^^^^^^^^^^^^^^^^
+if ( print ("string") == false  ) {} // Noncompliant
+if ( print ("string") .  "str"  ) {} // Noncompliant
+if ( print ("string"  .  "str") ) {} // Noncompliant
+if ( print~("string")           ) {}
+if ( print (~"string")          ) {} // Noncompliant
+
+if ( print ("string") || false || true ) {}          // Noncompliant
+if ( print ("string") || false && true ) {}          // Noncompliant
+if ( print ("string") ^ false xor true && true ) {}  // Noncompliant
+
+// other cases
+echof("string");
+$var = "echo";
+$var("string");
