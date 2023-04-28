bde770b213f89eb9c52f1225c4ef4a1e3de94718
Jonas Wielage <127737854+jonas-wielage-sonarsource@users.noreply.github.com>
2023-04-28 10:13:20 +0200

    updated expected issue message in test

--- php-checks/src/test/resources/checks/HardCodedCredentialsInVariablesAndUrisCheckCustom.php
+++ php-checks/src/test/resources/checks/HardCodedCredentialsInVariablesAndUrisCheckCustom.php
@@ -5,14 +5,14 @@ class A {
   public $password = "";
   public $passwd = "";
 
-  public $iReallyLikeLadyMarmalade = "haha"; // Noncompliant {{'marmalade' detected in this variable name, review this potentially hardcoded credential.}}
+  public $iReallyLikeLadyMarmalade = "haha"; // Noncompliant {{Detected 'marmalade' in this variable name, review this potentially hardcoded credential.}}
 //       ^^^^^^^^^^^^^^^^^^^^^^^^^
   public $someMarmalade = ""; // OK, empty
 
   private function foo() {
     $variable1 = "blabla";
     $variable2 = "login=a&pwd=xxx"; // Compliant
-    $worms = "bazooka=xxx"; // Noncompliant {{'bazooka' detected in this variable name, review this potentially hardcoded credential.}}
+    $worms = "bazooka=xxx"; // Noncompliant {{Detected 'bazooka' in this variable name, review this potentially hardcoded credential.}}
 //           ^^^^^^^^^^^^^
   }
 }
