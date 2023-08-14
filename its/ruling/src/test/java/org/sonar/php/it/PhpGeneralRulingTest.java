0a779fa5e56a3b10e51d3a67d4473131d926f490
Nils Werner <nils.werner@sonarsource.com>
2023-08-14 17:18:10 +0200

    SONARPHP-1442 Ignore `PHP_CodeSniffer from ruling test to ensure deterministic results

--- its/ruling/src/test/java/org/sonar/php/it/PhpGeneralRulingTest.java
+++ its/ruling/src/test/java/org/sonar/php/it/PhpGeneralRulingTest.java
@@ -71,7 +71,9 @@ public class PhpGeneralRulingTest {
   }
 
 
+  // TODO SONARPHP-1441 Solve ruling test results due to nondeterministic symbol creation
   @Test
+  @Ignore("Due to nondeterministic results")
   public void testPhpCodeSniffer() throws Exception {
     testProject("PHP_CodeSniffer");
   }
