c533d84d841c06b50822b1031225ac719e3a3aba
Marcin Stachniuk <marcin.stachniuk@sonarsource.com>
2023-08-14 16:11:54 +0200

    SONARPHP-1165 Remove dependency on guava's checkNotNull function, fix

--- php-frontend/src/main/java/org/sonar/plugins/php/api/cfg/PhpCfgBlock.java
+++ php-frontend/src/main/java/org/sonar/plugins/php/api/cfg/PhpCfgBlock.java
@@ -32,6 +32,8 @@ import javax.annotation.Nullable;
 import org.sonar.php.utils.collections.SetUtils;
 import org.sonar.plugins.php.api.tree.Tree;
 
+import static java.util.Objects.requireNonNull;
+
 class PhpCfgBlock implements CfgBlock {
 
   private Set<PhpCfgBlock> predecessors = new HashSet<>();
@@ -46,7 +48,8 @@ class PhpCfgBlock implements CfgBlock {
   }
 
   PhpCfgBlock(PhpCfgBlock successor, PhpCfgBlock syntacticSuccessor) {
-    this(SetUtils.immutableSetOf(successor), Preconditions.checkNotNull(syntacticSuccessor,
+
+    this(SetUtils.immutableSetOf(successor), requireNonNull(syntacticSuccessor,
       "Syntactic successor cannot be null"));
   }
 
