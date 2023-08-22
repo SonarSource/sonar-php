66c8a6f7a496a4e051684b51e469b5f1b989aeff
Jonas Wielage <jonas.wielage@sonarsource.com>
2023-08-22 09:14:49 +0200

    SONARPHP-1374 Wrong line detected for S1757

---
+++ php-checks/src/test/resources/checks/OpeningPHPTagCheck/ko_starting_comment_2.php
@@ -0,0 +1,6 @@
+// Make sure skipping works.
+
+// Noncompliant@+1
+Some content <?
+echo '<?';
+?> Some more content
---
+++ php-checks/src/test/resources/checks/OpeningPHPTagCheck/ko_starting_comment_3.php
@@ -0,0 +1,6 @@
+// Test multi-line.
+
+// Noncompliant@+1
+Some content <?
+echo $var;
+?> Some more content
