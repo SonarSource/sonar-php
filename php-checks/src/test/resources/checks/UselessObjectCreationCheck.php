3717a85b5e3f185d4d0588b677cb61d2454e5393
Jonas Wielage <127737854+jonas-wielage-sonarsource@users.noreply.github.com>
2023-08-22 09:44:10 +0200

    SONARPHP-1412 S1848 Allow object creation for format validation when used in try-catch block (#1121)

--- php-checks/src/test/resources/checks/UselessObjectCreationCheck.php
+++ php-checks/src/test/resources/checks/UselessObjectCreationCheck.php
@@ -1,4 +1,5 @@
 <?php
+
 $x = new class1();
 
   new class1();               // Noncompliant {{Either remove this useless object instantiation of class "class1" or use it}}
@@ -11,3 +12,49 @@ new class1().method1();
 $x;
 
 class1();
+
+function isValidDate1($varToTest) {
+    try {
+        new DateTime($varToTest); // Compliant, as it is the only statement in the try catch
+    } catch (Exception $exception) {
+        return false;
+    }
+    return true;
+}
+
+function isValidDate2($varToTest) {
+    try {
+        $x = 1;
+        new DateTime($varToTest); // Noncompliant
+    } catch (Exception $exception) {
+        return false;
+    }
+    return true;
+}
+
+function isValidDate3($varToTest) {
+    try {
+        new DateTime($varToTest); // Noncompliant
+        $x = 1;
+    } catch (Exception $exception) {
+        return false;
+    }
+    return true;
+}
+
+function isValidDate4($varToTest) {
+    try {
+    } catch (Exception $exception) {
+        return false;
+    }
+    return true;
+}
+
+if($x) {
+  new DateTime($varToTest); // Noncompliant
+}
+
+if($x) {
+  $x = 1;
+  new DateTime($varToTest); // Noncompliant
+}
