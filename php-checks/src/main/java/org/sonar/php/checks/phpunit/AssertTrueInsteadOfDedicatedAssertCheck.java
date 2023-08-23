ac077e2310c1ded3dde8e7fb4eed3bec1380594d
Rudy Regazzoni <110470341+rudy-regazzoni-sonarsource@users.noreply.github.com>
2023-08-23 13:53:43 +0200

    SONARPHP-1396 S5785 assertSame is suggested, but assertTrue will be better (#1120)

--- php-checks/src/main/java/org/sonar/php/checks/phpunit/AssertTrueInsteadOfDedicatedAssertCheck.java
+++ php-checks/src/main/java/org/sonar/php/checks/phpunit/AssertTrueInsteadOfDedicatedAssertCheck.java
@@ -19,6 +19,7 @@
  */
 package org.sonar.php.checks.phpunit;
 
+import java.util.Locale;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
@@ -26,21 +27,26 @@ import org.sonar.check.Rule;
 import org.sonar.php.checks.utils.CheckUtils;
 import org.sonar.php.checks.utils.PhpUnitCheck;
 import org.sonar.php.utils.collections.MapBuilder;
-import org.sonar.php.utils.collections.SetUtils;
+import org.sonar.plugins.php.api.tree.Tree;
 import org.sonar.plugins.php.api.tree.expression.BinaryExpressionTree;
 import org.sonar.plugins.php.api.tree.expression.ExpressionTree;
 import org.sonar.plugins.php.api.tree.expression.FunctionCallTree;
+import org.sonar.plugins.php.api.tree.expression.LiteralTree;
 import org.sonar.plugins.php.api.tree.expression.UnaryExpressionTree;
 
 import static org.sonar.php.checks.utils.CheckUtils.skipParenthesis;
+import static org.sonar.plugins.php.api.tree.Tree.Kind.BOOLEAN_LITERAL;
 import static org.sonar.plugins.php.api.tree.Tree.Kind.NULL_LITERAL;
 
 @Rule(key = "S5785")
 public class AssertTrueInsteadOfDedicatedAssertCheck extends PhpUnitCheck {
 
-  private static final String MESSAGE = "Use %s instead.";
-  private static final String SECONDARY_MESSAGE = "%s is performed here, which is better expressed with %s.";
-  private static final Set<String> ASSERT_METHOD_NAMES = SetUtils.immutableSetOf("assertTrue", "assertFalse");
+  private static final String MESSAGE_USE_INSTEAD = "Use %s() instead.";
+  private static final String SECONDARY_MESSAGE_USE_INSTEAD = "%s is performed here, which is better expressed with %s.";
+  private static final String MESSAGE_SIMPLIFY = "Simplify this expression by %sremoving the comparison to '%s'.";
+  private static final String ASSERT_TRUE = "assertTrue";
+  private static final String ASSERT_FALSE = "assertFalse";
+  private static final Set<String> ASSERT_BOOLEAN_METHOD_NAMES = Set.of(ASSERT_TRUE, ASSERT_FALSE);
 
   private static final Map<ReplacementAssertion, ReplacementAssertion> COMPLEMENTS = MapBuilder.<ReplacementAssertion, ReplacementAssertion>builder()
     .put(ReplacementAssertion.NULL, ReplacementAssertion.NOT_NULL)
@@ -50,6 +56,9 @@ public class AssertTrueInsteadOfDedicatedAssertCheck extends PhpUnitCheck {
     .put(ReplacementAssertion.EQUALS, ReplacementAssertion.NOT_EQUALS)
     .put(ReplacementAssertion.NOT_EQUALS, ReplacementAssertion.EQUALS)
     .build();
+  private static final Map<String, String> ASSERTIONS_COMPLEMENT = Map.of(
+    ASSERT_FALSE, ASSERT_TRUE,
+    ASSERT_TRUE, ASSERT_FALSE);
 
   private enum ReplacementAssertion {
     NULL("Null", "A null-check"),
@@ -67,8 +76,8 @@ public class AssertTrueInsteadOfDedicatedAssertCheck extends PhpUnitCheck {
     ReplacementAssertion(String namePostfix, String actionDescription) {
       this.methodName = "assert" + namePostfix;
       this.actionDescription = actionDescription;
-      this.useInsteadMessage = String.format(MESSAGE, methodName);
-      this.secondaryExplanationMessage = String.format(SECONDARY_MESSAGE, actionDescription, methodName);
+      this.useInsteadMessage = String.format(MESSAGE_USE_INSTEAD, methodName);
+      this.secondaryExplanationMessage = String.format(SECONDARY_MESSAGE_USE_INSTEAD, actionDescription, methodName);
     }
   }
 
@@ -79,7 +88,7 @@ public class AssertTrueInsteadOfDedicatedAssertCheck extends PhpUnitCheck {
     }
 
     getAssertion(fct).ifPresent(a -> {
-      if (ASSERT_METHOD_NAMES.contains(a.name())) {
+      if (ASSERT_BOOLEAN_METHOD_NAMES.contains(a.name())) {
         checkBooleanExpressionInAssertMethod(fct, a.name());
       }
     });
@@ -89,13 +98,34 @@ public class AssertTrueInsteadOfDedicatedAssertCheck extends PhpUnitCheck {
 
   private void checkBooleanExpressionInAssertMethod(FunctionCallTree problematicAssertionCall, String assertionName) {
     CheckUtils.argumentValue(problematicAssertionCall, "", 0).ifPresent(argumentExpression -> {
-      Optional<ReplacementAssertion> replacementAssertionOpt = getReplacementAssertion(argumentExpression);
-      if (assertionName.equals("assertFalse")) {
-        replacementAssertionOpt = replacementAssertionOpt.map(COMPLEMENTS::get);
-      }
+      Optional<String> argumentValue = retrieveBooleanLiteralFromBinaryExpression(argumentExpression);
 
-      replacementAssertionOpt.ifPresent(replacementAssertion -> reportIssue(problematicAssertionCall, replacementAssertion, argumentExpression));
+      if (argumentValue.isPresent()) {
+        // Boolean comparison, raise an issue to simplify it
+        BinaryExpressionTree bet = (BinaryExpressionTree) argumentExpression;
+        String booleanLiteralToRemove = argumentValue.get();
+        String additionalAction = shouldChangeAssertionFunction(assertionName, booleanLiteralToRemove, bet)
+          ? String.format("using %s() and ", ASSERTIONS_COMPLEMENT.get(assertionName))
+          : "";
+        String message = String.format(MESSAGE_SIMPLIFY, additionalAction, booleanLiteralToRemove.toLowerCase(Locale.ROOT));
+        newIssue(problematicAssertionCall, message);
+      } else {
+        // Non-boolean comparison, raise an issue to replace it
+        Optional<ReplacementAssertion> replacementAssertionOpt = getReplacementAssertion(argumentExpression);
+        if (assertionName.equals(ASSERT_FALSE)) {
+          replacementAssertionOpt = replacementAssertionOpt.map(COMPLEMENTS::get);
+        }
+        replacementAssertionOpt.ifPresent(replacementAssertion -> reportIssue(problematicAssertionCall, replacementAssertion, argumentExpression));
+      }
     });
+  }
+
+  private static boolean shouldChangeAssertionFunction(String assertionName, String booleanLiteralValue, BinaryExpressionTree bet) {
+    boolean isAssertTrue = ASSERT_TRUE.equals(assertionName);
+    boolean isTrueValue = "true".equalsIgnoreCase(booleanLiteralValue);
+    boolean isEqualComparison = bet.is(Tree.Kind.EQUAL_TO, Tree.Kind.STRICT_EQUAL_TO);
+
+    return !((isAssertTrue && isTrueValue == isEqualComparison) || (!isAssertTrue && isTrueValue));
   }
 
   private void reportIssue(FunctionCallTree problematicAssertionCall, ReplacementAssertion replacementAssertion, ExpressionTree argumentExpression) {
@@ -152,5 +182,17 @@ public class AssertTrueInsteadOfDedicatedAssertCheck extends PhpUnitCheck {
 
   private static boolean isCheckForNull(BinaryExpressionTree bet) {
     return bet.leftOperand().is(NULL_LITERAL) || bet.rightOperand().is(NULL_LITERAL);
+  }
+
+  private static Optional<String> retrieveBooleanLiteralFromBinaryExpression(ExpressionTree expr) {
+    if (expr.is(Tree.Kind.EQUAL_TO, Tree.Kind.STRICT_EQUAL_TO, Tree.Kind.NOT_EQUAL_TO, Tree.Kind.STRICT_NOT_EQUAL_TO)) {
+      BinaryExpressionTree bet = (BinaryExpressionTree) expr;
+      if (bet.leftOperand().is(BOOLEAN_LITERAL)) {
+        return Optional.of(((LiteralTree) bet.leftOperand()).value());
+      } else if (bet.rightOperand().is(BOOLEAN_LITERAL)) {
+        return Optional.of(((LiteralTree) bet.rightOperand()).value());
+      }
+    }
+    return Optional.empty();
   }
 }
--- php-checks/src/test/resources/checks/phpunit/AssertTrueInsteadOfDedicatedAssertCheck.php
+++ php-checks/src/test/resources/checks/phpunit/AssertTrueInsteadOfDedicatedAssertCheck.php
@@ -6,32 +6,46 @@ class MyTest extends TestCase
 {
   public function test()
   {
-    assertTrue($a === $b); // Noncompliant {{Use assertSame instead.}}
+    assertTrue($a === $b); // Noncompliant {{Use assertSame() instead.}}
 //  ^^^^^^^^^^^^^^^^^^^^^
-    assertTrue($a == $b); // Noncompliant {{Use assertEquals instead.}}
-    assertTrue($a === null); // Noncompliant {{Use assertNull instead.}}
-    assertTrue($a !== null); // Noncompliant {{Use assertNotNull instead.}}
-    assertTrue($a !== $b); // Noncompliant {{Use assertNotSame instead.}}
-    assertTrue($a != $b); // Noncompliant {{Use assertNotEquals instead.}}
-    assertTrue($a == null); // Noncompliant {{Use assertNull instead.}}
-    assertTrue($a != null); // Noncompliant {{Use assertNotNull instead.}}
+    assertTrue($a == $b); // Noncompliant {{Use assertEquals() instead.}}
+    assertTrue($a === null); // Noncompliant {{Use assertNull() instead.}}
+    assertTrue($a !== null); // Noncompliant {{Use assertNotNull() instead.}}
+    assertTrue($a !== $b); // Noncompliant {{Use assertNotSame() instead.}}
+    assertTrue($a != $b); // Noncompliant {{Use assertNotEquals() instead.}}
+    assertTrue($a == null); // Noncompliant {{Use assertNull() instead.}}
+    assertTrue($a != null); // Noncompliant {{Use assertNotNull() instead.}}
 
-    assertFalse($a === $b); // Noncompliant {{Use assertNotSame instead.}}
-    assertFalse($a == $b); // Noncompliant {{Use assertNotEquals instead.}}
-    assertFalse($a === null); // Noncompliant {{Use assertNotNull instead.}}
-    assertFalse($a !== null); // Noncompliant {{Use assertNull instead.}}
-    assertFalse($a === $b); // Noncompliant {{Use assertNotSame instead.}}
-    assertFalse($a == $b); // Noncompliant {{Use assertNotEquals instead.}}
-    assertFalse($a == null); // Noncompliant {{Use assertNotNull instead.}}
-    assertFalse($a != null); // Noncompliant {{Use assertNull instead.}}
+    assertFalse($a === $b); // Noncompliant {{Use assertNotSame() instead.}}
+    assertFalse($a == $b); // Noncompliant {{Use assertNotEquals() instead.}}
+    assertFalse($a === null); // Noncompliant {{Use assertNotNull() instead.}}
+    assertFalse($a !== null); // Noncompliant {{Use assertNull() instead.}}
+    assertFalse($a === $b); // Noncompliant {{Use assertNotSame() instead.}}
+    assertFalse($a == $b); // Noncompliant {{Use assertNotEquals() instead.}}
+    assertFalse($a == null); // Noncompliant {{Use assertNotNull() instead.}}
+    assertFalse($a != null); // Noncompliant {{Use assertNull() instead.}}
 
-    assertTrue(!($a === null)); // Noncompliant {{Use assertNotNull instead.}}
+    assertTrue(!($a === null)); // Noncompliant {{Use assertNotNull() instead.}}
 
     assertTrue(null == null); // Noncompliant
 
-    assertTrue(strpos($this->Mail->Body, 'src="composer.json"') === false); // Noncompliant {{Use assertSame instead.}}
-    assertTrue(strpos($this->Mail->Body, 'src="composer.json"') !== false); // Noncompliant {{Use assertNotSame instead.}}
-    assertTrue(doSomeThing() === true); // Noncompliant
+    assertTrue(strpos($this->Mail->Body, 'src="composer.json"') === false); // Noncompliant {{Simplify this expression by using assertFalse() and removing the comparison to 'false'.}}
+    assertTrue(strpos($this->Mail->Body, 'src="composer.json"') !== false); // Noncompliant {{Simplify this expression by removing the comparison to 'false'.}}
+
+    assertTrue ($a == true);  // Noncompliant {{Simplify this expression by removing the comparison to 'true'.}}
+    assertTrue ($a == false); // Noncompliant {{Simplify this expression by using assertFalse() and removing the comparison to 'false'.}}
+    assertFalse($a == true);  // Noncompliant {{Simplify this expression by removing the comparison to 'true'.}}
+    assertFalse($a == false); // Noncompliant {{Simplify this expression by using assertTrue() and removing the comparison to 'false'.}}
+    assertTrue ($a != true);  // Noncompliant {{Simplify this expression by using assertFalse() and removing the comparison to 'true'.}}
+    assertTrue ($a != false); // Noncompliant {{Simplify this expression by removing the comparison to 'false'.}}
+    assertFalse($a != true);  // Noncompliant {{Simplify this expression by removing the comparison to 'true'.}}
+    assertFalse($a != false); // Noncompliant {{Simplify this expression by using assertTrue() and removing the comparison to 'false'.}}
+    assertTrue($a === true);  // Noncompliant
+    assertTrue($a === false); // Noncompliant
+    assertTrue(true == $a);  // Noncompliant
+    assertTrue(false == $a); // Noncompliant
+    assertTrue ($a == TRue);  // Noncompliant {{Simplify this expression by removing the comparison to 'true'.}}
+
     assertEquals($a, $b);
     doSomeThing();
   }
