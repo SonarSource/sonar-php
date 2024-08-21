1bf1aba3cd1b996ff9e1dfe0f7261095d97edcac
GabinL21 <gabin.laigle21@gmail.com>
2024-08-21 16:40:12 +0200

    Add regular string literals

--- php-checks/src/main/java/org/sonar/php/checks/ConstantConditionCheck.java
+++ php-checks/src/main/java/org/sonar/php/checks/ConstantConditionCheck.java
@@ -11,11 +11,16 @@ public class ConstantConditionCheck extends PHPVisitorCheck {
 
   public static final String KEY = "S5797";
   private static final String MESSAGE = "Replace this expression; used as a condition it will always be constant.";
+  private static final Tree.Kind[] LITERAL_KINDS = {
+    Tree.Kind.BOOLEAN_LITERAL,
+    Tree.Kind.NUMERIC_LITERAL,
+    Tree.Kind.REGULAR_STRING_LITERAL
+  };
 
   @Override
   public void visitIfStatement(IfStatementTree tree) {
     ExpressionTree conditionExpression = tree.condition().expression();
-    if(conditionExpression.is(Tree.Kind.BOOLEAN_LITERAL) || conditionExpression.is(Tree.Kind.NUMERIC_LITERAL)) {
+    if(conditionExpression.is(LITERAL_KINDS)) {
       newIssue(conditionExpression, MESSAGE);
     }
     super.visitIfStatement(tree);
--- php-checks/src/test/resources/checks/ConstantConditionCheck.php
+++ php-checks/src/test/resources/checks/ConstantConditionCheck.php
@@ -3,3 +3,4 @@
 if(true); // Noncompliant {{Replace this expression; used as a condition it will always be constant.}}
 // ^^^^
 if (42); // Noncompliant
+if ("foo"); // Noncompliant
