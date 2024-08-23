3a307ea98960a46101d8d001b2c5ebabc0c75f38
GabinL21 <gabin.laigle21@gmail.com>
2024-08-23 12:26:36 +0200

    Add license and specifications

--- php-checks/src/main/java/org/sonar/php/checks/ConstantConditionCheck.java
+++ php-checks/src/main/java/org/sonar/php/checks/ConstantConditionCheck.java
@@ -1,3 +1,22 @@
+/*
+ * SonarQube PHP Plugin
+ * Copyright (C) 2010-2024 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 package org.sonar.php.checks;
 
 import org.sonar.check.Rule;
@@ -16,12 +35,13 @@ public class ConstantConditionCheck extends PHPVisitorCheck {
     Tree.Kind.NUMERIC_LITERAL,
     Tree.Kind.REGULAR_STRING_LITERAL,
     Tree.Kind.NULL_LITERAL,
+    Tree.Kind.ARRAY_INITIALIZER_FUNCTION,
   };
 
   @Override
   public void visitIfStatement(IfStatementTree tree) {
     ExpressionTree conditionExpression = tree.condition().expression();
-    if(conditionExpression.is(LITERAL_KINDS)) {
+    if (conditionExpression.is(LITERAL_KINDS)) {
       newIssue(conditionExpression, MESSAGE);
     }
     super.visitIfStatement(tree);
