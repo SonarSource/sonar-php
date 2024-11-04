d008b7af845e5ec006ed9337f2b9911d42633d5e
GabinL21 <gabin.laigle@sonarsource.com>
2024-11-04 17:24:31 +0100

    SONARPHP-1522 Fix formatting

--- build-logic/src/main/kotlin/org.sonarsource.php.rule-api.gradle.kts
+++ build-logic/src/main/kotlin/org.sonarsource.php.rule-api.gradle.kts
@@ -1,18 +1,18 @@
 val ruleApiVersion = "2.9.0.4061"
 
 repositories {
-    maven {
-        url = project.uri("https://repox.jfrog.io/repox/sonarsource-private-releases")
-        authentication {
-            credentials {
-                val artifactoryUsername: String? by project
-                val artifactoryPassword: String? by project
-                username = artifactoryUsername
-                password = artifactoryPassword
-            }
-        }
+  maven {
+    url = project.uri("https://repox.jfrog.io/repox/sonarsource-private-releases")
+    authentication {
+      credentials {
+        val artifactoryUsername: String? by project
+        val artifactoryPassword: String? by project
+        username = artifactoryUsername
+        password = artifactoryPassword
+      }
     }
-    mavenCentral()
+  }
+  mavenCentral()
 }
 
 val ruleApi: Configuration = configurations.create("ruleApi")
@@ -49,6 +49,6 @@ tasks.register<JavaExec>("ruleApiGenerateRule") {
         add("-branch")
         add(branch.get())
       }
-    }
+    },
   )
 }
