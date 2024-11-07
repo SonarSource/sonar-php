e2d883cd833d8d5afcd0d35e56a6f4ac3cad1b03
GabinL21 <gabin.laigle@sonarsource.com>
2024-11-07 16:23:46 +0100

    SONARPHP-1520 Enable custom rules integration tests

--- its/plugin/tests/src/integrationTest/java/com/sonar/it/php/CustomRulesTest.java
+++ its/plugin/tests/src/integrationTest/java/com/sonar/it/php/CustomRulesTest.java
@@ -31,8 +31,6 @@ import static com.sonar.it.php.Tests.createScanner;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.sonarqube.ws.Issues.Issue;
 
-// TODO Enable back with SONARPHP-1520
-@Disabled
 class CustomRulesTest {
 
   @RegisterExtension
