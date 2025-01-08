023a994306939dcf4a4bac554f0eac0bc31e3e9d
Rudy Regazzoni <110470341+rudy-regazzoni-sonarsource@users.noreply.github.com>
2025-01-08 10:52:58 +0100

    SONARPHP-1590 - S1192 should not raise for HTML tags - reapply (#1363)

--- php-checks/src/main/java/org/sonar/php/checks/StringLiteralDuplicatedCheck.java
+++ php-checks/src/main/java/org/sonar/php/checks/StringLiteralDuplicatedCheck.java
@@ -21,7 +21,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
-import org.apache.commons.lang3.StringUtils;
 import org.sonar.check.Rule;
 import org.sonar.check.RuleProperty;
 import org.sonar.plugins.php.api.tree.CompilationUnitTree;
@@ -30,19 +29,56 @@ import org.sonar.plugins.php.api.tree.expression.LiteralTree;
 import org.sonar.plugins.php.api.visitors.PHPVisitorCheck;
 import org.sonar.plugins.php.api.visitors.PreciseIssue;
 
+import static org.sonar.php.checks.utils.RegexUtils.firstOf;
+import static org.sonar.php.checks.utils.RegexUtils.oneOrMore;
+import static org.sonar.php.checks.utils.RegexUtils.optional;
+import static org.sonar.php.checks.utils.RegexUtils.zeroOrMore;
+
 @Rule(key = "S1192")
 public class StringLiteralDuplicatedCheck extends PHPVisitorCheck {
 
   private static final String MESSAGE = "Define a constant instead of duplicating this literal \"%s\" %s times.";
   private static final String SECONDARY_MESSAGE = "Duplication.";
 
-  private static final Pattern ALLOWED_DUPLICATED_LITERALS = Pattern.compile("^[a-zA-Z_][.\\-\\w]+$");
+  private static final String ONLY_ALPHANUMERIC_UNDERSCORES_HYPHENS_AND_PERIODS = "^[a-zA-Z_][.\\-\\w]+$";
 
-  private final Map<String, LiteralTree> firstOccurrenceTrees = new HashMap<>();
-  private final Map<String, List<LiteralTree>> sameLiteralOccurrences = new HashMap<>();
+  // Single elements
+  private static final String IDENTIFIER = "[a-zA-Z][a-zA-Z0-9\\-_:.]*+";
+  private static final String OPT_SPACING = "\\s*+";
+  private static final String OPT_TEXT_OUTSIDE_OF_TAGS = "[^<>]*+";
+  private static final String DOUBLE_QUOTED_STRING = "\"(?:\\\\.|[^\"])*+\"";
+  private static final String SINGLE_QUOTED_STRING = "'(?:\\\\.|[^'])*+'";
+  private static final String NO_QUOTED_STRING = "[a-zA-Z0-9\\-_:./]++";
+
+  // Partial elements matching
+  private static final String DOUBLE_QUOTED_STRING_PARTIAL_START = "\"(?:\\\\.|[^\"])*+";
+  private static final String SINGLE_QUOTED_STRING_PARTIAL_START = "'(?:\\\\.|[^'])*+";
+  private static final String NO_QUOTED_STRING_PARTIAL_START = "[a-zA-Z0-9\\-_:./]++";
+  private static final String TAG_ATTRIBUTE_PARTIAL_START = OPT_SPACING
+    + optional("=", OPT_SPACING, optional(firstOf(DOUBLE_QUOTED_STRING_PARTIAL_START, SINGLE_QUOTED_STRING_PARTIAL_START, NO_QUOTED_STRING_PARTIAL_START)));
+
+  // Complex regexes
+  private static final String TAG_ATTRIBUTE = IDENTIFIER + OPT_SPACING + optional("=", OPT_SPACING, firstOf(DOUBLE_QUOTED_STRING, SINGLE_QUOTED_STRING, NO_QUOTED_STRING));
+  private static final String HTML_TAG_FULL = "</?" + OPT_SPACING + IDENTIFIER + OPT_SPACING + zeroOrMore(TAG_ATTRIBUTE, OPT_SPACING) + "/?+>";
+  private static final String HTML_TAG_PARTIAL_START = OPT_SPACING + "</?+" + OPT_SPACING
+    + optional(IDENTIFIER, OPT_SPACING, zeroOrMore(TAG_ATTRIBUTE, OPT_SPACING), optional(TAG_ATTRIBUTE_PARTIAL_START));
+  private static final String HTML_TAG_PARTIAL_END = "[\"']?+" + OPT_SPACING + zeroOrMore(TAG_ATTRIBUTE, OPT_SPACING) + "/?+>";
+  private static final String HTML_CONTENT = optional(HTML_TAG_PARTIAL_END) + oneOrMore(OPT_TEXT_OUTSIDE_OF_TAGS, HTML_TAG_FULL) + OPT_TEXT_OUTSIDE_OF_TAGS
+    + optional(HTML_TAG_PARTIAL_START);
+
+  private static final String FULL_ALLOWED_LITERALS_REGEX = firstOf(
+    HTML_CONTENT,
+    OPT_TEXT_OUTSIDE_OF_TAGS + HTML_TAG_PARTIAL_START,
+    HTML_TAG_PARTIAL_END + OPT_TEXT_OUTSIDE_OF_TAGS,
+    HTML_TAG_PARTIAL_END + OPT_TEXT_OUTSIDE_OF_TAGS + HTML_TAG_PARTIAL_START,
+    ONLY_ALPHANUMERIC_UNDERSCORES_HYPHENS_AND_PERIODS);
+  private static final Pattern ALLOWED_DUPLICATED_LITERALS = Pattern.compile(FULL_ALLOWED_LITERALS_REGEX);
 
   public static final int THRESHOLD_DEFAULT = 3;
   public static final int MINIMAL_LITERAL_LENGTH_DEFAULT = 5;
+
+  private final Map<String, LiteralTree> firstOccurrenceTrees = new HashMap<>();
+  private final Map<String, List<LiteralTree>> sameLiteralOccurrences = new HashMap<>();
 
   @RuleProperty(
     key = "threshold",
@@ -66,9 +102,10 @@ public class StringLiteralDuplicatedCheck extends PHPVisitorCheck {
 
   private void finish() {
     for (Map.Entry<String, List<LiteralTree>> literalOccurrences : sameLiteralOccurrences.entrySet()) {
+      String value = literalOccurrences.getKey();
       List<LiteralTree> occurrences = literalOccurrences.getValue();
 
-      if (occurrences.size() >= threshold) {
+      if (occurrences.size() >= threshold && !ALLOWED_DUPLICATED_LITERALS.matcher(value).matches()) {
         String literal = literalOccurrences.getKey();
         String message = String.format(MESSAGE, literal, occurrences.size());
         LiteralTree firstOccurrenceTree = firstOccurrenceTrees.get(literal);
@@ -83,22 +120,25 @@ public class StringLiteralDuplicatedCheck extends PHPVisitorCheck {
   @Override
   public void visitLiteral(LiteralTree tree) {
     if (tree.is(Kind.REGULAR_STRING_LITERAL)) {
-      String literal = tree.value();
-      String value = StringUtils.substring(literal, 1, literal.length() - 1);
+      String literal = tree.value().replace("\\'", "'").replace("\\\"", "\"");
+      String value = removeQuotesAndQuotesEscaping(literal);
 
-      if (value.length() >= minimalLiteralLength && !ALLOWED_DUPLICATED_LITERALS.matcher(value).find()) {
-
+      if (value.length() >= minimalLiteralLength) {
         if (!sameLiteralOccurrences.containsKey(value)) {
           List<LiteralTree> occurrences = new ArrayList<>();
           occurrences.add(tree);
           sameLiteralOccurrences.put(value, occurrences);
           firstOccurrenceTrees.put(value, tree);
-
         } else {
           sameLiteralOccurrences.get(value).add(tree);
         }
       }
     }
+  }
+
+  private static String removeQuotesAndQuotesEscaping(String s) {
+    var quote = s.charAt(0);
+    return s.substring(1, s.length() - 1).replace("\\" + quote, String.valueOf(quote));
   }
 
 }
---
+++ php-checks/src/main/java/org/sonar/php/checks/utils/RegexUtils.java
@@ -0,0 +1,38 @@
+/*
+ * SonarQube PHP Plugin
+ * Copyright (C) 2010-2025 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the Sonar Source-Available License Version 1, as published by SonarSource SA.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the Sonar Source-Available License for more details.
+ *
+ * You should have received a copy of the Sonar Source-Available License
+ * along with this program; if not, see https://sonarsource.com/license/ssal/
+ */
+package org.sonar.php.checks.utils;
+
+public final class RegexUtils {
+  private RegexUtils() {
+  }
+
+  public static String oneOrMore(String... s) {
+    return "(?:" + String.join("", s) + ")++";
+  }
+
+  public static String zeroOrMore(String... s) {
+    return "(?:" + String.join("", s) + ")*+";
+  }
+
+  public static String optional(String... s) {
+    return "(?:" + String.join("", s) + ")?+";
+  }
+
+  public static String firstOf(String... s) {
+    return "(?:(?:" + String.join(")|(?:", s) + "))";
+  }
+}
