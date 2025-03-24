8a8f861a7c49baec24e418224b6e5e1088dc55a5
Marcin Stachniuk <marcin.stachniuk@sonarsource.com>
2025-03-24 12:54:12 +0100

    SONARPHP-1635 Integrate cloud-native-gradle-modules as a source of shared build logic

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,37 +16,14 @@
  */
 pluginManagement {
   includeBuild("build-logic")
-
-  repositories {
-    mavenCentral()
-    gradlePluginPortal()
-    maven {
-      name = "artifactory"
-      url = uri("https://repox.jfrog.io/repox/sonarsource")
-      val artifactoryUsername =
-        providers.environmentVariable("ARTIFACTORY_PRIVATE_USERNAME")
-          .orElse(providers.gradleProperty("artifactoryUsername"))
-      val artifactoryPassword =
-        providers.environmentVariable("ARTIFACTORY_PRIVATE_PASSWORD")
-          .orElse(providers.gradleProperty("artifactoryPassword"))
-
-      if (artifactoryUsername.isPresent && artifactoryPassword.isPresent) {
-        authentication {
-          credentials {
-            username = artifactoryUsername.get()
-            password = artifactoryPassword.get()
-          }
-        }
-      }
-    }
-  }
 }
 
 plugins {
-  id("com.diffplug.blowdryerSetup") version "1.7.1"
+  id("org.sonarsource.cloud-native.common-settings")
 }
 
 rootProject.name = "php"
+includeBuild("build-logic")
 
 include(":sonar-php-plugin")
 include(":php-frontend")
@@ -54,41 +31,3 @@ include(":php-checks")
 include(":php-custom-rules-plugin")
 include(":its:plugin")
 include(":its:ruling")
-
-gradle.allprojects {
-  // this value is present on CI
-  val buildNumber: String? = System.getProperty("buildNumber")
-  project.extra["buildNumber"] = buildNumber
-  val version = properties["version"] as String
-  if (version.endsWith("-SNAPSHOT") && buildNumber != null) {
-    val versionSuffix = if (version.count { it == '.' } == 1) ".0.$buildNumber" else ".$buildNumber"
-    project.version =
-      version.replace("-SNAPSHOT", versionSuffix).also {
-        logger.lifecycle("Project ${project.name} version set to $it")
-      }
-  }
-}
-
-dependencyResolutionManagement {
-  repositories {
-    mavenCentral()
-    maven {
-      url = uri("https://repox.jfrog.io/repox/sonarsource")
-      val artifactoryUsername =
-        providers.environmentVariable("ARTIFACTORY_PRIVATE_USERNAME")
-          .orElse(providers.gradleProperty("artifactoryUsername"))
-      val artifactoryPassword =
-        providers.environmentVariable("ARTIFACTORY_PRIVATE_PASSWORD")
-          .orElse(providers.gradleProperty("artifactoryPassword"))
-
-      if (artifactoryUsername.isPresent && artifactoryPassword.isPresent) {
-        authentication {
-          credentials {
-            username = artifactoryUsername.get()
-            password = artifactoryPassword.get()
-          }
-        }
-      }
-    }
-  }
-}
