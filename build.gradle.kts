8d20f66aa5b6904aebf658bbb313df273a46c502
Jonas Wielage <jonas.wielage@sonarsource.com>
2025-09-09 13:54:28 +0200

    SONARPHP-1699 Implement Unified dogfooding for sonar-php

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,8 +45,9 @@ spotless {
 
 sonar {
   properties {
-    property("sonar.projectName", "SonarSource PHP Analyzer")
+    property("sonar.projectName", "SonarPHP")
     property("sonar.projectKey", "org.sonarsource.php:php")
+    property("sonar.organization", "sonarsource")
     property("sonar.exclusions", "**/build/**/*")
     property("sonar.links.ci", "https://cirrus-ci.com/github/SonarSource/sonar-php")
     property("sonar.links.scm", "https://github.com/SonarSource/sonar-php")
