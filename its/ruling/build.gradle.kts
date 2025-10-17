c5e0698309c79521445c541be542c86864e0f2d5
Peter Trifanov <peter.trifanov@sonarsource.com>
2025-10-17 10:07:53 +0200

    Remove explicit dependency on `xerces`

--- its/ruling/build.gradle.kts
+++ its/ruling/build.gradle.kts
@@ -27,7 +27,6 @@ dependencies {
   "integrationTestImplementation"(libs.junit.jupiter)
   "integrationTestImplementation"(libs.assertj.core)
   "integrationTestImplementation"(libs.sonar.analyzer.commons)
-  "integrationTestImplementation"("xerces:xercesImpl:2.12.2")
   "integrationTestImplementation"(libs.sonar.orchestrator.junit5)
   "integrationTestRuntimeOnly"(libs.junit.platform.launcher)
 }
