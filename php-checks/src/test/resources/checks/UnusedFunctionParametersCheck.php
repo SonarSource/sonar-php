595b9a5d3aff6c8137edcba79dbe453cf69047e2
Ziad Marey <ziad.marey@sonarsource.com>
2025-12-23 14:46:42 +0100

    Removed redundant tests.

--- php-checks/src/test/resources/checks/UnusedFunctionParametersCheck.php
+++ php-checks/src/test/resources/checks/UnusedFunctionParametersCheck.php
@@ -304,101 +304,11 @@ class ExamplePlugin
     }
 }
 
-// Magic methods - methods starting with __ should not raise issues (except __construct which has special handling)
+// Magic methods - methods starting with __ should not raise issues
 class MagicMethodsClass
 {
     // Destructor - should not raise issues
     public function __destruct()
-    {
-        return;
-    }
-
-    // __call - should not raise issues
-    public function __call($name, $arguments)
-    {
-        return;
-    }
-
-    // __callStatic - should not raise issues
-    public static function __callStatic($name, $arguments)
-    {
-        return;
-    }
-
-    // __get - should not raise issues
-    public function __get($name)
-    {
-        return;
-    }
-
-    // __set - should not raise issues
-    public function __set($name, $value)
-    {
-        return;
-    }
-
-    // __isset - should not raise issues
-    public function __isset($name)
-    {
-        return;
-    }
-
-    // __unset - should not raise issues
-    public function __unset($name)
-    {
-        return;
-    }
-
-    // __sleep - should not raise issues
-    public function __sleep()
-    {
-        return [];
-    }
-
-    // __wakeup - should not raise issues
-    public function __wakeup()
-    {
-        return;
-    }
-
-    // __toString - should not raise issues
-    public function __toString()
-    {
-        return "";
-    }
-
-    // __invoke - should not raise issues
-    public function __invoke($param1, $param2, $param3)
-    {
-        return;
-    }
-
-    // __set_state - should not raise issues
-    public static function __set_state($properties)
-    {
-        return new self();
-    }
-
-    // __clone - should not raise issues
-    public function __clone()
-    {
-        return;
-    }
-
-    // __debugInfo - should not raise issues
-    public function __debugInfo()
-    {
-        return [];
-    }
-
-    // __serialize - should not raise issues
-    public function __serialize()
-    {
-        return [];
-    }
-
-    // __unserialize - should not raise issues
-    public function __unserialize($data)
     {
         return;
     }
