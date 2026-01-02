0bc33138c63d27e3076a83372180f4827193731c
Ziad Marey <ziad.marey@sonarsource.com>
2026-01-02 14:26:52 +0100

    Spotless

--- php-frontend/src/main/java/org/sonar/php/tree/symbols/SymbolVisitor.java
+++ php-frontend/src/main/java/org/sonar/php/tree/symbols/SymbolVisitor.java
@@ -628,7 +628,7 @@ public class SymbolVisitor extends NamespaceNameResolvingVisitor {
     }
 
     // Check if the object refers to the current class by name (e.g., MySingleton::$instance inside MySingleton class)
-    if (isInClassScope() && tree.object() instanceof NamespaceNameTree treeObject && treeObject.is(Kind.NAMESPACE_NAME)){
+    if (isInClassScope() && tree.object() instanceof NamespaceNameTree treeObject && treeObject.is(Kind.NAMESPACE_NAME)) {
       Tree classTree = currentClassScope().tree();
       if (classTree instanceof ClassDeclarationTree classDeclaration) {
         String className = treeObject.fullName();
